645e48cb82a3f075129d395648571857b2d5989f
Anthony Minessale <anthony.minessale@gmail.com>
2005-12-14 18:11:49 +0000

    lame
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@150 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/mod_portaudio/ringbuffer.h
+++ src/mod/mod_portaudio/ringbuffer.h
@@ -40,6 +40,7 @@ extern "C"
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  */
+#define PortAudioStream PaStream
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
