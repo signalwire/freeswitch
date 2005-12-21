c0db418cd1d3722480d4e2fdbf5c9d25ecf2df64
Anthony Minessale <anthony.minessale@gmail.com>
2005-12-21 17:22:38 +0000

    unix build tweaks
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@185 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- buildlib.sh
+++ buildlib.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
+
+if [ -f .nodepends ] ; then
+    exit 0
+fi
+
+root=$1
+shift
+
+
 install=
 base=http://www.sofaswitch.com/mikej
-tar=$1
-
-shift
 
 if [ ! -z $1 ] && [ $1 = install ] ; then
     install=1
     shift
 fi
 
-cd libs
+tar=$1
+shift
+
+cd $root/libs/.
+CFLAGS=
+LDFLAGS=
+MAKEFLAGS=
 
 if [ -d $tar ] ; then
     uncompressed=$tar
@@ -28,14 +40,14 @@ else
     fi
 fi
 
-if [ -f $uncompressed/.complete ] && [ -z $install ] ; then
+if [ -f $uncompressed/.complete ] ; then
     echo $uncompressed already installed
     exit
 fi
 
 cd $uncompressed
-
-./configure $@
+make clean 2>&1
+sh ./configure $@
 
 if [ $? == 0 ] ; then
     make
@@ -46,6 +58,7 @@ fi
 
 if [ ! -z $install ] ; then
     make install
+    ldconfig 2>&1
 fi
 
 if [ $? == 0 ] ; then
