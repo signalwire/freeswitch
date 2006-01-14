5e86c382d995ee1480ce7eff0829f814751f920d
Anthony Minessale <anthony.minessale@gmail.com>
2006-01-14 18:55:20 +0000

    optimize pcre and fix autoheadache
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@387 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- checkversion.sh
+++ checkversion.sh
@@ -12,6 +12,7 @@ oldversion=`cat .version 2>/dev/null || echo "0"`
 if [ $oldversion != $version ] ; then
     cat src/include/switch_version.h.in | sed "s/@SVN_VERSION@/$version/g" > src/include/switch_version.h
     echo $version > .version
+    make modclean
 fi
 
 
