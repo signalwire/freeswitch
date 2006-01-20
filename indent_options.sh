c7b94cf33f89d899b306daefaebbbbecc6de64a4
Michael Jerris <mike@jerris.com>
2006-01-20 00:34:52 +0000

    make indent_options.sh write to .indent.pro
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@414 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- indent_options.sh
+++ indent_options.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo -n "-brs -npsl -di0 -br -ce -d0 -cli0 -npcs -nfc1 -ut -i4 -ts4 -l120 -cs -T size_t " > indent.pro
-grep typedef src/include/*.h | grep switch_ | grep -v "\*\|{" | perl -ne '@l = split; $l[2] =~ s/;//g ; print "-T $l[2] "' >> indent.pro
-grep "} switch_" src/include/*.h | perl -ne '@l = split; $l[1] =~ s/;//g ; print " -T $l[1] "' >> indent.pro
+echo -n "-brs -npsl -di0 -br -ce -d0 -cli0 -npcs -nfc1 -ut -i4 -ts4 -l120 -cs -T size_t " > .indent.pro
+grep typedef src/include/*.h | grep switch_ | grep -v "\*\|{" | perl -ne '@l = split; $l[2] =~ s/;//g ; print "-T $l[2] "' >> .indent.pro
+grep "} switch_" src/include/*.h | perl -ne '@l = split; $l[1] =~ s/;//g ; print " -T $l[1] "' >> .indent.pro
