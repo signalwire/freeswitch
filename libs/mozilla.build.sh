1588f9e76781e9cae50f8ff1748aa01492f0f6d3
Brian West <brian@freeswitch.org>
2006-03-01 06:09:37 +0000

    This should work better in most cases.
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@712 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/mozilla.build.sh
+++ libs/mozilla.build.sh
@@ -1,3 +1,3 @@
 cd nsprpub && ./configure && make
-cd ../js/src && JS_THREADSAFE=1 OTHER_LIBS="-L../../../mozilla/nsprpub/dist/lib" INCLUDES="-I../../../mozilla/nsprpub/dist/include/nspr"  make -f Makefile.ref $(OS_CONFIG)_DBG.OBJ/libjs.a
+cd ../js/src && JS_THREADSAFE=1 OTHER_LIBS="-L../../../mozilla/nsprpub/dist/lib" INCLUDES="-I../../../mozilla/nsprpub/dist/include/nspr"  make -f Makefile.ref `find . -name libjs.a`
 
