c7ab1b5885b7331d23a8681aa8ca5b8837f1a501
Anthony Minessale <anthony.minessale@gmail.com>
2006-03-21 17:08:54 +0000

    64bit tweak
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@888 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/mozilla.build.sh
+++ libs/mozilla.build.sh
@@ -1,3 +1,11 @@
-cd nsprpub && ./configure && make
+arch=`uname -m`
+
+opts=""
+if [ $arch = "x86_64" ] ; then
+opts="--enable-64bit"
+fi
+
+cd nsprpub && ./configure $opts && make
+
 cd ../js/src && JS_THREADSAFE=1 OTHER_LIBS="-L../../../mozilla/nsprpub/dist/lib" INCLUDES="-I../../../mozilla/nsprpub/dist/include/nspr"  make -f Makefile.ref `find . -name libjs.a`
 
