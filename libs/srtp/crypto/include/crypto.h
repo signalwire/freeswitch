b3d890ef2526636fe8547283f8c54797b75dd9a9
Anthony Minessale <anthony.minessale@gmail.com>
2006-04-07 16:10:03 +0000

    add srtp
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@1086 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ libs/srtp/crypto/include/crypto.h
@@ -0,0 +1,17 @@
+/*
+ * crypto.h
+ *
+ * API for libcrypto
+ * 
+ * David A. McGrew
+ * Cisco Systems, Inc.
+ */
+
+#ifndef CRYPTO_H
+#define CRYPTO_H
+
+#include "crypto_kernel.h"
+
+#endif /* CRYPTO_H */
+
+
