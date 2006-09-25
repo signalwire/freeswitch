b474fafc5eab9f6355afb547a3a29e54ca2a7bbc
Michael Jerris <mike@jerris.com>
2006-09-25 14:57:44 +0000

    Merged revision 2827-2828 from freeswitch/branches/tholo.  Thanks tholo:
    2827: Embed installed name of shared library on Mac OS X -- otherwise code that is linked against it will not find it later on as the relative path during build gets embedded.
    
    2828: Make it build using Xcode on Mac OS X again
    
    
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@2830 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/portaudio.build.sh
+++ libs/portaudio.build.sh
@@ -1,3 +1,17 @@
+patch <<__EOF__
+--- configure.orig	2006-09-24 23:56:36.000000000 -0700
++++ configure	2006-09-24 23:57:32.000000000 -0700
+@@ -1886,7 +1886,7 @@
+ 	OTHER_OBJS="pa_mac_core/pa_mac_core.o";
+ 	LIBS="-framework CoreAudio -lm";
+ 	PADLL="libportaudio.dylib";
+-	SHARED_FLAGS="-framework CoreAudio -dynamiclib";
++	SHARED_FLAGS="-framework CoreAudio -dynamiclib -install_name \\\$(PREFIX)/lib/\\\$(PADLL)";
+ 	;;
+ 
+   mingw* )
+__EOF__
+
 arch=`uname -m`
 
 opts=""
