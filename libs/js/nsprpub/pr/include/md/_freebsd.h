0948a663d81b3fc6f372906ab031421f72d1e8b5
Anthony Minessale <anthony.minessale@gmail.com>
2006-12-19 01:26:41 +0000

    sigh
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@3713 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/js/nsprpub/pr/include/md/_freebsd.h
+++ libs/js/nsprpub/pr/include/md/_freebsd.h
@@ -106,6 +106,11 @@
 #define _PR_IPV6_V6ONLY_PROBE
 #endif
 
+#if __FreeBSD_version >= 601103
+#define _PR_HAVE_GETPROTO_R
+#define _PR_HAVE_5_ARG_GETPROTO_R
+#endif
+
 #define USE_SETJMP
 
 #ifndef _PR_PTHREADS
