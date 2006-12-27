2e23a95ffe82b3f8e72906a1390785ecd08d1b87
Anthony Minessale <anthony.minessale@gmail.com>
2006-12-27 15:54:56 +0000

    tweak
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@3849 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -1612,13 +1612,13 @@ static switch_status_t sofia_read_frame(switch_core_session_t *session, switch_f
 
 			if (tech_pvt->read_frame.datalen > 0) {
                 size_t bytes = 0;
-                int frames = 0;
+                int frames = 1;
 				//tech_pvt->last_read = switch_time_now();
                 if (!switch_test_flag((&tech_pvt->read_frame), SFF_CNG)) {
                     if ((bytes = tech_pvt->read_codec.implementation->encoded_bytes_per_frame)) {
                         frames = (tech_pvt->read_frame.datalen / bytes);
-                        tech_pvt->read_frame.samples = (int) (frames * tech_pvt->read_codec.implementation->samples_per_frame);
                     }
+                    tech_pvt->read_frame.samples = (int) (frames * tech_pvt->read_codec.implementation->samples_per_frame);
                 }
 				break;
 			}
