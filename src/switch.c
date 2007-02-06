57c5be057d6ab5eac6aa5beb2f013cd99a7122a6
Michael Jerris <mike@jerris.com>
2007-02-06 19:20:39 +0000

    vg default on windows, not otherwise.
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@4139 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch.c
+++ src/switch.c
@@ -198,9 +198,9 @@ int main(int argc, char *argv[])
 	const char *err = NULL;		// error value for return from freeswitch initialization
 #ifndef WIN32
 	int nf = 0;					// TRUE if we are running in nofork mode
-	int vg = 1;					// Allways TRUE on windows to not do apr_terminate
+	int vg = 0;					// Allways TRUE on windows to not do apr_terminate
 #else
-	int vg = 0;					// TRUE if we are running in vg mode
+	int vg = 1;					// TRUE if we are running in vg mode
 #endif
 	int nc = 0;					// TRUE if we are running in noconsole mode
 	FILE *f;					// file handle to the pid file
