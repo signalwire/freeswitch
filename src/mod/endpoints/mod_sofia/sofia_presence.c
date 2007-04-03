c4a882f07843084340685fd9464b525bc84e09d0
Anthony Minessale <anthony.minessale@gmail.com>
2007-04-03 01:24:24 +0000

    wtf
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@4837 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/sofia_presence.c
+++ src/mod/endpoints/mod_sofia/sofia_presence.c
@@ -617,7 +617,11 @@ static int sofia_presence_mwi_callback(void *pArg, int argc, char **argv, char *
 	assert(nh != NULL);
 
 	id = switch_mprintf("sip:%s@%s", sub_to_user, sub_to_host);
-	exp = switch_mprintf("active;expires=%ld", time(NULL) - expire_sec);
+	expire_sec -= time(NULL);
+	if (expire_sec < 0) {
+		expire_sec = 3600;
+	}
+	exp = switch_mprintf("active;expires=%ld", expire_sec);
 
 	tmp = contact;
 	contact = sofia_glue_get_url_from_contact(tmp, 0);
