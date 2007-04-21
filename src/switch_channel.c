8755dea7b8190aab5587cf378eafbe53c856fc4b
Anthony Minessale <anthony.minessale@gmail.com>
2007-04-21 14:23:12 +0000

    add backslash to escape the backslash escape char in channel variable expansion
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@4996 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_channel.c
+++ src/switch_channel.c
@@ -1243,6 +1243,10 @@ SWITCH_DECLARE(char *) switch_channel_expand_variables(switch_channel_t *channel
 			if (*p == '\\') {
 				if (*(p + 1) == '$') {
 					nv = 1;
+				} else if (*(p + 1) == '\\') {
+					*c++ = *p++;
+					len++;
+					continue;
 				}
 				p++;
 			}
