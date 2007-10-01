1944a0ff6ad9992e5cb0adf9a8702a4d16c180f9
Anthony Minessale <anthony.minessale@gmail.com>
2007-10-01 20:39:04 +0000

    fix typo
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@5773 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/applications/mod_conference/mod_conference.c
+++ src/mod/applications/mod_conference/mod_conference.c
@@ -713,7 +713,7 @@ static void *SWITCH_THREAD_FUNC conference_thread_run(switch_thread_t * thread, 
 
 		if (conference->perpetual_sound && !conference->fnode) {
 			conference_play_file(conference, conference->perpetual_sound, CONF_DEFAULT_LEADIN, NULL, 1);
-		} else if (conference->moh_sound && conference->count == 1 && !conference->fnode) {
+		} else if (conference->moh_sound && conference->count == 1 && !conference->async_fnode) {
 			conference_play_file(conference, conference->moh_sound, CONF_DEFAULT_LEADIN, NULL, 1);
 		}
 
