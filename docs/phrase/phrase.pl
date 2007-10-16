6987f2ab99594670e9c0b3aec486476f56aee125
Brian West <brian@freeswitch.org>
2007-10-16 19:13:48 +0000

    generated test files with cepstral on OS X
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@5909 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- docs/phrase/phrase.pl
+++ docs/phrase/phrase.pl
@@ -12,11 +12,11 @@ foreach $language ( sort keys %{$ref}) {
   foreach $item ( sort keys %{$ref->{$language}}) {
     foreach $element ( sort keys %{$ref->{$language}->{$item}}) {
       print "Language: $language, $item, $element\n";
-      #system("mkdir -p $language/$item");
+      system("mkdir -p $language/$item");
       foreach $foo (@{$ref->{$language}->{$item}->{$element}}) {
 	print "filename: $language/$item/$foo->{filename} contains phrase \"$foo->{phrase}\"\n";
 	# insert command to verify or generate files here
-	# system("say -o $language/$item/$foo->{filename} \"$foo->{phrase}\"");
+	system("swift -o $language/$item/$foo->{filename} \"$foo->{phrase}\"");
       }
     }
   }
