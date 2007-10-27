ec6310f56b88ab14d0270f36586f88d6940ed6b0
Łukasz Zwierko <lzwierko@gmail.com>
2007-10-27 10:31:36 +0000

    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@6065 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ src/mod/endpoints/mod_opal/opalh323_backend.h
@@ -0,0 +1,28 @@
+/*
+ * opalh323_backend.cpp
+ *
+ * Backend for OpalH323 module, implements 
+ * H323 handling via OPAL library
+ *
+ * Copyright (c) 2007 Lukasz Zwierko (lzwierko@gmail.com)
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ *
+ * The Initial Developer of the Original Code is Lukasz Zwierko (lzwierko@gmail.com)
+ *
+ * Contributor(s):
+ *
+ * $Log: opalh323_backend.cpp,v $
+ *
+ * Revision 1.00  2007/10/24 07:29:52  lzwierko
+ * Initial revision
+ */
