eff23152c1c43861927378198591f07669cc1f2a
Anthony Minessale <anthony.minessale@gmail.com>
2007-10-31 20:37:30 +0000

    fix path bs
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@6110 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_ivr_play_say.c
+++ src/switch_ivr_play_say.c
@@ -675,25 +675,14 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_play_file(switch_core_session_t *sess
 	}
 
 	if (!strstr(file, SWITCH_URL_SEPARATOR)) {
-		if (switch_is_file_path(file)) {
-			char *new_file;
-			uint32_t len;
-			len = (uint32_t) strlen(file) + (uint32_t) strlen(prefix) + 10;
-			new_file = switch_core_session_alloc(session, len);
-			snprintf(new_file, len, "%s%s%s", prefix, SWITCH_PATH_SEPARATOR, file);
-			file = new_file;
+		if (!switch_is_file_path(file)) {
+			file = switch_core_session_sprintf(session, "%s%s%s", prefix, SWITCH_PATH_SEPARATOR, file);
 		}
-
 		if ((ext = strrchr(file, '.'))) {
 			ext++;
 		} else {
-			char *new_file;
-			uint32_t len;
 			ext = read_codec->implementation->iananame;
-			len = (uint32_t) strlen(file) + (uint32_t) strlen(ext) + 2;
-			new_file = switch_core_session_alloc(session, len);
-			snprintf(new_file, len, "%s.%s", file, ext);
-			file = new_file;
+			file = switch_core_session_sprintf(session, "%s.%s", file, ext);
 			asis = 1;
 		}
 	}
