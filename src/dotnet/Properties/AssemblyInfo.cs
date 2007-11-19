6684778bc6abb5db48449637dfc91c54f5ac820f
Brian West <brian@freeswitch.org>
2007-11-19 15:24:49 +0000

    movin
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@6328 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/dotnet/Properties/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyTitle("Freeswitch API")]
-[assembly: AssemblyDescription("mod_mono allows you to load .NET assemblies that can interact with Freeswitch's module system.")]
