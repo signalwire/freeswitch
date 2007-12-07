0ca450e2fb669d7991d14f35cfc814695f5ba144
Anthony Minessale <anthony.minessale@gmail.com>
2007-12-07 16:11:50 +0000

    don't hangup when transfering
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@6565 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_ivr_bridge.c
+++ src/switch_ivr_bridge.c
@@ -464,8 +464,10 @@ static switch_status_t signal_bridge_on_hangup(switch_core_session_t *session)
 		switch_channel_set_variable(channel, SWITCH_BRIDGE_VARIABLE, NULL);
 		switch_channel_set_variable(other_channel, SWITCH_BRIDGE_VARIABLE, NULL);
 
-		if (switch_channel_test_flag(other_channel, CF_ANSWERED) && switch_channel_get_state(other_channel) < CS_HANGUP
-			&& switch_true(switch_channel_get_variable(other_channel, SWITCH_HANGUP_AFTER_BRIDGE_VARIABLE))) {
+		if (switch_channel_test_flag(other_channel, CF_ANSWERED) && 
+			switch_channel_get_state(other_channel) < CS_HANGUP &&
+			!switch_channel_test_flag(other_channel, CF_TRANSFER) &&
+			switch_true(switch_channel_get_variable(other_channel, SWITCH_HANGUP_AFTER_BRIDGE_VARIABLE))) {
 			switch_channel_hangup(other_channel, switch_channel_get_cause(channel));
 		} else {
 			switch_channel_set_state(other_channel, CS_EXECUTE);
@@ -684,6 +686,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_multi_threaded_bridge(switch_core_ses
 
 	if (switch_channel_test_flag(peer_channel, CF_ANSWERED) && 
 		switch_channel_get_state(caller_channel) < CS_HANGUP && 
+		!switch_channel_test_flag(caller_channel, CF_TRANSFER) &&
 		switch_true(switch_channel_get_variable(caller_channel, SWITCH_HANGUP_AFTER_BRIDGE_VARIABLE))) {
 		switch_channel_hangup(caller_channel, switch_channel_get_cause(peer_channel));
 	}
