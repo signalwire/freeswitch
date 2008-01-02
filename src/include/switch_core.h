ee82edb170b398f81bffbdafe16f617d926b7b9a
Michael Jerris <mike@jerris.com>
2008-01-02 07:00:07 +0000

    correct param requirements
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@7058 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/include/switch_core.h
+++ src/include/switch_core.h
@@ -716,7 +716,7 @@ SWITCH_DECLARE(void) switch_core_service_session(_In_ switch_core_session_t *ses
   \paeam flags flags to use
   \return the cause code of the attempted call
 */
-SWITCH_DECLARE(switch_call_cause_t) switch_core_session_outgoing_channel(_In_ switch_core_session_t *session,
+SWITCH_DECLARE(switch_call_cause_t) switch_core_session_outgoing_channel(_In_opt_ switch_core_session_t *session,
 																		 _In_z_ const char *endpoint_name,
 																		 _In_ switch_caller_profile_t *caller_profile,
 																		 _Inout_ switch_core_session_t **new_session,
