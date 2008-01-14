221c32e846f610622a7c80a7c483961a5eb8bc29
Anthony Minessale <anthony.minessale@gmail.com>
2008-01-14 15:44:05 +0000

    add process_cdr variable
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@7213 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_core_state_machine.c
+++ src/switch_core_state_machine.c
@@ -424,9 +424,9 @@ SWITCH_DECLARE(void) switch_core_session_run(switch_core_session_t *session)
 
 					if (!switch_strlen_zero(var)) {
 						
-						if (!strcasecmp(var, "a_only") && !switch_channel_test_flag(session->channel, CF_ORIGINATOR)) {
+						if (!strcasecmp(var, "a_only") && switch_channel_get_originator_caller_profile(session->channel)) {
 							do_extra_handlers = 0;
-						} else if (!strcasecmp(var, "b_only") && switch_channel_test_flag(session->channel, CF_ORIGINATOR)) {
+						} else if (!strcasecmp(var, "b_only") && switch_channel_get_originatee_caller_profile(session->channel)) {
 							do_extra_handlers = 0;
 						} else if (!switch_true(var)) {
                             do_extra_handlers = 0;
