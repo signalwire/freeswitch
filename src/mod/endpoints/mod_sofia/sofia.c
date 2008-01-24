bbd7f87a2ad7876b0a98f9b0e6aecc2ea11ea6c2
Anthony Minessale <anthony.minessale@gmail.com>
2008-01-24 15:09:55 +0000

    set sdp flag in more places
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@7344 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -1553,7 +1553,7 @@ static void sofia_handle_sip_i_state(switch_core_session_t *session, int status,
 						switch_channel_set_variable(channel, SWITCH_ENDPOINT_DISPOSITION_VARIABLE, "RECEIVED");
 						switch_set_flag_locked(tech_pvt, TFLAG_READY);
 						switch_channel_set_state(channel, CS_INIT);
-
+						switch_set_flag(tech_pvt, TFLAG_SDP);
 						if (replaces_str) {
 							home = su_home_new(sizeof(*home));
 							switch_assert(home != NULL);
@@ -1748,6 +1748,7 @@ static void sofia_handle_sip_i_state(switch_core_session_t *session, int status,
 							if (switch_channel_get_state(channel) == CS_HIBERNATE) {
 								switch_set_flag_locked(tech_pvt, TFLAG_READY);
 								switch_channel_set_state(channel, CS_INIT);
+								switch_set_flag(tech_pvt, TFLAG_SDP);
 							}
 							goto done;
 						}
