d2ef447ed16537ba7dfaf4309caef68944f6379c
Michael Jerris <mike@jerris.com>
2008-02-17 04:46:07 +0000

    add mod_voipcodecs to msvc build
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@7649 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ libs/voipcodecs/src/msvc/voipcodecs.h
@@ -0,0 +1,50 @@
+/*
+ * VoIPcodecs - a series of DSP components for telephony
+ *
+ * voipcodecs.h - The head guy amongst the headers
+ *
+ * Written by Steve Underwood <steveu@coppice.org>
+ *
+ * Copyright (C) 2003 Steve Underwood
+ *
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the Lesser GNU General Public License version 2.1, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+
+/*! \file */
+
+#if !defined(_VOIPCODECS_H_)
+#define _VOIPCODECS_H_
+
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <time.h>
+
+#include <voipcodecs/telephony.h>
+#include <voipcodecs/bit_operations.h>
+#include <voipcodecs/bitstream.h>
+#include <voipcodecs/g711.h>
+#include <voipcodecs/g722.h>
+#include <voipcodecs/g726.h>
+#include <voipcodecs/gsm0610.h>
+#include <voipcodecs/ima_adpcm.h>
+#include <voipcodecs/lpc10.h>
+#include <voipcodecs/oki_adpcm.h>
+
+#endif
+/*- End of file ------------------------------------------------------------*/
