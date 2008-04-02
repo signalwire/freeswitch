1c52292c9b2d4c68e888bd3d5a119083fbe569a8
Brian West <brian@freeswitch.org>
2008-04-02 07:33:50 +0000

    This can be used to test DTMF connectivity from remote devices and read back the DTMF verbally.
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@8002 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- scripts/dtmftest.js
+++ scripts/dtmftest.js
@@ -3,6 +3,7 @@ function onPlayFile(s, type, obj, arg)
     try {
       if (type == "dtmf") {
 	console_log("info", "DTMF digit: " + s.name + " [" + obj.digit + "] len [" + obj.duration + "]\n\n");
+	session.execute("phrase", "spell," + obj.digit);
       }
 
     }  catch (e) {
