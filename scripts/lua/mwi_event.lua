5743b7e15285bc84cb3117506f2bf2a33242d6e5
Brian West <brian@freeswitch.org>
2008-04-26 01:00:34 +0000

    be consistent
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@8205 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- scripts/lua/mwi_event.lua
+++ scripts/lua/mwi_event.lua
@@ -1,6 +1,6 @@
 -- This is an example of sending an event via luarun from the cli
 -- Edit to your liking.  luarun mwi_event.lua
-freeswitch.console_log("info", "Sending MWI Event using Lua\n");
+freeswitch.console_log("info", "Lua in da house!!!\n");
 
 local event = freeswitch.Event("message_waiting");
 event:add_header("MWI-Messages-Waiting", "no");
