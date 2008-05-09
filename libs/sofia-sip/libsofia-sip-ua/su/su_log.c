439ad8460e78c3f7113505ba1fdbb446f52a1904
Michael Jerris <mike@jerris.com>
2008-05-09 17:46:43 +0000

    Wed May  7 10:59:52 EDT 2008  Pekka Pessi <first.last@nokia.com>   * su_log.c: allow multiple calls su_log_soft_set_level() to change logging level
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@8337 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/sofia-sip/libsofia-sip-ua/su/su_log.c
+++ libs/sofia-sip/libsofia-sip-ua/su/su_log.c
@@ -240,11 +240,13 @@ void su_log_soft_set_level(su_log_t *log, unsigned level)
 {
   if (log == NULL)
     log = su_log_default;
-  if (log->log_init > 1)
+  if (log->log_init == 1)
     return;
+
   if (log->log_env && getenv(log->log_env)) {
     su_log_init(log);
-  } else {
+  }
+  else {
     log->log_level = level;
     log->log_init = 2;
 
