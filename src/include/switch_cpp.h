8bb2182db18824fc6c121ae514c9e4a233ac87fc
Anthony Minessale <anthony.minessale@gmail.com>
2008-05-13 21:07:01 +0000

    update
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@8388 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/include/switch_cpp.h
+++ src/include/switch_cpp.h
@@ -191,8 +191,8 @@ class CoreSession {
 	SWITCH_DECLARE(void *)getPrivate(char *var);
 	SWITCH_DECLARE(const char *)getVariable(char *var);
 	SWITCH_DECLARE(switch_status_t) process_callback_result(char *ret);	
-	SWITCH_DECLARE(void) CoreSession::say(const char *tosay, const char *module_name, const char *say_type, const char *say_method);
-	SWITCH_DECLARE(void) CoreSession::sayPhrase(const char *phrase_name, const char *phrase_data = "", const char *phrase_lang = NULL);
+	SWITCH_DECLARE(void) say(const char *tosay, const char *module_name, const char *say_type, const char *say_method);
+	SWITCH_DECLARE(void) sayPhrase(const char *phrase_name, const char *phrase_data = "", const char *phrase_lang = NULL);
 
 	/** \brief Record to a file
 	 * \param filename 
