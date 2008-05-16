22cb7b134ac25f61f7cb374c4420170d1d8b66ca
Anthony Minessale <anthony.minessale@gmail.com>
2008-05-16 17:29:22 +0000

    update
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@8446 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_core_db.c
+++ src/switch_core_db.c
@@ -96,7 +96,7 @@ SWITCH_DECLARE(int) switch_core_db_exec(switch_core_db_t *db, const char *sql, s
 
 	if (errmsg) {
 		*errmsg = err;
-	} else {
+	} else if (err) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "SQL ERR [%s]\n", err);
 		switch_core_db_free(err);
 		err = NULL;
