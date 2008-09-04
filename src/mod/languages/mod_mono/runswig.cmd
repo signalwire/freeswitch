86d26dc54730514613425c70fa983206c3fc6140
Michael Giagnocavo <mgg@giagnocavo.net>
2008-09-04 23:13:56 +0000

    Mono2.0 does not seem to need swigStringFix
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@9454 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/languages/mod_mono/runswig.cmd
+++ src/mod/languages/mod_mono/runswig.cmd
@@ -3,4 +3,3 @@ del swig.csx
 for %%X in (*.cs) do type %%X >> swig.csx
 move swig.csx ..\mod_mono_managed\swig.cs
 del *.cs
-REM ..\mod_mono_managed\swigStringFix ..\mod_mono_managed\swig\freeswitchPINVOKE.cs > ..\mod_mono_managed\swig\freeswitchPINVOKE_fixed.cs
