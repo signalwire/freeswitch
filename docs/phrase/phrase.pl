7417910f928ea382bceb2b4c73d3f12307337190
Brian West <brian@freeswitch.org>
2008-09-22 08:55:01 +0000

    tweaks to phrase file
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@9610 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- docs/phrase/phrase.pl
+++ docs/phrase/phrase.pl
@@ -6,17 +6,17 @@
 use XML::Simple;
 use Data::Dumper;
 
-my $ref = XMLin("phrase_fr.xml");
+my $ref = XMLin("phrase_en.xml");
 
 foreach $language ( sort keys %{$ref}) {
   foreach $item ( sort keys %{$ref->{$language}}) {
     foreach $element ( sort keys %{$ref->{$language}->{$item}}) {
-      print "Language: $language, $item, $element\n";
+      #print "Language: $language, $item, $element\n";
       system("mkdir -p $language/$item");
       foreach $foo (@{$ref->{$language}->{$item}->{$element}}) {
-	print "filename: $language/$item/$foo->{filename} contains phrase \"$foo->{phrase}\"\n";
+	#print "filename: $language/$item/$foo->{filename} contains phrase \"$foo->{phrase}\"\n";
 	# insert command to verify or generate files here
-	system("swift -n Isabelle -o $language/$item/$foo->{filename} \"$foo->{phrase}\"");
+	print "$item/8000/$foo->{filename}\n";
       }
     }
   }
