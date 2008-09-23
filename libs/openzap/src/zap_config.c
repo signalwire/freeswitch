f79ea715d7ca267b1c38882c2fbfa0d9f46d0377
Michael Jerris <mike@jerris.com>
2008-09-23 15:58:44 +0000

    (OPENZAP-17) make # not a special char in config files now.
    git-svn-id: http://svn.openzap.org/svn/openzap/trunk@562 a93c3328-9c30-0410-af19-c9cd2b2d52af

--- libs/openzap/src/zap_config.c
+++ libs/openzap/src/zap_config.c
@@ -157,7 +157,7 @@ int zap_config_next_pair(zap_config_t *cfg, char **var, char **val)
 		}
 
 
-		if (((end = strchr(*var, '#')) || (end = strchr(*var, ';'))) && *(end+1) == *end) {
+		if ((end = strchr(*var, ';')) && *(end+1) == *end) {
 			*end = '\0';
 			end--;
 		} else if ((end = strchr(*var, '\n')) != 0) {
