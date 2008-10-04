42bd1e4097705ebdd7f9caf0ebf622a969688018
Anthony Minessale <anthony.minessale@gmail.com>
2008-10-04 07:47:30 +0000

    linux build system fixups
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@9829 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/languages/mod_managed/runswig.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-swig -I../../../include -v -O -c++ -csharp -namespace FreeSWITCH.Native -dllimport mod_managed freeswitch.i
-rm -f ./managed/swig.cs
-cat *.cs > ./managed/swig.cs
-rm -f *.cs
