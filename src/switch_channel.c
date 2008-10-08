4a013d7957efef3e587fcc096a909f3a975decff
Anthony Minessale <anthony.minessale@gmail.com>
2008-10-08 18:12:11 +0000

    s/Pre-Answer/Ring-Ready/
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@9895 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_channel.c
+++ src/switch_channel.c
@@ -1458,7 +1458,7 @@ SWITCH_DECLARE(switch_status_t) switch_channel_perform_mark_ring_ready(switch_ch
 	switch_event_t *event;
 
 	if (!switch_channel_test_flag(channel, CF_RING_READY)) {
-		switch_log_printf(SWITCH_CHANNEL_ID_LOG, file, func, line, NULL, SWITCH_LOG_NOTICE, "Ring-Ready %s!\n", channel->name);
+		switch_log_printf(SWITCH_CHANNEL_ID_LOG, file, func, line, NULL, SWITCH_LOG_NOTICE, "Pre-Answer %s!\n", channel->name);
 		switch_channel_set_flag(channel, CF_RING_READY);
 		if (channel->caller_profile && channel->caller_profile->times) {
 			switch_mutex_lock(channel->profile_mutex);
