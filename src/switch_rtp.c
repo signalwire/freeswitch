6d89c019181b6156d4da9d0206d78115f8709878
Anthony Minessale <anthony.minessale@gmail.com>
2008-10-14 23:22:39 +0000

    add debug line
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@10019 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -1606,6 +1606,7 @@ SWITCH_DECLARE(switch_size_t) switch_rtp_dequeue_dtmf(switch_rtp_t *rtp_session,
 	if (switch_queue_trypop(rtp_session->dtmf_data.dtmf_inqueue, &pop) == SWITCH_STATUS_SUCCESS) {
 		_dtmf = (switch_dtmf_t *) pop;
 		*dtmf = *_dtmf;
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "RTP RECV DTMF %c:%d\n", dtmf->digit, dtmf->duration);
 		bytes++;
 	}
 	switch_mutex_unlock(rtp_session->dtmf_data.dtmf_mutex);
