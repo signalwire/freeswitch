fa459336d804a83a5bf96cbad624217b0106a320
Anthony Minessale <anthony.minessale@gmail.com>
2008-10-24 20:30:33 +0000

    add data to params
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@10145 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/dialplans/mod_dialplan_xml/mod_dialplan_xml.c
+++ src/mod/dialplans/mod_dialplan_xml/mod_dialplan_xml.c
@@ -205,6 +205,7 @@ static switch_status_t dialplan_xml_locate(switch_core_session_t *session, switc
 	switch_assert(params);
 
 	switch_channel_event_set_data(channel, params);
+	switch_caller_profile_event_set_data(caller_profile, "Hunt", params);
 
 	status = switch_xml_locate("dialplan", NULL, NULL, NULL, root, node, params);
 	switch_event_destroy(&params);
