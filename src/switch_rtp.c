abeba3204c88246e4117ba0270f0eb7244b7a15d
Anthony Minessale <anthony.minessale@gmail.com>
2008-11-18 23:44:41 +0000

    update
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@10449 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -771,7 +771,7 @@ SWITCH_DECLARE(switch_status_t) switch_rtp_change_interval(switch_rtp_t *rtp_ses
 		} else {
 			memset(&rtp_session->timer, 0, sizeof(rtp_session->timer));
 			switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR,
-							  "Problem RE-Starting timer [%s] %d bytes per %dms\n", rtp_session->timer_name, samples_per_interval, ms_per_packet);
+							  "Problem RE-Starting timer [%s] %d bytes per %dms\n", rtp_session->timer_name, samples_per_interval, ms_per_packet / 1000);
 			return SWITCH_STATUS_FALSE;
 		}
 	}
