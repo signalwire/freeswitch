2dce7f8e07537a0df69292792d168f1cae41265e
Michael Jerris <mike@jerris.com>
2008-12-09 16:39:56 +0000

    C&P fail
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@10680 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -3498,7 +3498,7 @@ void sofia_handle_sip_i_info(nua_t *nua, sofia_profile_t *profile, nua_handle_t 
 					int tmp;
 					/* move signal_ptr where we need it (right past Signal=) */
 					signal_ptr = signal_ptr + 7;
-					if (*strlen && strlen(signal_ptr) == 1 && is_dtmf(*signal_ptr)) {
+					if (*signal_ptr && strlen(signal_ptr) == 1 && is_dtmf(*signal_ptr)) {
 						dtmf.digit = *signal_ptr;
 					} else {
 						tmp = atoi(signal_ptr);
