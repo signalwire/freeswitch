decdc849841a188399160230b4c209ad3ec7726e
Michael Jerris <mike@jerris.com>
2008-12-16 20:01:12 +0000

    Thu Dec 11 06:02:09 CST 2008  Pekka Pessi <first.last@nokia.com>   * sdp_print.c: print sdptl in lowercase, too
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@10817 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/sofia-sip/libsofia-sip-ua/sdp/sdp_print.c
+++ libs/sofia-sip/libsofia-sip-ua/sdp/sdp_print.c
@@ -588,7 +588,7 @@ static void print_media(sdp_printer_t *p,
     case sdp_proto_udp:   proto = "udp"; break;
     case sdp_proto_rtp:   proto = "RTP/AVP"; break;
     case sdp_proto_srtp:  proto = "RTP/SAVP"; break;
-    case sdp_proto_udptl: proto = "UDPTL"; break;
+    case sdp_proto_udptl: proto = "udptl"; break;
     case sdp_proto_tls:   proto = "tls"; break;
     default:              proto = m->m_proto_name; break;
     }
