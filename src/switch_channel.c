d6290803ac9d9dbe70ab2b6ff63bc216a1481659
Anthony Minessale <anthony.minessale@gmail.com>
2009-01-08 16:43:07 +0000

    add bridge/unbridge too
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@11098 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_channel.c
+++ src/switch_channel.c
@@ -1214,6 +1214,8 @@ SWITCH_DECLARE(void) switch_channel_event_set_extended_data(switch_channel_t *ch
 		event->event_id == SWITCH_EVENT_CHANNEL_ANSWER ||
 		event->event_id == SWITCH_EVENT_CHANNEL_PARK ||
 		event->event_id == SWITCH_EVENT_CHANNEL_UNPARK ||
+		event->event_id == SWITCH_EVENT_CHANNEL_BRIDGE ||
+		event->event_id == SWITCH_EVENT_CHANNEL_UNBRIDGE ||
 		event->event_id == SWITCH_EVENT_CHANNEL_PROGRESS ||
 		event->event_id == SWITCH_EVENT_CHANNEL_PROGRESS_MEDIA ||
 		event->event_id == SWITCH_EVENT_CHANNEL_HANGUP ||
