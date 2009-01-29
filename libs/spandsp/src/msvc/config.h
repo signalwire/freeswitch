deedf9fd34caf0beadfdbdaf6ed772ec4cae3db2
Michael Jerris <mike@jerris.com>
2009-01-29 04:55:54 +0000

    add src/msvc/config.h
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@11551 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ libs/spandsp/src/msvc/config.h
@@ -0,0 +1,42 @@
+/*
+ * SpanDSP - a series of DSP components for telephony
+ *
+ * inttypes.h - a fudge for MSVC, which lacks this header
+ *
+ * Written by Steve Underwood <steveu@coppice.org>
+ *
+ * Copyright (C) 2006 Michael Jerris
+ *
+ *
+ * This file is released in the public domain.
+ *
+ */
+
+#if !defined(_SPANDSP_CONFIG_H_)
+#define _SPANDSP_CONFIG_H_
+
+#define HAVE_SINF
+#define HAVE_COSF
+#define HAVE_TANF
+#define HAVE_ASINF
+#define HAVE_ACOSF
+#define HAVE_ATANF
+#define HAVE_ATAN2F
+#define HAVE_CEILF
+#define HAVE_FLOORF
+#define HAVE_POWF
+#define HAVE_EXPF
+#define HAVE_LOGF
+#define HAVE_LOG10F
+#define HAVE_MATH_H
+#define HAVE_TGMATH_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
