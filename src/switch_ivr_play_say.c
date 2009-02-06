14aba33e38cecc456a8c07339725d7ae679b796c
Anthony Minessale <anthony.minessale@gmail.com>
2009-02-06 20:44:53 +0000

    add enable_file_write_buffering variable
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@11677 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_ivr_play_say.c
+++ src/switch_ivr_play_say.c
@@ -420,7 +420,10 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_record_file(switch_core_session_t *se
 		}
 	}
 
-	fh->pre_buffer_datalen = SWITCH_DEFAULT_FILE_BUFFER_LEN;
+	vval = switch_channel_get_variable(channel, "enable_file_write_buffering");
+	if (!vval || !switch_true(vval)) {
+		fh->pre_buffer_datalen = SWITCH_DEFAULT_FILE_BUFFER_LEN;
+	}
 
 	if (switch_core_file_open(fh,
 							  file,
