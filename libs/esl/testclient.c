0dc800c78e8c379fc4f7176da54a59d381431b1e
Brian West <brian@freeswitch.org>
2009-02-06 16:05:46 +0000

    doh
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@11664 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/testclient.c
+++ libs/esl/testclient.c
@@ -9,21 +9,7 @@ int main(void)
 
 	esl_connect(&handle, "localhost", 8021, "ClueCon");
 
-	esl_send_recv(&handle, "api conference 3100 play /root/sr8k.raw 1\n\n");
-	
-	esl_send_recv(&handle, "api uuid_transfer aa1cedb1-5abc-4154-b068-9a0d1be6a7b6 3101\n\n");
-	
-	esl_send_recv(&handle, "api conference 3101 play /root/sr8k.raw 2\n\n");
-	
-	esl_send_recv(&handle, "api uuid_transfer aa1cedb1-5abc-4154-b068-9a0d1be6a7b6 3102\n\n");
-	
-	esl_send_recv(&handle, "api conference 3102 play /root/sr8k.raw 3\n\n");
-	
-	esl_send_recv(&handle, "api uuid_transfer aa1cedb1-5abc-4154-b068-9a0d1be6a7b6 3103\n\n");
-	
-	esl_send_recv(&handle, "api conference 3103 play /root/sr8k.raw 4\n\n");
-	
-	esl_send_recv(&handle, "api uuid_transfer aa1cedb1-5abc-4154-b068-9a0d1be6a7b6 3104\n\n");
+	esl_send_recv(&handle, "api status\n\n");
 
 	esl_disconnect(&handle);
 	
