0aa16a65d67b3edbdbb7a8b26bd1e62abbc9e09c
Anthony Minessale <anthony.minessale@gmail.com>
2009-02-07 21:22:12 +0000

    lisdexic
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@11682 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_ivr_play_say.c
+++ src/switch_ivr_play_say.c
@@ -421,7 +421,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_record_file(switch_core_session_t *se
 	}
 
 	vval = switch_channel_get_variable(channel, "enable_file_write_buffering");
-	if (!vval || !switch_true(vval)) {
+	if (!vval || switch_true(vval)) {
 		fh->pre_buffer_datalen = SWITCH_DEFAULT_FILE_BUFFER_LEN;
 	}
 
