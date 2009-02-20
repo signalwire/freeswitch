ba5322f6c0ede4d570fb4baebe35e6d66dcde0ac
Anthony Minessale <anthony.minessale@gmail.com>
2009-02-20 16:54:08 +0000

    change example
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@12181 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/perl/single_command.pl
+++ libs/esl/perl/single_command.pl
@@ -1,6 +1,8 @@
 require ESL;
 
-my $command = join(" ", @ARGV);
+my $command = shift;
+my $args = join(" ", @ARGV);
+
 my $con = new ESL::ESLconnection("localhost", "8021", "ClueCon");
-my $e = $con->sendRecv("api $command");
+my $e = $con->api($command, $args);
 print $e->getBody();
