bb52a206c23744ba303e410d4a4355e2cef11dd0
Brian West <brian@freeswitch.org>
2009-02-20 22:36:10 +0000

    oops remove the perl example inside here
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@12206 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/python/server.py
+++ libs/esl/python/server.py
@@ -4,8 +4,6 @@ import SocketServer
 import pickle
 from ESL import *
 
-#eslSetLogLevel(7)
-
 class ESLRequestHandler(SocketServer.BaseRequestHandler ):
 	def setup(self):
 		print self.client_address, 'connected!'
@@ -28,39 +26,5 @@ class ESLRequestHandler(SocketServer.BaseRequestHandler ):
 #server host is a tuple ('host', port)
 server = SocketServer.ThreadingTCPServer(('', 8040), ESLRequestHandler)
 server.serve_forever()
-
-
-
-
-#require ESL;
-#use IO::Socket::INET;
-#
-#my $ip = "127.0.0.1";
-#my $sock = new IO::Socket::INET ( LocalHost => $ip,  LocalPort => '8040',  Proto => 'tcp',  Listen => 1,  Reuse => 1 );
-#die "Could not create socket: $!\n" unless $sock;
-#
-#for(;;) {
-#  my $new_sock = $sock->accept();
-#  my $pid = fork();
-#  if ($pid) {
-#    close($new_sock);
-#    next;
-#  }
-#
-#  my $host = $new_sock->sockhost();
-#  my $fd = fileno($new_sock);
-#  
-#  my $con = new ESL::ESLconnection($fd);
-#  my $info = $con->getInfo();
-#
-#  print $info->serialize();
-#
-#  my $uuid = $info->getHeader("unique-id");
-#
-#  $con->execute("answer", "", $uuid);
-#  $con->execute("playback", "/ram/swimp.raw", $uuid);
-#
-#  close($new_sock);
-#}
 
 
