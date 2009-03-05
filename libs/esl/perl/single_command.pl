f070469b2c90742e12d76d3cfd2363a6770135fb
Anthony Minessale <anthony.minessale@gmail.com>
2009-03-05 15:18:54 +0000

    revert
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@12461 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/perl/single_command.pl
+++ libs/esl/perl/single_command.pl
@@ -4,5 +4,5 @@ my $command = shift;
 my $args = join(" ", @ARGV);
 
 my $con = new ESL::ESLconnection("localhost", "8021", "ClueCon");
-my $e = $con->bgapi($command, $args);
+my $e = $con->api($command, $args);
 print $e->getBody();
