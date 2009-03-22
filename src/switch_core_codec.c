9998cf9f4c78728dd6bb155ab865e9199c4f525f
Brian West <brian@freeswitch.org>
2009-03-22 15:50:34 +0000

    fix ilbc snafu
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@12709 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_core_codec.c
+++ src/switch_core_codec.c
@@ -437,7 +437,7 @@ SWITCH_DECLARE(switch_status_t) switch_core_codec_init(switch_codec_t *codec, co
 		return SWITCH_STATUS_GENERR;
 	}
 
-	if (!strcasecmp(codec_name, "ilbc") && mode && strncasecmp(mode, "mode=", 5)) {
+	if (!strcasecmp(codec_name, "ilbc") && mode && !strncasecmp(mode, "mode=", 5)) {
 		int mms;
 		mode += 5;
 		if (mode) {
