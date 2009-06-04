32a1336d8a5815bc06feb461a8a22a9c202f8ddb
Brian West <brian@freeswitch.org>
2009-06-04 05:48:59 +0000

    don't need contact here its already set on the nh .. this results on double contact headers on unregister
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@13609 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/sofia_reg.c
+++ src/mod/endpoints/mod_sofia/sofia_reg.c
@@ -335,7 +335,6 @@ void sofia_reg_check_gateway(sofia_profile_t *profile, time_t now)
 							   TAG_IF(user_via, SIPTAG_VIA_STR(user_via)),
 							   SIPTAG_FROM_STR(gateway_ptr->register_from),
 							   SIPTAG_TO_STR(gateway_ptr->register_from),
-							   SIPTAG_CONTACT_STR(gateway_ptr->register_contact),
 							   SIPTAG_EXPIRES_STR(gateway_ptr->expires_str),
 							   NUTAG_REGISTRAR(gateway_ptr->register_proxy),
 							   NUTAG_OUTBOUND("no-options-keepalive"), NUTAG_OUTBOUND("no-validate"), NUTAG_KEEPALIVE(0), TAG_NULL());
