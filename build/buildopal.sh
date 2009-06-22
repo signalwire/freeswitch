59d0e2b4b4a1032e77f3598a177be4deee3408f6
Raymond Chandler <intralanman@freeswitch.org>
2009-06-22 19:40:30 +0000

    [temporary] bsd-friendly hack
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@13898 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- build/buildopal.sh
+++ build/buildopal.sh
@@ -1,17 +1,20 @@
 #!/bin/sh
+uname -a | grep -qi bsd && MAKE=gmake || MAKE=make
 FS_DIR=`pwd`
+
 cd /root
 svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/ptlib/trunk ptlib
 cd ptlib
 ./configure --prefix=/usr
-make
-make install
+${MAKE}
+${MAKE} install
+
 cd ..
 svn co https://opalvoip.svn.sourceforge.net/svnroot/opalvoip/opal/branches/v3_6 opal 
 cd opal
 export PKG_CONFIG_PATH=/usr/lib/pkgconfig 
 ./configure --prefix=/usr
-make
-make install
+${MAKE}
+${MAKE} install
 cd ${FS_DIR}
-make mod_opal-install
+${MAKE} mod_opal-install
