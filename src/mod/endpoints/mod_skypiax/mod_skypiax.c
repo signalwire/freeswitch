11f493015662d40707f81a9745d46cdbcd50a2c7
Giovanni Maruzzelli <gmaruzz@gmail.com>
2009-07-24 14:22:38 +0000

    skypiax: compiles AND links on windows ;-)
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@14343 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_skypiax/mod_skypiax.c
+++ src/mod/endpoints/mod_skypiax/mod_skypiax.c
@@ -36,6 +36,61 @@
 
 #include "skypiax.h"
 
+#ifdef WIN32
+/***************/
+// from http://www.openasthra.com/c-tidbits/gettimeofday-function-for-windows/
+
+#include <time.h>
+     
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
+#else
+#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
+#endif
+     
+    struct timezone 
+    {
+      int  tz_minuteswest; /* minutes W of Greenwich */
+      int  tz_dsttime;     /* type of dst correction */
+    };
+     
+    int gettimeofday(struct timeval *tv, struct timezone *tz)
+    {
+      FILETIME ft;
+      unsigned __int64 tmpres = 0;
+      static int tzflag;
+     
+      if (NULL != tv)
+      {
+        GetSystemTimeAsFileTime(&ft);
+     
+        tmpres |= ft.dwHighDateTime;
+        tmpres <<= 32;
+        tmpres |= ft.dwLowDateTime;
+     
+        /*converting file time to unix epoch*/
+        tmpres /= 10;  /*convert into microseconds*/
+        tmpres -= DELTA_EPOCH_IN_MICROSECS; 
+        tv->tv_sec = (long)(tmpres / 1000000UL);
+        tv->tv_usec = (long)(tmpres % 1000000UL);
+      }
+     
+      if (NULL != tz)
+      {
+        if (!tzflag)
+        {
+          _tzset();
+          tzflag++;
+        }
+        tz->tz_minuteswest = _timezone / 60;
+        tz->tz_dsttime = _daylight;
+      }
+     
+      return 0;
+    }
+/***************/
+#endif /* WIN32 */
+
 SWITCH_MODULE_LOAD_FUNCTION(mod_skypiax_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_skypiax_shutdown);
 SWITCH_MODULE_DEFINITION(mod_skypiax, mod_skypiax_load, mod_skypiax_shutdown, NULL);
--- src/mod/endpoints/mod_skypiax/skypiax.h
+++ src/mod/endpoints/mod_skypiax/skypiax.h
@@ -49,60 +49,6 @@
 //Windows macro  for FD_SET includes a warning C4127: conditional expression is constant
 #pragma warning(push)
 #pragma warning(disable:4127)
-
-/***************/
-// from http://www.openasthra.com/c-tidbits/gettimeofday-function-for-windows/
-
-#include <time.h>
-     
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif
-     
-    struct timezone 
-    {
-      int  tz_minuteswest; /* minutes W of Greenwich */
-      int  tz_dsttime;     /* type of dst correction */
-    };
-     
-    int gettimeofday(struct timeval *tv, struct timezone *tz)
-    {
-      FILETIME ft;
-      unsigned __int64 tmpres = 0;
-      static int tzflag;
-     
-      if (NULL != tv)
-      {
-        GetSystemTimeAsFileTime(&ft);
-     
-        tmpres |= ft.dwHighDateTime;
-        tmpres <<= 32;
-        tmpres |= ft.dwLowDateTime;
-     
-        /*converting file time to unix epoch*/
-        tmpres /= 10;  /*convert into microseconds*/
-        tmpres -= DELTA_EPOCH_IN_MICROSECS; 
-        tv->tv_sec = (long)(tmpres / 1000000UL);
-        tv->tv_usec = (long)(tmpres % 1000000UL);
-      }
-     
-      if (NULL != tz)
-      {
-        if (!tzflag)
-        {
-          _tzset();
-          tzflag++;
-        }
-        tz->tz_minuteswest = _timezone / 60;
-        tz->tz_dsttime = _daylight;
-      }
-     
-      return 0;
-    }
-/***************/
-
 #endif
 
 #define SAMPLERATE_SKYPIAX 16000
