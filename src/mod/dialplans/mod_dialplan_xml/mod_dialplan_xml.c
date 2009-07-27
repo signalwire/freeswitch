e1ef1f24730357fab2ee0eaaa7416caec23a2634
Anthony Minessale <anthony.minessale@gmail.com>
2009-07-27 16:59:32 +0000

    add min of day to time rules
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@14378 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/dialplans/mod_dialplan_xml/mod_dialplan_xml.c
+++ src/mod/dialplans/mod_dialplan_xml/mod_dialplan_xml.c
@@ -74,6 +74,7 @@ static int parse_exten(switch_core_session_t *session, switch_caller_profile_t *
 		const char *xwday = switch_xml_attr(xcond, "wday");
 		const char *xhour = switch_xml_attr(xcond, "hour");
 		const char *xminute = switch_xml_attr(xcond, "minute");
+		const char *xminday = switch_xml_attr(xcond, "minute_of_day");
 		switch_time_t ts = switch_micro_time_now();
 		int time_match = -1;
 		switch_time_exp_t tm;
@@ -119,6 +120,10 @@ static int parse_exten(switch_core_session_t *session, switch_caller_profile_t *
 
 		if (time_match && xminute) {
 			time_match = switch_number_cmp(xminute, tm.tm_min + 1);
+		}
+
+		if (time_match && xminday) {
+			time_match = switch_number_cmp(xminday, (tm.tm_hour * 60) + (tm.tm_min + 1));
 		}
 		
 		field = (char *) switch_xml_attr(xcond, "field");
