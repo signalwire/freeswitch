b4fbb358651674652f6ae726ffe11728b66a9c3b
Andrew Thompson <andrew@hijacked.us>
2009-08-18 05:33:42 +0000

    Winsock blows
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@14550 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/event_handlers/mod_erlang_event/ei_helpers.c
+++ src/mod/event_handlers/mod_erlang_event/ei_helpers.c
@@ -74,7 +74,11 @@ void ei_link(listener_t *listener, erlang_pid *from, erlang_pid *to) {
 	/* sum:  542 */
 
 	switch_mutex_lock(listener->sock_mutex);
+#ifdef WIN32
+	ret = send(listener->sockfd, msgbuf, index, 0);
+#else
 	ret = write(listener->sockfd, msgbuf, index);
+#endif
 	switch_mutex_unlock(listener->sock_mutex);
 }
 
