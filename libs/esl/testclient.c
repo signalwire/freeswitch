b87e6cdaf599740d738c0c44acadb3412fbf53bc
Anthony Minessale <anthony.minessale@gmail.com>
2009-08-24 14:04:59 +0000

    update example
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@14617 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/testclient.c
+++ libs/esl/testclient.c
@@ -11,6 +11,13 @@ int main(void)
 
 	esl_send_recv(&handle, "api status\n\n");
 
+	if (handle.last_sr_event && handle.last_sr_event->body) {
+		printf("%s\n", handle.last_sr_event->body);
+	} else {
+		// this is unlikely to happen with api or bgapi (which is hardcoded above) but prefix but may be true for other commands
+		printf("%s\n", handle.last_sr_reply);
+	}
+
 	esl_disconnect(&handle);
 	
 	return 0;
