b7f4b4cd2b30fc4eea9da38130baf7f99dd1e8bc
Brian West <brian@freeswitch.org>
2009-12-07 22:10:38 +0000

    don't seg if tech_pvt isn't there.
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@15837 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -3835,7 +3835,7 @@ static void sofia_handle_sip_i_state(switch_core_session_t *session, int status,
 		break;
 	case nua_callstate_completing:
 		nua_ack(nh,
-				TAG_IF(!zstr(tech_pvt->user_via), SIPTAG_VIA_STR(tech_pvt->user_via)),
+				TAG_IF(tech_pvt && !zstr(tech_pvt->user_via), SIPTAG_VIA_STR(tech_pvt->user_via)),
 				TAG_END());
 		break;
 	case nua_callstate_received:
