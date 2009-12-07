1bac80436d4d82641a0c5b6d3f464699e32194f3
Anthony Minessale <anthony.minessale@gmail.com>
2009-12-07 21:27:23 +0000

    add chan name to log lines
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@15835 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/switch_core_state_machine.c
+++ src/switch_core_state_machine.c
@@ -456,12 +456,14 @@ SWITCH_DECLARE(void) switch_core_session_hangup_state(switch_core_session_t *ses
 
 
 	if (switch_thread_self() != session->thread_id) {
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "thread mismatch skipping state handler.\n");
+		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "%s thread mismatch skipping state handler.\n", 
+						  switch_channel_get_name(session_channel));
 		return;
 	}
 
 	if (switch_test_flag(session, SSF_HANGUP)) {
-		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "handler already called, skipping state handler.\n");
+		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "%s handler already called, skipping state handler.\n",
+						  switch_channel_get_name(session_channel));
 		return;
 	}
 
