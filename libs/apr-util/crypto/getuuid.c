638790af30e268f769567e2d2f48c0fc75006a2d
Michael Jerris <mike@jerris.com>
2009-12-28 19:58:15 +0000

    make uuids a bit less likely to collide
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@16066 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/apr-util/crypto/getuuid.c
+++ libs/apr-util/crypto/getuuid.c
@@ -201,7 +201,7 @@ APU_DECLARE(void) apr_uuid_get(apr_uuid_t *uuid)
     d[7] = (unsigned char)(timestamp >> 48);
     d[6] = (unsigned char)(((timestamp >> 56) & 0x0F) | 0x10);
     /* clock_seq_hi_and_reserved, uint8 */
-    d[8] = (unsigned char)(((uuid_state_seqnum >> 8) & 0x3F) | 0x80);
+    d[8] = (unsigned char)(((++uuid_state_seqnum >> 8) & 0x3F) | 0x80);
     /* clock_seq_low, uint8 */
     d[9] = (unsigned char)uuid_state_seqnum;
     /* node, byte[6] */
