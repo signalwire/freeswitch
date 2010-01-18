108c17718a031d555bae31d2a8358b17bfa8da01
Brian West <brian@freeswitch.org>
2010-01-18 04:30:48 +0000

    ESL Mangaged examples and solution file for esl.dll
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@16353 d0543943-73ff-0310-b7d9-9358b9ac24b2

---
+++ libs/esl/managed/ManagedEslTest/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ManagedEslTest
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Connect to FreeSWITCH
+      ESLconnection eslConnection = new ESLconnection("localhost", "8021", "ClueCon");
+      // We want all Events (probably will want to change this depending on your needs)
+      eslConnection.sendRecv("event plain ALL");
+
+
+      // Grab Events until process is killed
+      while (eslConnection.connected() == 1)
+      {
+        ESLevent eslEvent = eslConnection.recvEvent();
+        Console.WriteLine(eslEvent.serialize(String.Empty));
+      }
+    }
+  }
+}
