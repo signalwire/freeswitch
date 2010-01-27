677246547bf13cbc5cffb2362a0375e6eaa95b11
Diego Toro <dftoro@yahoo.com>
2010-01-27 02:25:22 +0000

    Improve C# support. Rename properties like C#-style
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@16520 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- libs/esl/managed/ManagedEslTest/Program.cs
+++ libs/esl/managed/ManagedEslTest/Program.cs
@@ -9,14 +9,14 @@ namespace ManagedEslTest
       // Connect to FreeSWITCH
       ESLconnection eslConnection = new ESLconnection("localhost", "8021", "ClueCon");
       // We want all Events (probably will want to change this depending on your needs)
-      eslConnection.sendRecv("event plain ALL");
+      eslConnection.SendRecv("event plain ALL");
 
 
       // Grab Events until process is killed
-      while (eslConnection.connected() == 1)
+      while (eslConnection.Connected() == 1)
       {
-        ESLevent eslEvent = eslConnection.recvEvent();
-        Console.WriteLine(eslEvent.serialize(String.Empty));
+        ESLevent eslEvent = eslConnection.RecvEvent();
+        Console.WriteLine(eslEvent.Serialize(String.Empty));
       }
     }
   }
