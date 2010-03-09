e222f74cbb1406af6981ca1efb88ccac606fa384
Rupa Schomaker <rupa@rupa.com>
2010-03-09 19:00:47 +0000

    fix typo in typo fix :)
    git-svn-id: http://svn.freeswitch.org/svn/freeswitch/trunk@16956 d0543943-73ff-0310-b7d9-9358b9ac24b2

--- src/mod/applications/mod_nibblebill/mod_nibblebill.c
+++ src/mod/applications/mod_nibblebill/mod_nibblebill.c
@@ -456,7 +456,7 @@ static switch_status_t do_billing(switch_core_session_t *session)
 	switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "%d seconds passed since last bill time of %s\n",
 					  (int) ((ts - nibble_data->lastts) / 1000000), date);
 
-	if ((ts - nibble_data->lastts) >= 0) {nn
+	if ((ts - nibble_data->lastts) >= 0) {
 		/* Convert billrate into microseconds and multiply by # of microseconds that have passed since last *successful* bill */
 		billamount = ((float) atof(billrate) / 1000000 / 60) * ((ts - nibble_data->lastts)) - nibble_data->bill_adjustments;
 
