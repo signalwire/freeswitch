5ce2759e709cd8861af00ae713249e0f8aa82763
Mathieu Parent <math.parent@gmail.com>
2010-04-02 12:14:41 +0200

    Skinny: Add the missing api files

---
+++ src/mod/endpoints/mod_skinny/skinny_api.h
@@ -0,0 +1,50 @@
+/* 
+ * FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ * Copyright (C) 2010, Mathieu Parent <math.parent@gmail.com>
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ *
+ * The Initial Developer of the Original Code is
+ * Mathieu Parent <math.parent@gmail.com>
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * 
+ * Mathieu Parent <math.parent@gmail.com>
+ *
+ *
+ * skinny_api.h -- Skinny Call Control Protocol (SCCP) Endpoint Module
+ *
+ */
+
+#ifndef _SKINNY_API_H
+#define _SKINNY_API_H
+
+switch_status_t skinny_api_register(switch_loadable_module_interface_t **module_interface);
+
+#endif /* _SKINNY_API_H */
+
+/* For Emacs:
+ * Local Variables:
+ * mode:c
+ * indent-tabs-mode:t
+ * tab-width:4
+ * c-basic-offset:4
+ * End:
+ * For VIM:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4:
+ */
+
