924e960d0a0931e34c372037573df9b0439cb183
Mathieu Parent <math.parent@gmail.com>
2010-04-09 14:40:33 +0200

    perl-skinny: enhance test
    - Use thread for: keepalive, receive and send
    - Run indefinitevly

--- src/mod/endpoints/mod_skinny/Net/Skinny/Message.pm
+++ src/mod/endpoints/mod_skinny/Net/Skinny/Message.pm
@@ -7,54 +7,96 @@ package Net::Skinny::Message;
 use strict;
 use warnings;
 
+use threads;
+use threads::shared;
+
 use Net::Skinny::Protocol qw/:all/;
 
-use Data::Dumper;
-
-require Exporter;
-our @ISA = qw(Exporter);
-our @EXPORT = qw(send);
-
-sub new {
+sub new_empty {
     my $class = shift;
     my $self = {};
     bless $self, $class;
-    $self->{'socket'} = shift;
-    $self->{'type'} = shift;
-    %{$self->{'data'}} = @_;
-    return $ self;
+    $self->{'type'} = undef;
+    $self->{'data'} = undef;
+    $self->{'raw'} = undef;
+    return $self;	
 }
 
-sub send {
+sub new {
+	my $self = shift->new_empty();
+	$self->type(shift);
+	$self->data(@_) if @_;
+    return $self;
+}
+
+sub new_raw {
+	my $self = shift->new_empty();
+	$self->type(shift);
+	$self->raw(shift);
+    return $self;
+}
+
+sub type
+{
     my $self = shift;
-    my $struct = Net::Skinny::Protocol::skinny_message_struct($self->{'type'});
-    my $raw = '';
-    my $parsed_count = 0;
-    for my $info ( @$struct) {
-        last if !defined($self->{'data'}{@$info[1]});
-        if(@$info[0] eq 'char') {
-            $raw .= pack("a".@$info[2], $self->{'data'}{@$info[1]});
-        } elsif(@$info[0] eq 'uint32_t') {
-            $raw .= pack("V".@$info[2], $self->{'data'}{@$info[1]});
-        } elsif(@$info[0] eq 'uint16_t') {
-            $raw .= pack("n".@$info[2], $self->{'data'}{@$info[1]});
-        } elsif(@$info[0] eq 'struct in_addr') {
-            $raw .= pack("V".@$info[2], $self->{'data'}{@$info[1]});
-        } elsif(@$info[0] eq 'struct station_capabilities') {
-            $raw .= $self->{'data'}{@$info[1]};
-        } else {
-            printf "Unknown type: %s\n", @$info[0];
-            return;
-        }
-        $parsed_count++;
+    my $type = @_ ? shift : undef;
+    if(defined($type)) {
+    	$self->{'type'} = $type;
     }
-    if($parsed_count != scalar(keys %{$self->{'data'}})) {
-    	printf "Incomplete message (type=%s (%X)) %d out of %d\n", Net::Skinny::Protocol::skinny_message_type2str($self->{'type'}), $self->{'type'},
-            $parsed_count, scalar(keys %{$self->{'data'}});
-        print Dumper(@$struct);
-        return;
+    return $self->{'type'};
+}
+
+sub data
+{
+    my $self = shift;
+    my @data = @_;
+    if(@data) {
+    	%{$self->{'data'}} = @data;
+    	$self->{'raw'} = undef;
+    } elsif(!defined($self->{'data'})) {
+    	printf "Conversion from raw to data not implemented\n";
     }
-    $self->{'socket'}->send_data($self->{'type'}, $raw);
+    return $self->{'data'};
+}
+
+sub raw
+{
+    my $self = shift;
+    my $raw = shift || undef;
+    if(defined($raw)) {
+    	$self->{'raw'} = $raw;
+    	$self->{'data'} = undef;
+    }
+    if(!defined($self->{'raw'})) {
+		my $struct = Net::Skinny::Protocol::skinny_message_struct($self->{'type'});
+		my $raw = '';
+		my $parsed_count = 0;
+		for my $info ( @$struct) {
+		    last if !defined($self->{'data'}{@$info[1]});
+		    if(@$info[0] eq 'char') {
+		        $raw .= pack("a".@$info[2], $self->{'data'}{@$info[1]});
+		    } elsif(@$info[0] eq 'uint32_t') {
+		        $raw .= pack("V".@$info[2], $self->{'data'}{@$info[1]});
+		    } elsif(@$info[0] eq 'uint16_t') {
+		        $raw .= pack("n".@$info[2], $self->{'data'}{@$info[1]});
+		    } elsif(@$info[0] eq 'struct in_addr') {
+		        $raw .= pack("V".@$info[2], $self->{'data'}{@$info[1]});
+		    } elsif(@$info[0] eq 'struct station_capabilities') {
+		        $raw .= $self->{'data'}{@$info[1]};
+		    } else {
+		        printf "Unknown type: %s\n", @$info[0];
+		        return;
+		    }
+		    $parsed_count++;
+		}
+		if($parsed_count != scalar(keys %{$self->{'data'}})) {
+			printf "Incomplete message (type=%s (%X)) %d out of %d\n", Net::Skinny::Protocol::skinny_message_type2str($self->{'type'}), $self->{'type'},
+		        $parsed_count, scalar(keys %{$self->{'data'}});
+		    return;
+		}
+		$self->{'raw'} = $raw;
+    }
+    return $self->{'raw'};
 }
 
 1;
