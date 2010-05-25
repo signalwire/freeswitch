166add6ed1f287e71d9adc82079f52a9f152aef1
Moises Silva <moy@sangoma.com>
2010-05-25 14:22:51 -0400

    freetdm: remove SIGEVENT_ANSWER, was never used

--- libs/freetdm/src/include/freetdm.h
+++ libs/freetdm/src/include/freetdm.h
@@ -281,7 +281,6 @@ typedef enum {
 typedef enum {
 	FTDM_SIGEVENT_START, /*!< Incoming call (ie: incoming SETUP msg or Ring) */
 	FTDM_SIGEVENT_STOP, /*!< Hangup */
-	FTDM_SIGEVENT_ANSWER, /*!< Outgoing call has been answered */
 	FTDM_SIGEVENT_UP, /*!< Outgoing call has been answered */
 	FTDM_SIGEVENT_FLASH, /*< Flash event  (typically on-hook/off-hook for analog devices) */
 	FTDM_SIGEVENT_PROGRESS, /*!< Outgoing call is making progress */
