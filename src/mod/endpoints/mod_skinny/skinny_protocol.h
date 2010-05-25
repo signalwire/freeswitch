a2ceff147152c1dd0489f17d7601a7c6994b40e3
Mathieu Parent <math.parent@gmail.com>
2010-05-25 09:29:01 +0200

    Skinny: size_t has different sizes on different platforms
    Resolve (shouldn't) MODSKINNY-5: Compiling mod_skinny on Mac OS X 10.6.3 fails
    with error message about format mismatch of 'long unsigned int'

--- src/mod/endpoints/mod_skinny/skinny_protocol.h
+++ src/mod/endpoints/mod_skinny/skinny_protocol.h
@@ -629,7 +629,7 @@ char* skinny_codec2string(enum skinny_codecs skinnycodec);
 #define skinny_check_data_length(message, len) \
     if (message->length < len+4) {\
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR,\
-			"Received Too Short Skinny Message %s (type=%x,length=%d), expected %d.\n",\
+			"Received Too Short Skinny Message %s (type=%x,length=%d), expected %" SWITCH_SIZE_T_FMT ".\n",\
 			skinny_message_type2str(request->type), request->type, request->length,\
 	    	len+4);\
 	    return SWITCH_STATUS_FALSE;\
