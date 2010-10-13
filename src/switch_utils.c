ffd28c193079a1e720a32b85b0a4727bcc869f92
Jeff Lenk <jeff@jefflenk.com>
2010-10-13 10:59:34 -0500

    fix build windows

--- src/switch_utils.c
+++ src/switch_utils.c
@@ -1515,7 +1515,7 @@ SWITCH_DECLARE(char *) get_addr6(char *buf, switch_size_t len, struct sockaddr_i
 
 	if (sa) {
 #if defined(NTDDI_VERSION)
-			switch_inet_ntop6((unsigned char*)sa->sin6_addr, buf, len);
+			switch_inet_ntop6((unsigned char*)&(sa->sin6_addr), buf, len);
 #else
 		inet_ntop(AF_INET6, &(sa->sin6_addr), buf, len);
 #endif
