9a0f638d1914b112b2990407a254afcd86d52bbc
Arnaldo M Pereira <egghunt@gmail.com>
2010-10-25 19:06:59 -0200

    freetdm: removed duplicated DELTA_EPOCH_IN_MICROSECS definition

--- libs/freetdm/src/ftdm_sched.c
+++ libs/freetdm/src/ftdm_sched.c
@@ -35,11 +35,6 @@
 #include "private/ftdm_core.h"
 
 #ifdef __WINDOWS__
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS)
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
-#else /*  */
-#define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
-#endif /*  */
 struct ftdm_timezone {
     int tz_minuteswest;         /* minutes W of Greenwich */
     int tz_dsttime;             /* type of dst correction */
