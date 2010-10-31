3428bd3d43199631f00cea23c1d0c1637b1bae75
Stefan Knoblich <s.knoblich@axsentis.de>
2010-11-01 00:47:43 +0100

    ftmod_isdn: FreeTDM port, first steps - Requires non-public development version of libisdn(!) - Based on older patches from http://oss.axsentis.de/gitweb/?p=ftmod_isdn.git;a=summary - Rework of ftmod_isdn related buildsystem parts (Makefile.am, configure.in and libpcap.m4)

--- libs/freetdm/src/ftmod/ftmod_isdn/ftmod_isdn.h
+++ libs/freetdm/src/ftmod/ftmod_isdn/ftmod_isdn.h
@@ -1,23 +1,23 @@
 /*
  * Copyright (c) 2007, Anthony Minessale II
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
  * are met:
- * 
+ *
  * * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
- * 
+ *
  * * Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * * Neither the name of the original author; nor the names of any contributors
  * may be used to endorse or promote products derived from this software
  * without specific prior written permission.
- * 
- * 
+ *
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -33,7 +33,6 @@
 
 #ifndef FTDM_ISDN_H
 #define FTDM_ISDN_H
-#include "freetdm.h"
 
 #define DEFAULT_DIGIT_TIMEOUT	10000		/* default overlap timeout: 10 seconds */
 
@@ -42,17 +41,23 @@ typedef enum {
 	FTDM_ISDN_OPT_NONE = 0,
 	FTDM_ISDN_OPT_SUGGEST_CHANNEL = (1 << 0),
 	FTDM_ISDN_OPT_OMIT_DISPLAY_IE = (1 << 1),	/*!< Do not send Caller name in outgoing SETUP message (= Display IE) */
-	FTDM_ISDN_OPT_DISABLE_TONES = (1 << 2),		/*!< Disable tone generating thread (NT mode) */
+	FTDM_ISDN_OPT_DISABLE_TONES   = (1 << 2),	/*!< Disable tone generating thread (NT mode) */
 
 	FTDM_ISDN_OPT_MAX = (2 << 0)
 } ftdm_isdn_opts_t;
 
 typedef enum {
-	FTDM_ISDN_RUNNING = (1 << 0),
-	FTDM_ISDN_TONES_RUNNING = (1 << 1),
-	FTDM_ISDN_STOP = (1 << 2)
+	FTDM_ISDN_RUNNING        = (1 << 0),
+	FTDM_ISDN_TONES_RUNNING  = (1 << 1),
+	FTDM_ISDN_STOP           = (1 << 2),
+
+	FTDM_ISDN_CAPTURE        = (1 << 3),
+	FTDM_ISDN_CAPTURE_L3ONLY = (1 << 4)
 } ftdm_isdn_flag_t;
 
+#ifdef HAVE_PCAP
+struct pcap_context;
+#endif
 
 struct ftdm_isdn_data {
 	Q921Data_t q921;
@@ -60,6 +65,7 @@ struct ftdm_isdn_data {
 	ftdm_channel_t *dchan;
 	ftdm_channel_t *dchans[2];
 	struct ftdm_sigmsg sigmsg;
+	fio_signal_cb_t sig_cb;
 	uint32_t flags;
 	int32_t mode;
 	int32_t digit_timeout;
@@ -67,6 +73,9 @@ struct ftdm_isdn_data {
 	ftdm_caller_data_t *outbound_crv[32768];
 	ftdm_channel_t *channels_local_crv[32768];
 	ftdm_channel_t *channels_remote_crv[32768];
+#ifdef HAVE_PCAP
+	struct pcap_context *pcap;
+#endif
 };
 
 typedef struct ftdm_isdn_data ftdm_isdn_data_t;
@@ -75,7 +84,7 @@ typedef struct ftdm_isdn_data ftdm_isdn_data_t;
 /* b-channel private data */
 struct ftdm_isdn_bchan_data
 {
-	L2ULONG digit_timeout;
+	int32_t digit_timeout;
 };
 
 typedef struct ftdm_isdn_bchan_data ftdm_isdn_bchan_data_t;
@@ -91,6 +100,6 @@ typedef struct ftdm_isdn_bchan_data ftdm_isdn_bchan_data_t;
  * c-basic-offset:4
  * End:
  * For VIM:
- * vim:set softtabstop=4 shiftwidth=4 tabstop=4:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4 expandtab:
  */
 
