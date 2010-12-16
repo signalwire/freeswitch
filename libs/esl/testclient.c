871c871b80d314b08bdf82e40e5ea04193c0f5a6
Anthony Minessale <anthm@freeswitch.org>
2010-12-16 12:17:02 -0600

    wrong week to quit sniffing glue

--- libs/esl/testclient.c
+++ libs/esl/testclient.c
@@ -6,47 +6,6 @@
 int main(void)
 {
 	esl_handle_t handle = {{0}};
-	esl_buffer_t *buffer;
-	char doh[65536];
-
-	esl_buffer_create(&buffer, 32 * 1024, 32 * 1024, 0);
-
-	snprintf(doh, sizeof(doh), "TEST 1 FOO BAR 1234\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	snprintf(doh, sizeof(doh), "TEST 1 END\n\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-
-
-
-	snprintf(doh, sizeof(doh), "TEST 2 BAR FOO 4321\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	snprintf(doh, sizeof(doh), "TEST 2 END\n\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-
-	snprintf(doh, sizeof(doh), "TEST 2 BAR FOO 4321\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	esl_buffer_write(buffer, doh, strlen(doh));
-	snprintf(doh, sizeof(doh), "TEST 2 END\n\n");
-	esl_buffer_write(buffer, doh, strlen(doh));
-
-	printf("COUNT %ld\n", esl_buffer_packet_count(buffer));
-
-	memset(doh, 0, sizeof(doh));
-	esl_buffer_read_packet(buffer, doh, sizeof(doh));
-	printf("TEST: [%s]\n", doh);
-
-	memset(doh, 0, sizeof(doh));
-
-
-	esl_buffer_read_packet(buffer, doh, sizeof(doh));
-	printf("TEST2: [%s]\n", doh);
-
-	return 0;
 
 	esl_connect(&handle, "localhost", 8021, NULL, "ClueCon");
 
