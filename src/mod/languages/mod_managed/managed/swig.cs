46853d7f3d08b20ba518d7b596e6082a2d8d9945
Brian West <brian@freeswitch.org>
2011-01-06 11:38:36 -0600

    swigall and fix gitignore for new celt.

--- src/mod/languages/mod_managed/managed/swig.cs
+++ src/mod/languages/mod_managed/managed/swig.cs
@@ -2732,6 +2732,16 @@ public class freeswitch {
     return ret;
   }
 
+  public static char switch_itodtmf(char i) {
+    char ret = freeswitchPINVOKE.switch_itodtmf(i);
+    return ret;
+  }
+
+  public static int switch_dtmftoi(string s) {
+    int ret = freeswitchPINVOKE.switch_dtmftoi(s);
+    return ret;
+  }
+
   public static uint switch_known_bitrate(byte payload) {
     uint ret = freeswitchPINVOKE.switch_known_bitrate(payload);
     return ret;
@@ -3281,6 +3291,16 @@ public class freeswitch {
     return ret;
   }
 
+  public static string switch_channel_get_hold_music(SWIGTYPE_p_switch_channel channel) {
+    string ret = freeswitchPINVOKE.switch_channel_get_hold_music(SWIGTYPE_p_switch_channel.getCPtr(channel));
+    return ret;
+  }
+
+  public static string switch_channel_get_hold_music_partner(SWIGTYPE_p_switch_channel channel) {
+    string ret = freeswitchPINVOKE.switch_channel_get_hold_music_partner(SWIGTYPE_p_switch_channel.getCPtr(channel));
+    return ret;
+  }
+
   public static switch_status_t switch_channel_export_variable_var_check(SWIGTYPE_p_switch_channel channel, string varname, string val, string export_varname, switch_bool_t var_check) {
     switch_status_t ret = (switch_status_t)freeswitchPINVOKE.switch_channel_export_variable_var_check(SWIGTYPE_p_switch_channel.getCPtr(channel), varname, val, export_varname, (int)var_check);
     return ret;
@@ -3366,6 +3386,11 @@ public class freeswitch {
 
   public static uint switch_channel_test_cap(SWIGTYPE_p_switch_channel channel, switch_channel_cap_t cap) {
     uint ret = freeswitchPINVOKE.switch_channel_test_cap(SWIGTYPE_p_switch_channel.getCPtr(channel), (int)cap);
+    return ret;
+  }
+
+  public static uint switch_channel_test_cap_partner(SWIGTYPE_p_switch_channel channel, switch_channel_cap_t cap) {
+    uint ret = freeswitchPINVOKE.switch_channel_test_cap_partner(SWIGTYPE_p_switch_channel.getCPtr(channel), (int)cap);
     return ret;
   }
 
@@ -4664,6 +4689,11 @@ public class freeswitch {
     return ret;
   }
 
+  public static switch_status_t switch_rtp_pause_jitter_buffer(SWIGTYPE_p_switch_rtp rtp_session, switch_bool_t pause) {
+    switch_status_t ret = (switch_status_t)freeswitchPINVOKE.switch_rtp_pause_jitter_buffer(SWIGTYPE_p_switch_rtp.getCPtr(rtp_session), (int)pause);
+    return ret;
+  }
+
   public static void switch_rtp_set_flag(SWIGTYPE_p_switch_rtp rtp_session, uint flags) {
     freeswitchPINVOKE.switch_rtp_set_flag(SWIGTYPE_p_switch_rtp.getCPtr(rtp_session), flags);
   }
@@ -5418,6 +5448,7 @@ public class freeswitch {
   public static readonly string SWITCH_PROXY_MEDIA_VARIABLE = freeswitchPINVOKE.SWITCH_PROXY_MEDIA_VARIABLE_get();
   public static readonly string SWITCH_ENDPOINT_DISPOSITION_VARIABLE = freeswitchPINVOKE.SWITCH_ENDPOINT_DISPOSITION_VARIABLE_get();
   public static readonly string SWITCH_HOLD_MUSIC_VARIABLE = freeswitchPINVOKE.SWITCH_HOLD_MUSIC_VARIABLE_get();
+  public static readonly string SWITCH_TEMP_HOLD_MUSIC_VARIABLE = freeswitchPINVOKE.SWITCH_TEMP_HOLD_MUSIC_VARIABLE_get();
   public static readonly string SWITCH_EXPORT_VARS_VARIABLE = freeswitchPINVOKE.SWITCH_EXPORT_VARS_VARIABLE_get();
   public static readonly string SWITCH_BRIDGE_EXPORT_VARS_VARIABLE = freeswitchPINVOKE.SWITCH_BRIDGE_EXPORT_VARS_VARIABLE_get();
   public static readonly string SWITCH_R_SDP_VARIABLE = freeswitchPINVOKE.SWITCH_R_SDP_VARIABLE_get();
@@ -6005,6 +6036,9 @@ class freeswitchPINVOKE {
 
   [DllImport("mod_managed", EntryPoint="CSharp_SWITCH_HOLD_MUSIC_VARIABLE_get")]
   public static extern string SWITCH_HOLD_MUSIC_VARIABLE_get();
+
+  [DllImport("mod_managed", EntryPoint="CSharp_SWITCH_TEMP_HOLD_MUSIC_VARIABLE_get")]
+  public static extern string SWITCH_TEMP_HOLD_MUSIC_VARIABLE_get();
 
   [DllImport("mod_managed", EntryPoint="CSharp_SWITCH_EXPORT_VARS_VARIABLE_get")]
   public static extern string SWITCH_EXPORT_VARS_VARIABLE_get();
@@ -8588,6 +8622,12 @@ class freeswitchPINVOKE {
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_is_digit_string")]
   public static extern int switch_is_digit_string(string jarg1);
+
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_itodtmf")]
+  public static extern char switch_itodtmf(char jarg1);
+
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_dtmftoi")]
+  public static extern int switch_dtmftoi(string jarg1);
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_known_bitrate")]
   public static extern uint switch_known_bitrate(byte jarg1);
@@ -11310,6 +11350,12 @@ class freeswitchPINVOKE {
   [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_get_variable_partner")]
   public static extern string switch_channel_get_variable_partner(HandleRef jarg1, string jarg2);
 
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_get_hold_music")]
+  public static extern string switch_channel_get_hold_music(HandleRef jarg1);
+
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_get_hold_music_partner")]
+  public static extern string switch_channel_get_hold_music_partner(HandleRef jarg1);
+
   [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_export_variable_var_check")]
   public static extern int switch_channel_export_variable_var_check(HandleRef jarg1, string jarg2, string jarg3, string jarg4, int jarg5);
 
@@ -11366,6 +11412,9 @@ class freeswitchPINVOKE {
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_test_cap")]
   public static extern uint switch_channel_test_cap(HandleRef jarg1, int jarg2);
+
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_test_cap_partner")]
+  public static extern uint switch_channel_test_cap_partner(HandleRef jarg1, int jarg2);
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_channel_set_flag_partner")]
   public static extern int switch_channel_set_flag_partner(HandleRef jarg1, int jarg2);
@@ -12479,6 +12528,9 @@ class freeswitchPINVOKE {
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_rtp_deactivate_jitter_buffer")]
   public static extern int switch_rtp_deactivate_jitter_buffer(HandleRef jarg1);
+
+  [DllImport("mod_managed", EntryPoint="CSharp_switch_rtp_pause_jitter_buffer")]
+  public static extern int switch_rtp_pause_jitter_buffer(HandleRef jarg1, int jarg2);
 
   [DllImport("mod_managed", EntryPoint="CSharp_switch_rtp_set_flag")]
   public static extern void switch_rtp_set_flag(HandleRef jarg1, uint jarg2);
@@ -21477,6 +21529,8 @@ public enum switch_channel_cap_t {
   CC_MEDIA_ACK = 1,
   CC_BYPASS_MEDIA,
   CC_PROXY_MEDIA,
+  CC_JITTERBUFFER,
+  CC_FS_RTP,
   CC_FLAG_MAX
 }
 
