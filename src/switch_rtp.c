9a3cf6015b5a9fc292d14c26033c2328b790490f
Jeff Lenk <jeff@jefflenk.com>
2011-02-25 15:39:42 -0600

    trivial compiler warning fix

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -2856,6 +2856,7 @@ static int rtp_common_read(switch_rtp_t *rtp_session, switch_payload_t *payload_
 			goto end;
 		}
 
+#ifdef 0
 		if (bytes && rtp_session->recv_msg.header.version == 2 && 
 			!switch_test_flag(rtp_session, SWITCH_RTP_FLAG_PROXY_MEDIA) && !switch_test_flag(rtp_session, SWITCH_RTP_FLAG_UDPTL) &&
 			rtp_session->recv_msg.header.pt != 13 && 
@@ -2865,6 +2866,7 @@ static int rtp_common_read(switch_rtp_t *rtp_session, switch_payload_t *payload_
 			/* drop frames of incorrect payload number and return CNG frame instead */
 			return_cng_frame();
 		}
+#endif
 
 		if (!bytes && (io_flags & SWITCH_IO_FLAG_NOBLOCK)) {
 			rtp_session->missed_count = 0;
