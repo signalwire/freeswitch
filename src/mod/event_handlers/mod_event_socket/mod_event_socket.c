64bc19387c47f614ea883fde3e0b912f0cbc5649
Anthony Minessale <anthm@freeswitch.org>
2011-03-17 12:36:27 -0500

    FS-3156 FS-3157 declinatio mortuus obfirmo!

--- src/mod/event_handlers/mod_event_socket/mod_event_socket.c
+++ src/mod/event_handlers/mod_event_socket/mod_event_socket.c
@@ -313,7 +313,7 @@ static void event_handler(switch_event_t *event)
 				const char *hval;
 
 				send = 0;
-
+				
 				for (hp = l->filters->headers; hp; hp = hp->next) {
 					if ((hval = switch_event_get_header(event, hp->name))) {
 						const char *comp_to = hp->value;
@@ -357,8 +357,9 @@ static void event_handler(switch_event_t *event)
 						}
 					}
 				}
-				switch_mutex_unlock(l->filter_mutex);
 			}
+
+			switch_mutex_unlock(l->filter_mutex);
 		}
 
 		if (send && switch_test_flag(l, LFLAG_MYEVENTS)) {
