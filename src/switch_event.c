f4824b077385f9a34996346f2658a616ec0fc79d
Jeff Lenk <jeff@jefflenk.com>
2011-05-25 17:16:48 -0500

    fix windows trivial compiler warnings

--- src/switch_event.c
+++ src/switch_event.c
@@ -963,7 +963,7 @@ static switch_status_t switch_event_base_add_header(switch_event_t *event, switc
 		header->array = m;
 
 	redraw:
-
+		len = 0;
 		for(j = 0; j < header->idx; j++) {
 			len += strlen(header->array[j]) + 2;
 		}
