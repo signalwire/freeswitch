1a39d7fb345da414fd6da4f3af94d2d917600000
Brian West <brian@freeswitch.org>
2011-06-15 13:08:54 -0500

    Update PocketSphinx to the latest builds... only had to make two changes

--- src/mod/asr_tts/mod_pocketsphinx/mod_pocketsphinx.c
+++ src/mod/asr_tts/mod_pocketsphinx/mod_pocketsphinx.c
@@ -32,8 +32,8 @@
 
 #include <switch.h>
 #include <pocketsphinx.h>
-#include <err.h>
-#include <logmath.h>
+#include <sphinxbase/err.h>
+#include <sphinxbase/logmath.h>
 
 SWITCH_MODULE_LOAD_FUNCTION(mod_pocketsphinx_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_pocketsphinx_shutdown);
