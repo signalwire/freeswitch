e47e52c9d4d560c6c31b80f709d38cc6e550223b
Anthony Minessale <anthm@freeswitch.org>
2011-06-22 18:58:56 -0500

    revert

--- src/switch_ivr_async.c
+++ src/switch_ivr_async.c
@@ -1287,7 +1287,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_eavesdrop_session(switch_core_session
 		msg.message_id = SWITCH_MESSAGE_INDICATE_DISPLAY;
 		switch_core_session_receive_message(tsession, &msg);
 
-		while (switch_channel_up(tchannel) && switch_channel_ready(channel)) {
+		while (switch_channel_ready(tchannel) && switch_channel_ready(channel)) {
 			uint32_t len = sizeof(buf);
 			switch_event_t *event = NULL;
 			char *fcommand = NULL;
