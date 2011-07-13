9f1b731164a2dd7c4aa20920184751066a560afc
Anthony Minessale <anthm@freeswitch.org>
2011-07-12 20:33:58 -0500

    up vals

--- src/mod/endpoints/mod_sofia/mod_sofia.h
+++ src/mod/endpoints/mod_sofia/mod_sofia.h
@@ -317,8 +317,8 @@ typedef enum {
 	TFLAG_MAX
 } TFLAGS;
 
-#define SOFIA_MAX_MSG_QUEUE 26
-#define SOFIA_MSG_QUEUE_SIZE 20
+#define SOFIA_MAX_MSG_QUEUE 51
+#define SOFIA_MSG_QUEUE_SIZE 2000
 
 struct mod_sofia_globals {
 	switch_memory_pool_t *pool;
