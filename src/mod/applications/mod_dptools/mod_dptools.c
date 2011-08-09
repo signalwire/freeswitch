01b2bd0494ffd3cf9d92463d54ec41028832add5
Anthony Minessale <anthm@freeswitch.org>
2011-08-09 08:33:03 -0500

    fix seg on user_recurse_variables reported on the mailing list

--- src/mod/applications/mod_dptools/mod_dptools.c
+++ src/mod/applications/mod_dptools/mod_dptools.c
@@ -3084,8 +3084,10 @@ static switch_call_cause_t user_outgoing_channel(switch_core_session_t *session,
 		}
 	}
 
-	switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "dialed_user", user);
-	switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "dialed_domain", domain);
+	if (var_event) {
+		switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "dialed_user", user);
+		switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "dialed_domain", domain);
+	}
 
 	if (!dest) {
 		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "No dial-string available, please check your user directory.\n");
