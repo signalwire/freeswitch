c3ceebf0bf2c54025c4e53201191f60d2219191b
Anthony Minessale <anthm@freeswitch.org>
2011-08-29 10:31:41 -0500

    FS-3528 --resolve

--- src/switch_ivr_originate.c
+++ src/switch_ivr_originate.c
@@ -2354,11 +2354,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_originate(switch_core_session_t *sess
 				}
 
 				if (zstr(new_profile->destination_number)) {
-					if (caller_channel) {
-						switch_channel_hangup(caller_channel, SWITCH_CAUSE_DESTINATION_OUT_OF_ORDER);
-					}
-					status = SWITCH_STATUS_FALSE;
-					goto done;
+					new_profile->destination_number = switch_core_strdup(new_profile->pool, "service");
 				}
 
 				new_profile->callee_id_name = switch_core_strdup(new_profile->pool, "Outbound Call");
