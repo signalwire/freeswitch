680d92967f41de660a64a5981e88b1a12e1a1089
Moises Silva <moy@sangoma.com>
2011-10-24 13:21:15 -0400

    freetdm: mod_freetdm - add SIP x header for FreeTDM-NADI value

--- libs/freetdm/mod_freetdm/mod_freetdm.c
+++ libs/freetdm/mod_freetdm/mod_freetdm.c
@@ -1773,6 +1773,11 @@ ftdm_status_t ftdm_channel_from_event(ftdm_sigmsg_t *sigmsg, switch_core_session
 		switch_channel_set_variable_printf(channel, "sip_h_X-FreeTDM-RDNIS-Plan", "%d", channel_caller_data->rdnis.plan);
 		switch_channel_set_variable_printf(channel, "sip_h_X-FreeTDM-CPC", "%s", ftdm_calling_party_category2str(channel_caller_data->cpc));
 		
+		var_value = ftdm_sigmsg_get_var(sigmsg, "ss7_clg_nadi");
+		if (!ftdm_strlen_zero(var_value)) {
+			switch_channel_set_variable_printf(channel, "sip_h_X-FreeTDM-NADI", "%d", var_value);
+		}
+
 		var_value = ftdm_sigmsg_get_var(sigmsg, "ss7_rdnis_screen_ind");
 		if (!ftdm_strlen_zero(var_value)) {
 			switch_channel_set_variable_printf(channel, "sip_h_X-FreeTDM-RDNIS-Screen", "%d", var_value);
