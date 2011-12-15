acdacfc6c0d4f2b6f87cffd56cd7bd4a6bd36d88
Marc Olivier Chouinard <mochouinard@moctel.com>
2011-12-15 13:14:29 -0500

    FS-3747 --resolve

--- src/mod/applications/mod_spandsp/mod_spandsp_modem.h
+++ src/mod/applications/mod_spandsp/mod_spandsp_modem.h
@@ -44,14 +44,41 @@
 
 #include <stdio.h>
 #include <string.h>
+
 #ifndef WIN32
+#ifdef __APPLE__
+#include <util.h>
+#include <sys/ioctl.h>
+#else
 #include <pty.h>
+#endif
 #include <unistd.h>
 #endif
 #include <fcntl.h>
 #include <errno.h>
 #ifndef WIN32
+#if defined(HAVE_BYTESWAP_H)
 #include <byteswap.h>
+#elif defined(USE_SYS_ENDIAN_H)
+#include <sys/endian.h>
+#elif defined (__APPLE__)
+#include <libkern/OSByteOrder.h>
+#define bswap_16 OSSwapInt16
+#define bswap_32 OSSwapInt32
+#define bswap_64 OSSwapInt64
+#else
+#define bswap_16(value)  \
+	((((value) & 0xff) << 8) | ((value) >> 8))
+
+#define bswap_32(value)	\
+	(((uint32_t)bswap_16((uint16_t)((value) & 0xffff)) << 16) | \
+	(uint32_t)bswap_16((uint16_t)((value) >> 16)))
+
+#define bswap_64(value)	\
+	(((uint64_t)bswap_32((uint32_t)((value) & 0xffffffff)) \
+	    << 32) | \
+	(uint64_t)bswap_32((uint32_t)((value) >> 32)))
+#endif
 #include <sys/time.h>
 #include <sys/signal.h>
 #endif
