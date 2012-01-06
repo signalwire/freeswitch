0b639ae0e3600e001144fb882031abd3be0b79ee
Michael S Collins <msc@freeswitch.org>
2012-01-05 17:38:58 -0800

    D'oh

--- src/mod/applications/mod_httapi/mod_httapi.c
+++ src/mod/applications/mod_httapi/mod_httapi.c
@@ -2215,7 +2215,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_httapi_load)
 	}
 	
 	SWITCH_ADD_API(httapi_api_interface, "httapi",
-				   "HT-TAPI Hypertext Telephony API", "HT-TAPI Hypertext Telephony API", httapi_api_function, HTTAPI_SYNTAX);
+				   "HT-TAPI Hypertext Telephony API", httapi_api_function, HTTAPI_SYNTAX);
 
 	SWITCH_ADD_APP(app_interface, "httapi", 
 				   "HT-TAPI Hypertext Telephony API", 
