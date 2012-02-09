d5f47a87cecc3f8f7e9bb748497121390755df81
Anthony Minessale <anthm@freeswitch.org>
2012-02-09 12:03:11 -0600

    FS-3794

--- src/mod/endpoints/mod_sofia/sofia_presence.c
+++ src/mod/endpoints/mod_sofia/sofia_presence.c
@@ -2503,8 +2503,8 @@ static int sofia_presence_sub_callback(void *pArg, int argc, char **argv, char *
 			if (!strcasecmp(astate, "hangup")) {
 				astate = "terminated";
 			}
-
-			if (!sofia_test_pflag(profile, PFLAG_PRESENCE_DISABLE_EARLY)) {
+			
+			if (sofia_test_pflag(profile, PFLAG_PRESENCE_DISABLE_EARLY)) {
 				if (!strcasecmp(astate, "ringing") || !strcasecmp(astate, "early")) {
 					astate = "confirmed";
 				}
