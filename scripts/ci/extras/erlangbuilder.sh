3b4023c6d3cbba7c7a3dbf623c384ae91b2b917d
Ken Rice <krice@tollfreegateway.com>
2012-03-24 03:44:21 +0000

    ci: add continuous integration helper scripts

---
+++ scripts/ci/extras/erlangbuilder.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+##### -*- mode:shell-script; indent-tabs-mode:nil; sh-basic-offset:2 -*-
+
+src_repo="$(pwd)"
+
+if [ ! -d .git ]; then
+  echo "error: must be run from within the top level of a FreeSWITCH git tree." 1>&2
+  exit 1;
+fi
+
+if [ -z "$1" ]; then
+  echo "usage: ./scripts/ci/extras/erlangbuilder.sh" 1>&2
+  exit 1;
+fi
+
+mkdir -p $src_repo/rpmbuild/{SOURCES,BUILD,BUILDROOT,i386,x86_64,SOURCES,SPECS}
+cd $src_repo
+
+rpmbuild --define "_topdir %(pwd)/rpmbuild" \
+  --define "_rpmdir %{_topdir}" \
+  --define "_srcrpmdir %{_topdir}" \
+  -ba erlang.spec
+
+mkdir $src_repo/RPMS
+mv $src_repo/rpmbuild/*/*.rpm $src_repo/RPMS/.
+
