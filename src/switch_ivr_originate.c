0b992a2bd20feac8619ecaff8e7de3d4e143f8bf
Anthony Minessale <anthm@freeswitch.org>
2012-04-09 12:35:26 -0500

    FS-4089 --resolve

--- src/switch_ivr_originate.c
+++ src/switch_ivr_originate.c
@@ -1931,6 +1931,8 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_originate(switch_core_session_t *sess
 					ok = 1;
 				} else if (!strcasecmp((char *) hi->name, "group_confirm_file")) {
 					ok = 1;
+				} else if (!strcasecmp((char *) hi->name, "group_confirm_read_timeout")) {
+					ok = 1;
 				} else if (!strcasecmp((char *) hi->name, "group_confirm_cancel_timeout")) {
 					ok = 1;
 				} else if (!strcasecmp((char *) hi->name, "forked_dial")) {
