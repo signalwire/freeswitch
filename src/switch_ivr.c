c0b69c415ccd5c0d5c1b0088976311e998933ff2
Anthony Minessale <anthm@freeswitch.org>
2012-04-10 09:59:56 -0500

    FS-4096 --resolve

--- src/switch_ivr.c
+++ src/switch_ivr.c
@@ -264,7 +264,7 @@ SWITCH_DECLARE(switch_status_t) switch_ivr_sleep(switch_core_session_t *session,
 				if (args->input_callback) {
 					status = args->input_callback(session, (void *) &dtmf, SWITCH_INPUT_TYPE_DTMF, args->buf, args->buflen);
 				} else if (args->buf) {
-					switch_copy_string((char *) args->buf, (void *) &dtmf, args->buflen);
+					*((char *) args->buf) = dtmf.digit;
 					status = SWITCH_STATUS_BREAK;
 				}
 			}
