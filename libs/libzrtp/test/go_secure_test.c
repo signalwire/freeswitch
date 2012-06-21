02b3b8060a456cdedf81cc068b4b0ece37149d88
Viktor Krykun <viktor@krykun.com>
2012-06-21 11:36:33 +0300

    Refactor test cases
    Modified-by: Travis Cross <tc@traviscross.com>
    Signed-off-by: Travis Cross <tc@traviscross.com>

--- libs/libzrtp/test/go_secure_test.c
+++ libs/libzrtp/test/go_secure_test.c
@@ -7,16 +7,7 @@
  * Viktor Krykun <v.krikun at zfoneproject.com>
  */
 
-#include <setjmp.h>		/*chmockery dependency*/
-#include <stdio.h>		/*chmockery dependency*/
-#include <unistd.h> 	/*for usleep*/
-
-#include "cmockery/cmockery.h"
-#include "test_engine.h"
-
-static zrtp_test_id_t g_alice, g_bob;
-static zrtp_test_id_t g_alice_sid, g_bob_sid;
-static zrtp_test_id_t g_secure_audio_channel;
+#include "engine_helpers.c"
 
 static void setup() {
 	zrtp_status_t s;
@@ -36,56 +27,6 @@ static void setup() {
 static void teardown() {
 	zrtp_test_endpoint_destroy(g_alice);
 	zrtp_test_endpoint_destroy(g_bob);
-}
-
-static void prepare_alice_bob() {
-	zrtp_status_t s;
-
-	//zrtp_test_channel_info_t channel_info;
-	zrtp_test_session_cfg_t session_config;
-	zrtp_test_session_config_defaults(&session_config);
-
-	/*
-	 * Create two test sessions, one for Alice and one for Bob and link them
-	 * into test secure channel
-	 */
-	s = zrtp_test_session_create(g_alice, &session_config, &g_alice_sid);
-	assert_int_equal(zrtp_status_ok, s);
-	assert_int_not_equal(ZRTP_TEST_UNKNOWN_ID, g_alice_sid);
-
-	s = zrtp_test_session_create(g_bob, &session_config, &g_bob_sid);
-	assert_int_equal(zrtp_status_ok, s);
-	assert_int_not_equal(ZRTP_TEST_UNKNOWN_ID, g_bob_sid);
-
-	s = zrtp_test_channel_create2(g_alice_sid, g_bob_sid, 0, &g_secure_audio_channel);
-	assert_int_equal(zrtp_status_ok, s);
-	assert_int_not_equal(ZRTP_TEST_UNKNOWN_ID, g_secure_audio_channel);
-}
-
-static void release_alice_bob() {
-	zrtp_test_session_destroy(g_alice_sid);
-	zrtp_test_session_destroy(g_bob_sid);
-
-	zrtp_test_channel_destroy(g_secure_audio_channel);
-}
-
-static void start_alice_bob_and_wait4secure() {
-	zrtp_status_t s;
-	zrtp_test_channel_info_t channel_info;
-
-	/* Everything is ready. Let's start the stream and give it few seconds to switch secure. */
-	s = zrtp_test_channel_start(g_secure_audio_channel);
-	assert_int_equal(zrtp_status_ok, s);
-
-	unsigned i = 30;
-	for (; i>0; i--) {
-		usleep(100*1000);
-	}
-
-	s = zrtp_test_channel_get(g_secure_audio_channel, &channel_info);
-	assert_int_equal(zrtp_status_ok, s);
-
-	assert_true(channel_info.is_secure);
 }
 
 
