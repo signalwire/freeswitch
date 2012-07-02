158f96651fa019bdba1a8eaa68c554b2b967525a
Anthony Minessale <anthm@freeswitch.org>
2012-07-02 11:04:10 -0500

    add trailing newlines for lazy parsers

--- src/mod/applications/mod_spandsp/mod_spandsp_dsp.c
+++ src/mod/applications/mod_spandsp/mod_spandsp_dsp.c
@@ -121,7 +121,7 @@ static void put_text_msg(void *user_data, const uint8_t *msg, int len)
         switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "proto", "tdd");
         switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "subject", "TDD MESSAGE");
         switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "Unique-ID", switch_core_session_get_uuid(pvt->session));
-        switch_event_add_body(event, "%s", (char *)msg);
+        switch_event_add_body(event, "%s\n\n", (char *)msg);
 
         if (switch_core_session_get_partner(pvt->session, &other_session) == SWITCH_STATUS_SUCCESS) {
         
