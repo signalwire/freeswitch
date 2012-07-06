4ead147e100d0bc38f17922dc49bdce3ae0615db
David Yat Sin <dyatsin@sangoma.com>
2012-07-06 12:04:52 -0400

    chlog:Support for send-connect-ack parameter

--- libs/freetdm/src/ftmod/ftmod_sangoma_isdn/ftmod_sangoma_isdn_cfg.c
+++ libs/freetdm/src/ftmod/ftmod_sangoma_isdn/ftmod_sangoma_isdn_cfg.c
@@ -302,6 +302,7 @@ ftdm_status_t ftmod_isdn_parse_cfg(ftdm_conf_parameter_t *ftdm_parameters, ftdm_
 
 	signal_data->cid_name_method = SNGISDN_CID_NAME_AUTO;
 	signal_data->send_cid_name = SNGISDN_OPT_DEFAULT;
+	signal_data->send_connect_ack = SNGISDN_OPT_DEFAULT;
 	
 	span->default_caller_data.dnis.plan = FTDM_NPI_INVALID;
 	span->default_caller_data.dnis.type = FTDM_TON_INVALID;
@@ -465,6 +466,8 @@ ftdm_status_t ftmod_isdn_parse_cfg(ftdm_conf_parameter_t *ftdm_parameters, ftdm_
 			parse_timer(val, &signal_data->timer_t319);
 		} else if (!strcasecmp(var, "timer-t322")) {
 			parse_timer(val, &signal_data->timer_t322);
+		} else if (!strcasecmp(var, "send-connect-ack")) {
+			parse_yesno(var, val, &signal_data->send_connect_ack);
 		} else {
 			ftdm_log(FTDM_LOG_WARNING, "Ignoring unknown parameter %s\n", ftdm_parameters[paramindex].var);
 		}
