a8efae998ffb90a8ae3321ec762b9adbc8fd3ab6
Daniel Swarbrick <daniel.swarbrick@gmail.com>
2012-07-12 01:34:36 +0200

    Add FreeSWITCH-Version string and Uptime-msec (uptime in *milliseconds*) to heartbeat events

--- src/switch_core.c
+++ src/switch_core.c
@@ -82,6 +82,8 @@ static void send_heartbeat(void)
 								duration.sec, duration.sec == 1 ? "" : "s",
 								duration.ms, duration.ms == 1 ? "" : "s", duration.mms, duration.mms == 1 ? "" : "s");
 
+		switch_event_add_header(event, SWITCH_STACK_BOTTOM, "FreeSWITCH-Version", SWITCH_VERSION_FULL);
+		switch_event_add_header(event, SWITCH_STACK_BOTTOM, "Uptime-msec", "%lu", switch_core_uptime() / 1000);
 		switch_event_add_header(event, SWITCH_STACK_BOTTOM, "Session-Count", "%u", switch_core_session_count());
 		switch_event_add_header(event, SWITCH_STACK_BOTTOM, "Max-Sessions", "%u", switch_core_session_limit(0));
 		switch_event_add_header(event, SWITCH_STACK_BOTTOM, "Session-Per-Sec", "%u", runtime.sps);
