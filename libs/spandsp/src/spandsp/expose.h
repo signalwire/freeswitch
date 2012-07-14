5d7e26141f9e01f31032808248d9279058202f9b
Steve Underwood <steveu@coppice.org>
2012-07-14 23:59:01 +0800

    Introducing T.85 encode and decodei to spandsp. This is not connected into the FAX engine in this update.

--- libs/spandsp/src/spandsp/expose.h
+++ libs/spandsp/src/spandsp/expose.h
@@ -78,8 +78,8 @@
 #include <spandsp/private/image_translate.h>
 #include <spandsp/private/t4_t6_decode.h>
 #include <spandsp/private/t4_t6_encode.h>
-/*#include <spandsp/private/t81_t82_arith_coding.h>*/
-/*#include <spandsp/private/t85.h>*/
+#include <spandsp/private/t81_t82_arith_coding.h>
+#include <spandsp/private/t85.h>
 /*#include <spandsp/private/t42.h>*/
 /*#include <spandsp/private/t43.h>*/
 #include <spandsp/private/t4_rx.h>
