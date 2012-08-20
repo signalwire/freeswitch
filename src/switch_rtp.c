679921d914db67d938b541b21df6c7833c3025d6
Anthony Minessale <anthm@freeswitch.org>
2012-08-20 12:11:17 -0500

    FS-4548 --resolve such a little patch for a big problem, good find

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -3545,6 +3545,8 @@ static int rtp_common_read(switch_rtp_t *rtp_session, switch_payload_t *payload_
 
 		if (((rtp_session->cng_pt && rtp_session->recv_msg.header.pt == rtp_session->cng_pt) || rtp_session->recv_msg.header.pt == 13)) {
 			*flags |= SFF_NOT_AUDIO;
+		} else {
+			*flags &= ~SFF_NOT_AUDIO; /* If this flag was already set, make sure to remove it when we get real audio */
 		}
 
 
