0343b3ae8f56ee8825039ce582709e490c027017
Anthony Minessale <anthm@freeswitch.org>
2012-10-31 11:45:09 -0400

    FS-4777 --resolve

--- libs/libdingaling/src/libdingaling.c
+++ libs/libdingaling/src/libdingaling.c
@@ -820,6 +820,9 @@ static ldl_status parse_jingle_code(ldl_handle_t *handle, iks *xml, char *to, ch
 										break;
 									}
 								}
+							} else {
+								globals.logger(DL_LOG_WARNING, "No preference specified");
+								continue; 
 							}
 						
 							if (index < 0) {
