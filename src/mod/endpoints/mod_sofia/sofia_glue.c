52c1d3c76baa6f6deccb4980c7463ad4a40a5a8a
Anthony Minessale <anthm@freeswitch.org>
2013-04-04 12:35:18 -0500

    FS-5265 --resolve

--- src/mod/endpoints/mod_sofia/sofia_glue.c
+++ src/mod/endpoints/mod_sofia/sofia_glue.c
@@ -1467,6 +1467,37 @@ void sofia_glue_pass_sdp(private_object_t *tech_pvt, char *sdp)
 	}
 }
 
+char *sofia_glue_get_path_from_contact(char *buf)
+{
+	char *p, *e, *path = NULL, *contact = NULL;
+
+	if (!buf) return NULL;
+
+	contact = sofia_glue_get_url_from_contact(buf, SWITCH_TRUE);
+
+	if (!contact) return NULL;
+	
+	if ((p = strstr(contact, "fs_path="))) {
+		p += 8;
+
+		if (!zstr(p)) {
+			path = strdup(p);
+		}
+	}
+
+	if (!path) return NULL;
+
+	if ((e = strrchr(path, ';'))) {
+		*e = '\0';
+	}
+
+	switch_url_decode(path);
+
+	free(contact);
+
+	return path;
+}
+
 char *sofia_glue_get_url_from_contact(char *buf, uint8_t to_dup)
 {
 	char *url = NULL, *e;
