8eef28ab24abb66b0435b26ce77beebde84b00c2
Anthony Minessale <anthm@freeswitch.org>
2013-04-17 17:33:42 -0500

    FS-5313 --resolve

--- src/switch_core_sqldb.c
+++ src/switch_core_sqldb.c
@@ -3238,8 +3238,12 @@ switch_status_t switch_core_sqldb_start(switch_memory_pool_t *pool, switch_bool_
 				//runtime.odbc_dsn = NULL;
 				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Database Error [%s]\n", err);
 				//switch_cache_db_release_db_handle(&sql_manager.dbh);
-				free(err);
-				goto top;
+                                if (switch_stristr("read-only", err)) { 
+                                        free(err);
+                                } else {
+                                        free(err);
+                                        goto top;
+                                }
 			}
 		}
 		break;
