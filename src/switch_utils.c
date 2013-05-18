3f06bbd04a469d9709ed9bfed4959dc43751a55e
William King <william.king@quentustech.com>
2013-05-18 13:38:24 -0700

    This was writing a null one space to the right, outside of the malloc'd buffer. On further review this uses apr_vsnprintf which always returns null terminated.

--- src/switch_utils.c
+++ src/switch_utils.c
@@ -185,8 +185,6 @@ SWITCH_DECLARE(char *) switch_find_parameter(const char *str, const char *param,
 				r = malloc(mlen);
 			}
 
-			*(r + mlen) = '\0';
-
 			switch_snprintf(r, mlen, "%s", ptr);
 
 			break;
