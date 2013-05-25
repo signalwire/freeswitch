58e48416bee678023d9c29994f4fe52392a9cb11
Travis Cross <tc@traviscross.com>
2013-05-25 09:47:12 +0000

    Add missing text editor local variable blocks

--- libs/esl/src/esl_oop.cpp
+++ libs/esl/src/esl_oop.cpp
@@ -505,3 +505,14 @@ const char *ESLevent::getType(void)
 	
 	return (char *) "invalid";
 }
+
+/* For Emacs:
+ * Local Variables:
+ * mode:c++
+ * indent-tabs-mode:t
+ * tab-width:4
+ * c-basic-offset:4
+ * End:
+ * For VIM:
+ * vim:set softtabstop=4 shiftwidth=4 tabstop=4:
+ */
