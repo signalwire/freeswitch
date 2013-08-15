signalwire/freeswitch

commit a4408e62a6ca1bc36e26109396af332d05fae812

--resolve FS-3918 using patch that adds conference_moderator_pin channel variable

src/mod/applications/mod_conference/mod_conference.c
@@ -7355,7 +7355,7 @@ SWITCH_STANDARD_APP(conference_function)
 	switch_core_session_message_t msg = { 0 };
 	uint8_t rl = 0, isbr = 0;
 	char *dpin = "";
-	char *mdpin = "";
+	const char *mdpin = "";
 	conf_xml_cfg_t xml_cfg = { 0 };
 	switch_event_t *params = NULL;
 	int locked = 0;
@@ -7596,6 +7596,9 @@ SWITCH_STANDARD_APP(conference_function)
 			switch_channel_set_variable(channel, "conference_name", conference->name);
 			rl++;
 		}
+
+		/* Moderator PIN as a channel variable */
+		mdpin = switch_channel_get_variable(channel, "conference_moderator_pin");
 
 		if (zstr(dpin) && conference->pin) {
 			dpin = conference->pin;

commit 43a11b00aadc22293730aab903990e1f2abd7d2b

Fix segfault in mod_conference
This was introduced by commit a4408e62a6ca1bc36e26109396af332d05fae812.

FS-5612 --resolve

src/mod/applications/mod_conference/mod_conference.c
@@ -7670,7 +7670,9 @@ SWITCH_STANDARD_APP(conference_function)
 			}
 
 			while (!pin_valid && pin_retries && status == SWITCH_STATUS_SUCCESS) {
-				int maxpin = strlen(dpin) > strlen(mdpin) ? strlen(dpin) : strlen(mdpin);
+				size_t dpin_length = dpin ? strlen(dpin) : 0;
+				size_t mdpin_length = mdpin ? strlen(mdpin) : 0;
+				int maxpin = dpin_length > mdpin_length ? dpin_length : mdpin_length;
 				switch_status_t pstatus = SWITCH_STATUS_FALSE;
 
 				/* be friendly */