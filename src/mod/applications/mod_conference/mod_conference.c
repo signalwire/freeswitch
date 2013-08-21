b4792e7a4eb8cfd950fadccd221cbea702b4d21d
Anthony Minessale <anthm@freeswitch.org>
2013-08-21 22:26:04 +0500

    recommit

--- src/mod/applications/mod_conference/mod_conference.c
+++ src/mod/applications/mod_conference/mod_conference.c
@@ -8911,7 +8911,8 @@ static void call_setup_event_handler(switch_event_t *event)
 
 					switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "conference_track_status", "true");
 					switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "conference_track_call_id", call_id);
-                                        switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "sip_invite_domain", domain);
+					switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "sip_invite_domain", domain);
+					switch_event_add_header_string(var_event, SWITCH_STACK_BOTTOM, "sip_invite_contact_params", "~isfocus");
 
 					if (!strncasecmp(ostr, "url+", 4)) {
 						ostr += 4;
