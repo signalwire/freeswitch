7c5b5ce8a46769ed16c05907db98509219480f1a
Anthony Minessale <anthm@freeswitch.org>
2013-08-27 13:59:24 -0400

    fix some close on -1 issues

--- libs/apr/network_io/unix/sockets.c
+++ libs/apr/network_io/unix/sockets.c
@@ -32,6 +32,10 @@ static apr_status_t socket_cleanup(void *sock)
 {
     apr_socket_t *thesocket = sock;
 
+	if (thesocket && thesocket->socketdes == -1) {
+		return APR_SUCCESS;
+	}
+
     if (close(thesocket->socketdes) == 0) {
         thesocket->socketdes = -1;
         return APR_SUCCESS;
