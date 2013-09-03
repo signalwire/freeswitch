01ad34b58a0c386543e450e266cee81c5fa14827
Chris Rienzo <chris.rienzo@grasshopper.com>
2013-09-03 10:05:13 -0400

    mod_http_cache: use case-insensitive domain to profile hash

--- src/mod/applications/mod_http_cache/mod_http_cache.c
+++ src/mod/applications/mod_http_cache/mod_http_cache.c
@@ -1633,7 +1633,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_http_cache_load)
 	gcache.pool = pool;
 	switch_core_hash_init(&gcache.map, gcache.pool);
 	switch_core_hash_init(&gcache.profiles, gcache.pool);
-	switch_core_hash_init(&gcache.fqdn_profiles, gcache.pool);
+	switch_core_hash_init_nocase(&gcache.fqdn_profiles, gcache.pool);
 	switch_mutex_init(&gcache.mutex, SWITCH_MUTEX_UNNESTED, gcache.pool);
 	switch_thread_rwlock_create(&gcache.shutdown_lock, gcache.pool);
 
