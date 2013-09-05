ac875b781074d478ff5ec00e67976b96bb033251
Anthony Minessale <anthm@freeswitch.org>
2013-09-05 20:36:03 +0500

    FS-5757 --resolve

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -8707,6 +8707,8 @@ void sofia_handle_sip_i_invite(switch_core_session_t *session, nua_t *nua, sofia
 						tech_pvt->caller_profile->callee_id_name = switch_core_strdup(tech_pvt->caller_profile->pool, name);
 						tech_pvt->caller_profile->callee_id_number = switch_core_strdup(tech_pvt->caller_profile->pool, num);
 
+						tech_pvt->caller_profile->originatee_caller_profile = switch_caller_profile_dup(tech_pvt->caller_profile->pool, bcp->originatee_caller_profile);
+
 						tech_pvt->caller_profile->destination_number = switch_core_sprintf(tech_pvt->caller_profile->pool, 
 																						   "%sanswer,sofia_sla:%s", codec_str, b_private->uuid);
 					}
@@ -8735,6 +8737,7 @@ void sofia_handle_sip_i_invite(switch_core_session_t *session, nua_t *nua, sofia
 		nua_handle_unref(bnh);
 	}
 
+				tech_pvt->caller_profile->originator_caller_profile = switch_caller_profile_dup(tech_pvt->caller_profile->pool, orig_cp);
 
 
 	if (tech_pvt->caller_profile) {
