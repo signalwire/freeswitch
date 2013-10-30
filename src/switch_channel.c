86f2047b1d640a90b2509f2b85cab2c66ace1816
Anthony Minessale <anthm@freeswitch.org>
2013-10-30 17:26:06 -0500

    remove_debug

--- src/switch_channel.c
+++ src/switch_channel.c
@@ -4870,7 +4870,6 @@ static void switch_channel_check_device_state(switch_channel_t *channel, switch_
 		if (!drec->active_start) {
 			drec->active_start = switch_micro_time_now();
 			drec->active_stop = 0;
-			switch_log_printf(SWITCH_CHANNEL_CHANNEL_LOG(channel), SWITCH_LOG_CRIT, "SET START start:%ld stop:%ld\n", drec->active_start, drec->active_stop);
 		}
 		break;
 	case SDS_HELD:
@@ -4885,7 +4884,6 @@ static void switch_channel_check_device_state(switch_channel_t *channel, switch_
 
 	if (drec->active_start && drec->state != SDS_ACTIVE && drec->state != SDS_ACTIVE_MULTI) {
 		drec->active_stop = switch_micro_time_now();
-		switch_log_printf(SWITCH_CHANNEL_CHANNEL_LOG(channel), SWITCH_LOG_CRIT, "SET STOP start:%ld stop:%ld\n", drec->active_start, drec->active_stop);
 	}
 
 	if (drec->ring_start && !drec->ring_stop && drec->state != SDS_RINGING) {
@@ -4947,7 +4945,6 @@ static void switch_channel_check_device_state(switch_channel_t *channel, switch_
 		if (drec->state == SDS_ACTIVE || drec->state == SDS_ACTIVE_MULTI) {
 			drec->active_start = switch_micro_time_now();
 		}
-		switch_log_printf(SWITCH_CHANNEL_CHANNEL_LOG(channel), SWITCH_LOG_CRIT, "SET START start:%ld stop:%ld\n", drec->active_start, drec->active_stop);
 	}
 
 	if (drec->hold_stop) {
