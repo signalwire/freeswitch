9c72dc950e3b08c4b987e62a1a02358a319d599b
Seven Du <dujinfang@gmail.com>
2013-11-22 14:23:18 +0800

    add CF_MEDIA_PAUSE channel flag to allow waiting for IO before media codec is ready
    This could be used at endpoints where signalling and media negotiated separately like in H323

--- src/switch_core_io.c
+++ src/switch_core_io.c
@@ -26,6 +26,7 @@
  * Anthony Minessale II <anthm@freeswitch.org>
  * Michael Jerris <mike@jerris.com>
  * Paul D. Tinsley <pdt at jackhammer.org>
+ * Seven Du <dujinfang@gmail.com>
  *
  *
  * switch_core_io.c -- Main Core Library (Media I/O)
@@ -43,6 +44,10 @@ SWITCH_DECLARE(switch_status_t) switch_core_session_write_video_frame(switch_cor
 
 	if (switch_channel_down(session->channel)) {
 		return SWITCH_STATUS_FALSE;
+	}
+
+	if (switch_channel_test_flag(session->channel, CF_MEDIA_PAUSE)) {
+		return SWITCH_STATUS_SUCCESS;
 	}
 
 	if (session->endpoint_interface->io_routines->write_video_frame) {
@@ -67,6 +72,12 @@ SWITCH_DECLARE(switch_status_t) switch_core_session_read_video_frame(switch_core
 
 	if (switch_channel_down(session->channel)) {
 		return SWITCH_STATUS_FALSE;
+	}
+
+	if (switch_channel_test_flag(session->channel, CF_MEDIA_PAUSE)) {
+		*frame = &runtime.dummy_cng_frame;
+		switch_yield(20000);
+		return SWITCH_STATUS_SUCCESS;
 	}
 
 	if (session->endpoint_interface->io_routines->read_video_frame) {
@@ -165,6 +176,14 @@ SWITCH_DECLARE(switch_status_t) switch_core_session_read_frame(switch_core_sessi
 			*frame = &runtime.dummy_cng_frame;
 			return SWITCH_STATUS_SUCCESS;
 		}
+
+		if (switch_channel_test_flag(session->channel, CF_MEDIA_PAUSE)) {
+			switch_yield(20000);
+			*frame = &runtime.dummy_cng_frame;
+			// switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "Media Paused!!!!\n");
+			return SWITCH_STATUS_SUCCESS;
+		}
+
 		switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_ERROR, "%s has no read codec.\n", switch_channel_get_name(session->channel));
 		switch_channel_hangup(session->channel, SWITCH_CAUSE_INCOMPATIBLE_DESTINATION);
 		return SWITCH_STATUS_FALSE;
@@ -1031,6 +1050,10 @@ SWITCH_DECLARE(switch_status_t) switch_core_session_write_frame(switch_core_sess
 		} else {
 			return SWITCH_STATUS_SUCCESS;
 		}
+	}
+
+	if (switch_channel_test_flag(session->channel, CF_MEDIA_PAUSE)) {
+		return SWITCH_STATUS_SUCCESS;
 	}
 
 	if (!(session->write_codec && switch_core_codec_ready(session->write_codec)) && !pass_cng) {
