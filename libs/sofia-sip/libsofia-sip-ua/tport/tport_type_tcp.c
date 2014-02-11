2b064e639355381bfea6fa85b500db3aafec5fcf
Travis Cross <tc@traviscross.com>
2014-02-11 05:40:41 +0000

    Add additional debugging output to keepalive handling

--- libs/sofia-sip/libsofia-sip-ua/tport/tport_type_tcp.c
+++ libs/sofia-sip/libsofia-sip-ua/tport/tport_type_tcp.c
@@ -202,12 +202,16 @@ int tport_tcp_init_secondary(tport_t *self, int socket, int accepted,
                 __func__, (void *)self,
 		self->tp_params->tpp_keepalive));
 #if defined(TCP_KEEPIDLE)
-  if (val != 0 && val != UINT_MAX)
-    setsockopt(socket, SOL_TCP, TCP_KEEPIDLE, (void *)&val, sizeof val);
+  if (val != 0 && val != UINT_MAX) {
+    SU_DEBUG_3(("%s(%p): Setting TCP_KEEPIDLE to %d\n",
+                __func__, (void *)self, val));
+    setsockopt(socket, SOL_TCP, TCP_KEEPIDLE, (void *)&val, sizeof val);}
 #endif
 #if defined(TCP_KEEPINTVL)
-  if (val != 0 && val != UINT_MAX)
-    setsockopt(socket, SOL_TCP, TCP_KEEPINTVL, (void *)&val, sizeof val);
+  if (val != 0 && val != UINT_MAX) {
+    SU_DEBUG_3(("%s(%p): Setting TCP_KEEPINTVL to %d\n",
+                __func__, (void *)self, val));
+    setsockopt(socket, SOL_TCP, TCP_KEEPINTVL, (void *)&val, sizeof val);}
 #endif
 
   if (!accepted)
