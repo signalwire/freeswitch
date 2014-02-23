c2798d7d11add78317c73db395598f698c1b2de2
Brian West <brian@freeswitch.org>
2014-02-23 10:08:02 -0600

    moving phpmod in esl to use swig2.0 which fixes the few issues when you reswig

--- libs/esl/php/single_command.php
+++ libs/esl/php/single_command.php
@@ -6,10 +6,9 @@ if ($argc > 1) {
     array_shift($argv);
     $command = sprintf('%s', implode(' ', $argv));
     printf("Command to run is: %s\n", $command);
-
-    $sock = new ESLconnection('localhost', '8021', 'ClueCon');
-    $res = $sock->api($command);
-    printf("%s\n", $res->getBody());
+    $esl = new eslConnection('127.0.0.1', '8021', 'ClueCon');
+    $e = $esl->api("$command");
+    print $e->getBody();
 } else {
     printf("ERROR: You Need To Pass A Command\nUsage:\n\t%s <command>", $argv[0]);
 }
