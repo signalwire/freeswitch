90b0ea724391c8489cb2b575a450df483748e95d
Brian West <brian@freeswitch.org>
2014-02-25 08:44:54 -0600

    FS-6267 I was able to replicate this issue, I've tweaked this to prevent -n

--- build/modcheck.sh
+++ build/modcheck.sh
@@ -2,29 +2,21 @@
 
 PATH=$PATH:/bin:/usr/bin
 mods=$1
+on='[01;00;35m'
+off='[0m'
 
 if [ -z $mods ] ; then
     mods="/usr/local/freeswitch/mod"
 fi
 
-
 echo "Checking module integrity in target [$mods]"
 echo
-
 
 here=`pwd`
 
 cd $mods
 files=`ls *.dylib *.so 2>/dev/null`
 cd $here
-
-alert() {
-    on='[01;00;35m'
-    off='[0m'
-    echo -n $on
-    echo WARNING: $1
-    echo -n $off
-}
 
 for i in $files ; do
     mod=${i%%.*}
@@ -33,9 +25,9 @@ for i in $files ; do
     commented=`grep ^\#.*$mod\$ ../modules.conf`
 
     if [ -z "$infile" ] ; then
-	alert "installed module: $i was not installed by this build.  It is not present in modules.conf."
+	echo "${on}WARNING: installed module: $i was not installed by this build.  It is not present in modules.conf.${off}"
     elif [ -n "$commented" ] ; then
-	alert "installed module: $i was not installed by this build.  It is commented from modules.conf. [$commented]"
+	echo "${on}WARNING: installed module: $i was not installed by this build.  It is commented from modules.conf. [$commented]${off}"
     fi
 
 done
