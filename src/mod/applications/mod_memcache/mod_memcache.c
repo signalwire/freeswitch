f18fe5770df52fcff765f72035dd293c8fc5ecdc
Marc Olivier Chouinard <mochouinard@moctel.com>
2014-02-24 21:21:51 -0500

    FS-6151 --resolve Make the module unloadable

--- src/mod/applications/mod_memcache/mod_memcache.c
+++ src/mod/applications/mod_memcache/mod_memcache.c
@@ -387,7 +387,7 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_memcache_load)
 	SWITCH_ADD_API(api_interface, "memcache", "Memcache API", memcache_function, "syntax");
 
 	/* indicate that the module should continue to be loaded */
-	return SWITCH_STATUS_SUCCESS;
+	return SWITCH_STATUS_NOUNLOAD;
 }
 
 /*
