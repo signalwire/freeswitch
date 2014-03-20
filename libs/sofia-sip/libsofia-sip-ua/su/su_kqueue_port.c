74ab9515a48b312be5170dc3256e3fef8607e6a2
Brian West <brian@freeswitch.org>
2014-03-20 13:08:30 -0500

    FS-6294 DragonFly requires this also.

--- libs/sofia-sip/libsofia-sip-ua/su/su_kqueue_port.c
+++ libs/sofia-sip/libsofia-sip-ua/su/su_kqueue_port.c
@@ -51,7 +51,7 @@
 /* Some platforms apparently define the udata field of struct kevent as
  * intptr_t, whereas others define it as void*.  There doesn't seem to be an
  * easy way to tell them apart via autoconf, so we need to use OS macros. */
-#if defined(HAVE_INTTYPES_H) && !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(__darwin__) && !defined(__APPLE__)
+#if defined(HAVE_INTTYPES_H) && !defined(__OpenBSD__) && !defined(__FreeBSD__) && !defined(__darwin__) && !defined(__APPLE__) && !defined(__DragonFly__)
 #define INT_TO_UDATA(x) ((intptr_t)(x))
 #else
 #define INT_TO_UDATA(x) ((void*)(intptr_t)(x))
