d6fa81b4b740ea8a20507aed97be7db684948a8b
Michael Jerris <mike@jerris.com>
2014-04-07 15:27:24 -0400

    ESL-89: --resolve add rubymod-install and add ability to use a ruby not in your path by setting RUBY env var

--- libs/esl/ruby/cflags.rb
+++ libs/esl/ruby/cflags.rb
@@ -3,6 +3,6 @@ require 'rbconfig'
 cflags = if RUBY_VERSION =~ /1.9/ then
   "-I#{RbConfig::CONFIG['rubyhdrdir']} -I#{RbConfig::CONFIG['rubyhdrdir']}/#{RbConfig::CONFIG['arch']}"
 else
-  "-I#{RbConfig::CONFIG["topdir"]}"
+  "-I#{RbConfig::CONFIG["topdir"]} -I#{RbConfig::CONFIG['rubyhdrdir']} -I#{RbConfig::CONFIG['rubyhdrdir']}/#{RbConfig::CONFIG['arch']}"
 end
 puts cflags
