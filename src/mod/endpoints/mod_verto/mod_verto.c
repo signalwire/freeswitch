7ce51712501a6e89e73ac0439f90ac565224e1b6
Brian West <brian@freeswitch.org>
2014-12-03 15:44:27 -0600

    FS-7046 follow up on type change

--- src/mod/endpoints/mod_verto/mod_verto.c
+++ src/mod/endpoints/mod_verto/mod_verto.c
@@ -1539,7 +1539,7 @@ new_req:
 			len = request.content_length - bytes;
 
 			if ((len = ws_raw_read(&jsock->ws, buffer + bytes, len, jsock->ws.block)) < 0) {
-				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Read error %d\n", len);
+				switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Read error %" SWITCH_SIZE_T_FMT"\n", len);
 				goto done;
 			}
 
