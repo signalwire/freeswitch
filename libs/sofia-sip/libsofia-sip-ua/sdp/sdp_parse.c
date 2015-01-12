abd71a71446f874d1dcc3526d5ed93fb448e2026
Anthony Minessale <anthm@freeswitch.org>
2015-01-12 11:34:42 -0600

    support AVPF too

--- libs/sofia-sip/libsofia-sip-ua/sdp/sdp_parse.c
+++ libs/sofia-sip/libsofia-sip-ua/sdp/sdp_parse.c
@@ -1401,6 +1401,10 @@ void sdp_media_transport(sdp_media_t *m, char const *s)
 	  m->m_proto = sdp_proto_extended_srtp, m->m_proto_name = "RTP/SAVPF";
   else if (su_casematch(s, "UDP/TLS/RTP/SAVPF"))
     m->m_proto = sdp_proto_extended_srtp, m->m_proto_name = "UDP/TLS/RTP/SAVPF";
+  else if (su_casematch(s, "RTP/AVPF"))
+	  m->m_proto = sdp_proto_extended_rtp, m->m_proto_name = "RTP/AVPF";
+  else if (su_casematch(s, "UDP/RTP/AVPF"))
+    m->m_proto = sdp_proto_extended_rtp, m->m_proto_name = "UDP/RTP/AVPF";
   else if (su_casematch(s, "udptl"))
     /* Lower case - be compatible with people living by T.38 examples */
     m->m_proto = sdp_proto_udptl, m->m_proto_name = "udptl";
@@ -1421,7 +1425,7 @@ void sdp_media_transport(sdp_media_t *m, char const *s)
 /** Check if media uses RTP as its transport protocol.  */
 int sdp_media_has_rtp(sdp_media_t const *m)
 {
-	return m && (m->m_proto == sdp_proto_rtp || m->m_proto == sdp_proto_srtp || m->m_proto == sdp_proto_extended_srtp);
+	return m && (m->m_proto == sdp_proto_rtp || m->m_proto == sdp_proto_srtp || m->m_proto == sdp_proto_extended_srtp || m->m_proto == sdp_proto_extended_rtp);
 }
 
 #define RTPMAP(pt, encoding, rate, params) \
