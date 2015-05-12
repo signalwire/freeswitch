d253f74e48bfaf2377838d6a12023884e28c6c14
Anthony Minessale <anthm@freeswitch.org>
2015-05-12 15:52:25 -0500

    FS-7501: flush video on jb activate/reset

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -3936,6 +3936,9 @@ SWITCH_DECLARE(switch_status_t) switch_rtp_set_video_buffer_size(switch_rtp_t *r
 		switch_vb_set_frames(rtp_session->vb, frames, frames * 3);
 	}
 
+	switch_rtp_flush(rtp_session);
+	switch_core_session_request_video_refresh(rtp_session->session);
+
 	switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(rtp_session->session), SWITCH_LOG_DEBUG, "Setting video buffer %u Frames.\n", frames);
 	
 	return SWITCH_STATUS_SUCCESS;
