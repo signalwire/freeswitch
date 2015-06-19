83c7def5d51923f8a64bbc9f2aaac814a233fdb0
Brian West <brian@freeswitch.org>
2015-06-19 17:25:22 -0500

    save these for now

---
+++ support-d/aliases.sql
@@ -0,0 +1,4 @@
+INSERT INTO "aliases" VALUES(1,'noww','fsctl shutdown now','TONY');
+INSERT INTO "aliases" VALUES(1,'sto','sofia global siptrace on','TONY');
+INSERT INTO "aliases" VALUES(1,'stf','sofia global siptrace off','TONY');
+INSERT INTO "aliases" VALUES(1,'now','hupall;;msleep 1000;;fsctl shutdown now','TONY');
