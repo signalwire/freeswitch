de7a123c8b9356d1621b4405a1fac2c3db8e1905
Anthony Minessale <anthm@freeswitch.org>
2015-11-13 14:15:39 -0600

    FS-8536 #resolve [Send Keyframe when getting SIP INFO with picture_fast_update]

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -8894,6 +8894,7 @@ void sofia_handle_sip_i_info(nua_t *nua, sofia_profile_t *profile, nua_handle_t 
 				switch_core_session_t *other_session;
 
 				if (switch_channel_test_flag(channel, CF_VIDEO)) {
+					switch_core_media_gen_key_frame(session);
 					if (switch_core_session_get_partner(session, &other_session) == SWITCH_STATUS_SUCCESS) {
 						sofia_glue_build_vid_refresh_message(other_session, sip->sip_payload->pl_data);
 						switch_core_session_rwunlock(other_session);
