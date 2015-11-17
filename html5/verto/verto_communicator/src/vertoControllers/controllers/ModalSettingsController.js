accc6206dd3fd355e029837dd7a079b70fea87f6
Jaon EarlWolf <jamonsterr@gmail.com>
2015-11-17 16:30:17 -0300

    FS-8550 [verto_communicator] - Now setting testSpeedJoin to false when 'auto' is disabled and changed instruction for closing modal in resetSettings()

--- html5/verto/verto_communicator/src/vertoControllers/controllers/ModalSettingsController.js
+++ html5/verto/verto_communicator/src/vertoControllers/controllers/ModalSettingsController.js
@@ -43,7 +43,7 @@
 	  if (confirm('Factory Reset Settings?')) {
             storage.factoryReset();
             $scope.logout();
-            $scope.ok();
+            $modalInstance.close('Ok.');
 	    window.location.reload();
 	  };
         };
@@ -54,6 +54,7 @@
             $scope.mydata.outgoingBandwidth = 'default';
             $scope.mydata.incomingBandwidth = 'default';
             $scope.mydata.vidQual = 'hd';
+            $scope.mydata.testSpeedJoin = false;
           } else {
             $scope.mydata.testSpeedJoin = true;
           }
