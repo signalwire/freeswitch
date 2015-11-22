08816959aae106af9a1340ec9a283f8c7afa8ce4
Jeff Lenk <jeff@jefflenk.com>
2015-11-22 15:38:12 -0600

    simple compile fixes for windows

--- src/mod/endpoints/mod_verto/ws.h
+++ src/mod/endpoints/mod_verto/ws.h
@@ -26,6 +26,15 @@
 #include <openssl/ssl.h>
 
 #ifdef _MSC_VER
+#define __bswap_64(x) \
+  x = (x>>56) | \
+    ((x<<40) & 0x00FF000000000000) | \
+    ((x<<24) & 0x0000FF0000000000) | \
+    ((x<<8)  & 0x000000FF00000000) | \
+    ((x>>8)  & 0x00000000FF000000) | \
+    ((x>>24) & 0x0000000000FF0000) | \
+    ((x>>40) & 0x000000000000FF00) | \
+    (x<<56)
 #ifndef strncasecmp
 #define strncasecmp _strnicmp
 #endif
