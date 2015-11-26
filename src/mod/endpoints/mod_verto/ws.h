5ab62d81147f4ec72bd467f5a28af958986d1e7f
Mike Jerris <mike@jerris.com>
2015-11-25 20:06:41 -0500

    FS-8578: [mod_verto] fix build error for missing __bswap_64 on osx

--- src/mod/endpoints/mod_verto/ws.h
+++ src/mod/endpoints/mod_verto/ws.h
@@ -25,7 +25,7 @@
 //#include "sha1.h"
 #include <openssl/ssl.h>
 
-#ifdef _MSC_VER
+#if defined(_MSC_VER) || defined(__APPLE__) 
 #define __bswap_64(x) \
   x = (x>>56) | \
     ((x<<40) & 0x00FF000000000000) | \
@@ -35,6 +35,8 @@
     ((x>>24) & 0x0000000000FF0000) | \
     ((x>>40) & 0x000000000000FF00) | \
     (x<<56)
+#endif
+#ifdef _MSC_VER
 #ifndef strncasecmp
 #define strncasecmp _strnicmp
 #endif
