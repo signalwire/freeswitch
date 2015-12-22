6c17410c25e503db2a27b246639f0e11dea3a203
Anthony Minessale <anthm@freeswitch.org>
2015-12-22 16:57:41 -0600

    add parsing for userLocation header

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -10424,7 +10424,7 @@ void sofia_handle_sip_i_invite(switch_core_session_t *session, nua_t *nua, sofia
 			} else if (!strcasecmp(un->un_name, "Geolocation")) {
 				switch_channel_set_variable(channel, "sip_geolocation", un->un_value);
 			} else if (!strcasecmp(un->un_name, "Geolocation-Error")) {
-				switch_channel_set_variable(channel, "sip_geolocation", un->un_value);
+				switch_channel_set_variable(channel, "sip_geolocation_error", un->un_value);
 			} else if (!strcasecmp(un->un_name, "userLocation")) {
 				switch_channel_set_variable(channel, "sip_user_location", un->un_value);
 			} else if (!strncasecmp(un->un_name, "X-", 2) || !strncasecmp(un->un_name, "P-", 2) || !strcasecmp(un->un_name, "User-to-User")) {
