63a30499dbb8d280dbf4d18b393aa68ac3ce8c15
Piotr Gregor <piotrek.gregor@gmail.com>
2016-03-20 23:58:45 +0000

    FS-8961 Increase robustness of estimation
    Add optional requirement of consecutive streak
    of estimations in SMA buffer. Fix definitions.
    Add options to control debugging/printing.

--- src/mod/applications/mod_avmd/options.h
+++ src/mod/applications/mod_avmd/options.h
@@ -1,7 +1,32 @@
+/*
+ * @brief   Options controlling avmd module.
+ *
+ * @author Eric des Courtis
+ * @par    Modifications: Piotr Gregor < piotrek.gregor gmail.com >
+ */
+
+
 #ifndef __OPTIONS_H__
 #define __OPTIONS_H__
 
-/* #define FASTMATH */
+
+/* #define AVMD_DEBUG 1 */
+
+/* define/undef this to enable/disable reporting of beep
+ * detection status after session ended */
+#define AVMD_REPORT_STATUS 1
+
+/* define/undefine this to enable/disable faster computation
+ * of arcus cosine - table will be created mapping floats
+ * to integers and returning arc cos values given these integer
+ * indexes into table */
+/* #define AVMD_FAST_MATH */
+
+/* define/undefine this to classify avmd beep detection as valid
+ * only when there is required number of consecutive elements
+ * in the SMA buffer without reset */
+#define AVMD_REQUIRE_CONTINUOUS_STREAK 1
+
 
 #endif
 
