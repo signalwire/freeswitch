6658f61ccdd61fa0638d452bab0356c58a9ce74a
Piotr Gregor <piotrek.gregor@gmail.com>
2016-03-28 01:46:02 +0100

    FS-8983 [avmd] Enable avmd on outbound channel
    It is possible now to do avmd on outgoing channel
    which is very handy while debugging.

--- src/mod/applications/mod_avmd/options.h
+++ src/mod/applications/mod_avmd/options.h
@@ -6,8 +6,8 @@
  */
 
 
-#ifndef __OPTIONS_H__
-#define __OPTIONS_H__
+#ifndef __AVMD_OPTIONS_H__
+#define __AVMD_OPTIONS_H__
 
 
 /* #define AVMD_DEBUG 1 */
@@ -19,14 +19,20 @@
 /* define/undefine this to enable/disable faster computation
  * of arcus cosine - table will be created mapping floats
  * to integers and returning arc cos values given these integer
- * indexes into table */
+ * indices into table */
 /* #define AVMD_FAST_MATH */
 
 /* define/undefine this to classify avmd beep detection as valid
  * only when there is required number of consecutive elements
  * in the SMA buffer without reset */
-#define AVMD_REQUIRE_CONTINUOUS_STREAK 1
+#define AVMD_REQUIRE_CONTINUOUS_STREAK 5
+
+/* define/undefine to enable/disable avmd on incoming audio */
+#define AVMD_INBOUND_CHANNEL
+
+/* define/undefine to enable/disable avmd on outgoing audio */
+/*#define AVMD_OUTBOUND_CHANNEL*/
 
 
-#endif
+#endif /* __AVMD_OPTIONS_H__ */
 
