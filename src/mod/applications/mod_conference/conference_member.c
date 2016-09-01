1f1e37282697d71632956bed301fcd6a12c38d4f
Anthony Minessale <anthm@freeswitch.org>
2016-09-01 13:30:55 -0500

    FS-9474 #resolve [Add variables to set initial volume on mod_conference]

--- src/mod/applications/mod_conference/conference_member.c
+++ src/mod/applications/mod_conference/conference_member.c
@@ -742,6 +742,22 @@ switch_status_t conference_member_add(conference_obj_t *conference, conference_m
 
 		conference_video_check_avatar(member, SWITCH_FALSE);
 
+		if ((var = switch_channel_get_variable_dup(member->channel, "conference_join_volume_in", SWITCH_FALSE, -1))) {
+			uint32_t id = atoi(var) - 1;
+
+			if (id > -5 && id < 5) {
+				member->volume_in_level = id;
+			}
+		}
+
+		if ((var = switch_channel_get_variable_dup(member->channel, "conference_join_volume_out", SWITCH_FALSE, -1))) {
+			uint32_t id = atoi(var) - 1;
+
+			if (id > -5 && id < 5) {
+				member->volume_out_level = id;
+			}
+		}
+		
 		if ((var = switch_channel_get_variable_dup(member->channel, "video_initial_canvas", SWITCH_FALSE, -1))) {
 			uint32_t id = atoi(var) - 1;
 			if (id < conference->canvas_count) {
