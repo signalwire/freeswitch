8b01f65971edfb156d92eaf57d5b7d2740cf9b62
Anthony Minessale <anthm@freeswitch.org>
2016-10-24 16:34:57 -0500

    FS-9666 #resolve [Remove legacy code when getting an XML video refresh]

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -9246,12 +9246,7 @@ void sofia_handle_sip_i_info(nua_t *nua, sofia_profile_t *profile, nua_handle_t 
 
 				if (switch_channel_test_flag(channel, CF_VIDEO)) {
 					switch_core_media_gen_key_frame(session);
-					if (switch_core_session_get_partner(session, &other_session) == SWITCH_STATUS_SUCCESS) {
-						sofia_glue_build_vid_refresh_message(other_session, sip->sip_payload->pl_data);
-						switch_core_session_rwunlock(other_session);
-					} else {
-						switch_channel_set_flag(channel, CF_VIDEO_REFRESH_REQ);
-					}
+					switch_channel_set_flag(channel, CF_VIDEO_REFRESH_REQ);
 				}
 
 			} else if (!strncasecmp(sip->sip_content_type->c_type, "application", 11) &&
