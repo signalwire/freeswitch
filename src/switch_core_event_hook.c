adb25b3fcd2a669168a9fe083a5550046b089a6f
Brian West <brian@freeswitch.org>
2016-10-25 11:38:51 -0500

    FS-9576: add missing event hook functions

--- src/switch_core_event_hook.c
+++ src/switch_core_event_hook.c
@@ -32,17 +32,19 @@
 #include "private/switch_core_pvt.h"
 
 NEW_HOOK_DECL(outgoing_channel)
-	NEW_HOOK_DECL(receive_message)
-	NEW_HOOK_DECL(receive_event)
-	NEW_HOOK_DECL(state_change)
-	NEW_HOOK_DECL(state_run)
-	NEW_HOOK_DECL(read_frame)
-	NEW_HOOK_DECL(write_frame)
-	NEW_HOOK_DECL(video_read_frame)
-	NEW_HOOK_DECL(video_write_frame)
-	NEW_HOOK_DECL(kill_channel)
-	NEW_HOOK_DECL(send_dtmf)
-	NEW_HOOK_DECL(recv_dtmf)
+NEW_HOOK_DECL(receive_message)
+NEW_HOOK_DECL(receive_event)
+NEW_HOOK_DECL(state_change)
+NEW_HOOK_DECL(state_run)
+NEW_HOOK_DECL(read_frame)
+NEW_HOOK_DECL(write_frame)
+NEW_HOOK_DECL(video_read_frame)
+NEW_HOOK_DECL(video_write_frame)
+NEW_HOOK_DECL(text_read_frame);
+NEW_HOOK_DECL(text_write_frame);
+NEW_HOOK_DECL(kill_channel)
+NEW_HOOK_DECL(send_dtmf)
+NEW_HOOK_DECL(recv_dtmf)
 
 /* For Emacs:
  * Local Variables:
