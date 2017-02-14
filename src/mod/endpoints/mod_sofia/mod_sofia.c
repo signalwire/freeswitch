ad08fa13b6f398b353faa704f28d8b6caf4aacd2
Brian West <brian@freeswitch.org>
2017-02-14 14:30:57 -0600

    FS-10041: [mod_conference,mod_sofia] Invalid contact,<(null)>;isfocus, when hold call inside a conference room #resolve
    Conflicts:
    	src/mod/endpoints/mod_sofia/mod_sofia.c

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -652,7 +652,7 @@ static switch_status_t sofia_answer_channel(switch_core_session_t *session)
 	char *sticky = NULL;
 	const char *call_info = switch_channel_get_variable(channel, "presence_call_info_full");
 
-	if (switch_channel_test_flag(channel, CF_CONFERENCE) && !switch_stristr(";isfocus", tech_pvt->reply_contact)) {
+	if (switch_channel_test_flag(channel, CF_CONFERENCE) && !zstr(tech_pvt->reply_contact) && !switch_stristr(";isfocus", tech_pvt->reply_contact)) {
 		tech_pvt->reply_contact = switch_core_session_sprintf(session, "%s;isfocus", tech_pvt->reply_contact);
 	}
 
@@ -1203,7 +1203,7 @@ static switch_status_t sofia_receive_message(switch_core_session_t *session, swi
 		goto end;
 	}
 
-	if (switch_channel_test_flag(channel, CF_CONFERENCE) && !switch_stristr(";isfocus", tech_pvt->reply_contact)) {
+	if (switch_channel_test_flag(channel, CF_CONFERENCE) && !zstr(tech_pvt->reply_contact) && !switch_stristr(";isfocus", tech_pvt->reply_contact)) {
 		tech_pvt->reply_contact = switch_core_session_sprintf(session, "%s;isfocus", tech_pvt->reply_contact);
 	}
 
