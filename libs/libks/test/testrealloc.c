7861bb0d4938c2fe74b7120b09ec6b73122b53c2
Shane Bryldt <astaelan@gmail.com>
2017-04-03 15:49:37 -0500

    FS-10167: Added test to identify issue with ks_pool_realloc

---
+++ libs/libks/test/testrealloc.c
@@ -0,0 +1,47 @@
+#include "ks.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "tap.h"
+
+int main(int argc, char **argv)
+{
+	ks_pool_t *pool;
+	uint32_t *buf = NULL;
+	intptr_t ptr = 0;
+
+	ks_init();
+
+	plan(3);
+
+	ks_pool_open(&pool);
+
+	buf = (uint32_t *)ks_pool_resize(pool, buf, sizeof(uint32_t) * 1);
+	ok(buf != NULL);
+
+	ptr = (intptr_t)buf;
+
+	buf = (uint32_t *)ks_pool_resize(pool, buf, sizeof(uint32_t) * 2);
+	ok(buf != NULL);
+
+	ok((intptr_t)buf == ptr);
+
+	buf = (uint32_t *)ks_pool_resize(pool, buf, sizeof(uint32_t) * 1);
+	ok(buf != NULL);
+
+	ok((intptr_t)buf == ptr);
+
+	buf = (uint32_t *)ks_pool_resize(pool, buf, sizeof(uint32_t) * 2);
+	ok(buf != NULL);
+
+	ok((intptr_t)buf == ptr);
+
+	ks_pool_free(pool, &buf);
+
+	ks_pool_close(&pool);
+
+	ks_shutdown();
+
+	done_testing();
+}
