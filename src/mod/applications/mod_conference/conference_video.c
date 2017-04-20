87db0852954ab893a3c985f42193c38ef8408d02
Anthony Minessale <anthm@freeswitch.org>
2017-04-20 16:41:53 -0500

    FS-10225: [mod_conference] Incorrect layout chosen when playing a file in a conference with a layout group -- fix regression when playing files into a group layout

--- src/mod/applications/mod_conference/conference_video.c
+++ src/mod/applications/mod_conference/conference_video.c
@@ -1846,7 +1846,7 @@ video_layout_t *conference_video_find_best_layout(conference_obj_t *conference, 
 			break;
 		}
 
-		if (vlnode->vlayout->layers - file_layers >= (int)count) {
+		if (vlnode->vlayout->layers - file_layers >= (int)count + file_count) {
 			if (!least || least->vlayout->layers > vlnode->vlayout->layers) {
 				least = vlnode;
 			}
