03eb15c97851b8187ee1fdb5a3d505c19abd4175
Seven Du <dujinfang@x-y-t.cn>
2017-10-10 20:23:07 +0800

    FS-10690 temp fix build on Mac

--- libs/libblade/src/include/blade.h
+++ libs/libblade/src/include/blade.h
@@ -70,6 +70,11 @@ KS_BEGIN_EXTERN_C
 #define config_lookup_from config_setting_lookup
 #endif
 
+#ifdef __APPLE__
+// @todo temp support for Mac comes with 1.6
+#define config_lookup_from config_setting_lookup
+#endif
+
 KS_DECLARE(ks_status_t) blade_init(void);
 KS_DECLARE(ks_status_t) blade_shutdown(void);
 
