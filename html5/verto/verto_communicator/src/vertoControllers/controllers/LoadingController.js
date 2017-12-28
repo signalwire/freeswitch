3188997b73314c9a7a2ca973dc4ba0b239b1b3a3
Italo Rossi <italorossib@gmail.com>
2017-12-28 17:36:35 -0300

    FS-10858 - [verto_communicator] cancel timeout when done to avoid redirect loop

--- html5/verto/verto_communicator/src/vertoControllers/controllers/LoadingController.js
+++ html5/verto/verto_communicator/src/vertoControllers/controllers/LoadingController.js
@@ -6,11 +6,19 @@
         .controller('LoadingController', ['$rootScope', '$scope', '$location', '$interval', 'verto',
             function($rootScope, $scope, $location, $interval, verto) {
                 console.log('Loading controller');
-                $interval(function() {
-                    if (verto.data.resCheckEnded) {
-                      $location.path('/preview');
-                    }
+                var int_id;
+
+                $scope.stopInterval = function() {
+                    $interval.cancel(int_id);
+                };
+
+                int_id = $interval(function() {
+                  if (verto.data.resCheckEnded) {
+                    $scope.stopInterval();
+                    $location.path('/preview');
+                  }
                 }, 1000);
+
             }
         ]);
 })();
