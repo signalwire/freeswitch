20eb89dcb0e59c6705236cc2e4f376ecd4ef2517
Anthony Minessale <anthm@freeswitch.org>
2018-03-29 14:51:37 -0500

    FS-11070: [mod_conference] Improve video bridge first two for mux mode -- add support for files playing in this mode #resolve

--- src/mod/applications/mod_conference/mod_conference.h
+++ src/mod/applications/mod_conference/mod_conference.h
@@ -758,6 +758,7 @@ typedef struct conference_obj {
 	uint32_t moh_wait;
 	uint32_t floor_holder_score_iir;
 	char *default_layout_name;
+	int mux_paused;
 } conference_obj_t;
 
 /* Relationship with another member */
