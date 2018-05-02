dda6fd79ed8d18b2147d419f07286793a21b8c00
Mike Jerris <mike@jerris.com>
2018-05-02 13:42:54 -0500

    FS-11104: [build] use the right openssl version everywhere

--- debian/bootstrap.sh
+++ debian/bootstrap.sh
@@ -318,7 +318,7 @@ Build-Depends:
 # core codecs
  libogg-dev, libspeex-dev, libspeexdsp-dev,
 # configure options
- libssl-dev, unixodbc-dev, libpq-dev,
+ libssl1.0-dev | libssl-dev, unixodbc-dev, libpq-dev,
  libncurses5-dev, libjpeg62-turbo-dev | libjpeg-turbo8-dev | libjpeg62-dev | libjpeg8-dev,
  python-dev, python-all-dev, python-support (>= 0.90) | dh-python, erlang-dev,
 # documentation
