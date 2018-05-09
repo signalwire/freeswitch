8d1b82b51db1d73ea70d8a5a467dc6e74059c27f
Andrey Volk <andywolk@gmail.com>
2018-05-09 18:47:29 +0300

    FS-11150: [Build-System] Fix broken ESL in .Net project.

--- libs/esl/managed/Properties/AssemblyInfo.cs
+++ libs/esl/managed/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ManagedEsl")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
