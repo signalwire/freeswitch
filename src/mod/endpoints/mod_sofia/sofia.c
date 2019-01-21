0c13b704fcc3901f671323a210a78ce52038a508
Brian West <brian@freeswitch.org>
2019-01-21 09:34:33 -0600

    FS-10913: [mod_sofia] do not mark pre_answer unless status is less than 200

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -7332,7 +7332,7 @@ static void sofia_handle_sip_i_state(switch_core_session_t *session, int status,
 			} else {
 				switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "Remote SDP:\n%s\n", r_sdp);
 				tech_pvt->mparams.remote_sdp_str = switch_core_session_strdup(session, r_sdp);
-				if (switch_channel_direction(channel) == SWITCH_CALL_DIRECTION_OUTBOUND) {
+				if (switch_channel_direction(channel) == SWITCH_CALL_DIRECTION_OUTBOUND && status < 200) {
 					switch_channel_mark_pre_answered(channel);
 				}
                 //if ((sofia_test_flag(tech_pvt, TFLAG_LATE_NEGOTIATION) || switch_channel_direction(channel) == SWITCH_CALL_DIRECTION_OUTBOUND)) {
