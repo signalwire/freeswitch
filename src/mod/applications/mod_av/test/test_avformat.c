03077d313321ca09655e7d9d9289cb261928f619
Mike Jerris <mike@signalwire.com>
2019-06-27 14:20:14 -0400

    FS-11895: [osx] fix mod_av osx test build

--- src/mod/applications/mod_av/test/test_avformat.c
+++ src/mod/applications/mod_av/test/test_avformat.c
@@ -156,9 +156,9 @@ FST_CORE_BEGIN("conf")
 
 		FST_TEARDOWN_BEGIN()
 		{
-			const char *err = NULL;
-			switch_sleep(1000000);
-			//fst_check(switch_loadable_module_unload_module(SWITCH_GLOBAL_dirs.mod_dir, (char *)"mod_av", SWITCH_TRUE, &err) == SWITCH_STATUS_SUCCESS);
+		  //const char *err = NULL;
+		  switch_sleep(1000000);
+		  //fst_check(switch_loadable_module_unload_module(SWITCH_GLOBAL_dirs.mod_dir, (char *)"mod_av", SWITCH_TRUE, &err) == SWITCH_STATUS_SUCCESS);
 		}
 		FST_TEARDOWN_END()
 	}
