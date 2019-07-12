b765bd46b5631b6f2438a6f865e6287ed9f32097
Andrey Volk <andywolk@gmail.com>
2019-07-12 20:46:20 +0400

    FS-11903: Fix errors reported by PVS-Studio Static Code Analyzer for switch_stun.c

--- src/switch_stun.c
+++ src/switch_stun.c
@@ -809,26 +809,23 @@ SWITCH_DECLARE(switch_status_t) switch_stun_lookup(char **ip,
 	end_buf = buf + ((sizeof(buf) > packet->header.length) ? packet->header.length : sizeof(buf));
 
 	switch_stun_packet_first_attribute(packet, attr);
+	switch_assert(attr);
+
+
 	do {
 		switch (attr->type) {
 		case SWITCH_STUN_ATTR_MAPPED_ADDRESS:
-			if (attr->type) {
-				if (funny) {
-					switch_stun_ip_t *tmp = (switch_stun_ip_t *) attr->value;
-					tmp->address ^= ntohl(0xabcdabcd);
-				}
-				switch_stun_packet_attribute_get_mapped_address(attr, rip, sizeof(rip), &rport);
+			if (funny) {
+				switch_stun_ip_t *tmp = (switch_stun_ip_t *) attr->value;
+				tmp->address ^= ntohl(0xabcdabcd);
 			}
+			switch_stun_packet_attribute_get_mapped_address(attr, rip, sizeof(rip), &rport);
 			break;
 		case SWITCH_STUN_ATTR_XOR_MAPPED_ADDRESS:
-			if (attr->type) {
-				switch_stun_packet_attribute_get_xor_mapped_address(attr, &packet->header, rip, sizeof(rip), &rport);
-			}
+			switch_stun_packet_attribute_get_xor_mapped_address(attr, &packet->header, rip, sizeof(rip), &rport);
 			break;
 		case SWITCH_STUN_ATTR_USERNAME:
-			if (attr->type) {
-				switch_stun_packet_attribute_get_username(attr, username, 32);
-			}
+			switch_stun_packet_attribute_get_username(attr, username, 32);
 			break;
 		}
 
