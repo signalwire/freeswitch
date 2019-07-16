89c2dc04a08b2af39fa2fe1e3e4c7e17724f30e0
Andrey Volk <andywolk@gmail.com>
2019-07-16 22:23:38 +0400

    FS-11513: [Build-System] Fix Windows versioning.

--- w32/Setup/FullVersion.cmd
+++ w32/Setup/FullVersion.cmd
@@ -5,5 +5,6 @@ FOR /f "delims=" %%i in ('FIND /N "AC_INIT" ..\..\configure.ac') DO SET version_
 FOR /f "tokens=2 delims=," %%a in ("%version_contained_string%") DO SET dirty_version=%%a
 SET almost_clean_version=%dirty_version:[=%
 SET version_with_spaces=%almost_clean_version:]=%
-SET full_version=%version_with_spaces: =%
+SET version_with_dash=%version_with_spaces: =%
+FOR /f "tokens=1 delims=-" %%a in ("%version_with_dash%") DO SET full_version=%%a
 echo %full_version%
