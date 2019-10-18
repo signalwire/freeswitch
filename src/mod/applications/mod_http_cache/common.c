c7527bc4e7b7b93644954031ffae2fcd625ed826
baonq-me <quocbao747@gmail.com>
2019-10-18 16:22:36 +0700

    FS-9676 [mod_http_cache] Support for AWS Signature version 4 and drop support for version 2

--- src/mod/applications/mod_http_cache/common.c
+++ src/mod/applications/mod_http_cache/common.c
@@ -22,13 +22,201 @@
  *
  * Contributor(s):
  * Chris Rienzo <chris.rienzo@grasshopper.com>
- *
+ * Quoc-Bao Nguyen <baonq5@vng.com.vn>
+ * 
  * common.c - Functions common to the store provider
  *
  */
 
-
 #include <switch.h>
+#include <switch_utils.h>
+#include <openssl/hmac.h>
+#include <openssl/sha.h>
+
+
+/**
+ * Create a folder if it is not exists. Equivalent to 'mkdir -p'
+ * @param path
+ */
+SWITCH_MOD_DECLARE(void) recursive_mkdir(char *path)
+{
+    mode_t mode = S_IRWXU | S_IRWXG | S_IRWXO; //S_IRUSR | S_IWUSR;        // 600
+    struct stat sb;
+
+    char *sep = strrchr(path, '/' );
+    if (sep != NULL) {
+        *sep = '\0';
+        recursive_mkdir(path);
+        *sep = '/';
+    }
+
+    if (stat(path, &sb) != 0 || !S_ISDIR(sb.st_mode))
+    {
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_DEBUG, "Making folder: %s\n", path);
+        mkdir(path, mode);
+    }
+}
+
+
+static int recursive_open(const char *path, int oflag, mode_t mode)
+{
+    char* path_dup;
+
+    char *sep = strrchr(path, '/' );
+    if (sep) {
+        switch_strdup(path_dup, path);
+        path_dup[sep - path] = 0;
+        recursive_mkdir(path_dup);
+        switch_safe_free(path_dup);
+    }
+
+    return open(path, oflag, mode);
+}
+
+
+/**
+ * Backup file to local storage. Used with http_put in case of failed
+ * @param source
+ * @param dest
+ * @return
+ */
+SWITCH_MOD_DECLARE(int) backup_file(const char* source, const char* dest)
+{
+    int source_fd, dest_fd;
+    int num_read;
+    mode_t dest_fd_mode;
+    char cwd[128];
+    const unsigned int BUFF_SIZE = 65536;
+    char* buffer;
+    int return_code = 0;
+
+    getcwd(cwd, sizeof(cwd));
+    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Current working dir: %s\n", cwd);
+
+    source_fd = open(source, O_RDONLY);
+    if (source_fd < 0)
+    {
+        return errno;
+    }
+
+    dest_fd_mode = S_IRWXU | S_IRWXG | S_IRWXO; //S_IRUSR | S_IWUSR;        // 600
+    dest_fd = recursive_open(dest, O_CREAT | O_WRONLY | O_TRUNC, dest_fd_mode);        // will overwrite existing file
+    if (dest_fd < 0)
+    {
+        close(source_fd);
+        return errno;
+    }
+
+    switch_malloc(buffer, BUFF_SIZE);
+    while ((num_read = read(source_fd, buffer, BUFF_SIZE)) > 0)
+    {
+        write(dest_fd, buffer, num_read);
+    }
+
+    if (num_read < 0)
+    {
+        return_code = errno;
+    }
+
+    close(source_fd);
+    close(dest_fd);
+    switch_safe_free(buffer);
+
+    return return_code;
+}
+
+
+/**
+ * Calculate HMAC-SHA256 hash of a message
+ * @param buffer buffer to store the HMAC-SHA256 version of message as byte array
+ * @param buffer_length length of buffer
+ * @param key buffer that store the key to run HMAC-SHA256
+ * @param key_length length of the key
+ * @param message message that will be hashed
+ * @return byte array, equals to buffer
+ */
+SWITCH_MOD_DECLARE(char*) hmac256(char* buffer, unsigned int buffer_length, const char* key, unsigned int key_length, const char* message)
+{
+    if (zstr(key) || zstr(message) || buffer_length < SHA256_DIGEST_LENGTH) {
+        return NULL;
+    }
+
+    HMAC(EVP_sha256(),
+		 key,
+		 (int)key_length,
+         (unsigned char *)message,
+         strlen(message),
+		 (unsigned char*)buffer,
+         &buffer_length);
+
+    return (char*)buffer;
+}
+
+
+/**
+ * Calculate HMAC-SHA256 hash of a message
+ * @param buffer buffer to store the HMAC-SHA256 version of the message as hex string
+ * @param key buffer that store the key to run HMAC-SHA256
+ * @param key_length length of the key
+ * @param message message that will be hashed
+ * @return hex string that store the HMAC-SHA256 version of the message
+ */
+SWITCH_MOD_DECLARE(char*) hmac256_hex(char* buffer, const char* key, unsigned int key_length, const char* message)
+{
+    char hmac256_raw[SHA256_DIGEST_LENGTH];
+
+    hmac256(hmac256_raw, SHA256_DIGEST_LENGTH, key, key_length, message);
+
+	for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+	{
+		snprintf(buffer + i*2, 3, "%02x", (unsigned char)hmac256_raw[i]);
+	}
+	buffer[SHA256_DIGEST_LENGTH * 2] = '\0';
+
+    return buffer;
+}
+
+
+/**
+ * Calculate SHA256 hash of a message
+ * @param buffer buffer to store the SHA256 version of the message as hex string
+ * @param string string to be hashed
+ * @return hex string that store the SHA256 version of the message
+ */
+SWITCH_MOD_DECLARE(char*) sha256_hex(char* buffer, const char* string)
+{
+    unsigned char sha256_raw[SHA256_DIGEST_LENGTH];
+
+	SHA256((unsigned char*)string, strlen(string), sha256_raw);
+
+	for (unsigned int i = 0; i < SHA256_DIGEST_LENGTH; i++)
+	{
+		snprintf(buffer + i*2, 3, "%02x", sha256_raw[i]);
+	}
+	buffer[SHA256_DIGEST_LENGTH * 2] = '\0';
+
+    return buffer;
+}
+
+
+/**
+ * Get current time_stamp. Example: 20190724T110316Z
+ * @param format format of the time in strftime format
+ * @param buffer buffer to store the result
+ * @param buffer_length length of buffer
+ * @return current time stamp
+ */
+SWITCH_MOD_DECLARE(char*) get_time(char* format, char* buffer, unsigned int buffer_length)
+{
+	switch_time_exp_t time;
+	switch_size_t size;
+
+	switch_time_exp_gmt(&time, switch_time_now());
+
+	switch_strftime(buffer, &size, buffer_length, format, &time);
+
+	return buffer;
+}
 
 /**
 * Reverse string substring search
@@ -75,7 +263,8 @@ SWITCH_MOD_DECLARE(void) parse_url(char *url, const char *base_domain, const cha
 	*object = NULL;
 
 	if (zstr(url)) {
-		return;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "url is empty\n");
+        return;
 	}
 
 	/* expect: http(s)://bucket.foo-bar.s3.amazonaws.com/object */
@@ -86,7 +275,8 @@ SWITCH_MOD_DECLARE(void) parse_url(char *url, const char *base_domain, const cha
 	}
 
 	if (zstr(bucket_start)) { /* invalid URL */
-		return;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "invalid url\n");
+        return;
 	}
 
 	if (zstr(base_domain)) {
@@ -96,7 +286,8 @@ SWITCH_MOD_DECLARE(void) parse_url(char *url, const char *base_domain, const cha
 	bucket_end = my_strrstr(bucket_start, base_domain_match);
 
 	if (!bucket_end) { /* invalid URL */
-		return;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "invalid url\n");
+        return;
 	}
 
 	*bucket_end = '\0';
@@ -104,13 +295,15 @@ SWITCH_MOD_DECLARE(void) parse_url(char *url, const char *base_domain, const cha
 	object_start = strchr(bucket_end + 1, '/');
 
 	if (!object_start) { /* invalid URL */
-		return;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "invalid url\n");
+        return;
 	}
 
 	object_start++;
 
 	if (zstr(bucket_start) || zstr(object_start)) { /* invalid URL */
-		return;
+        switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "invalid url\n");
+        return;
 	}
 
 	/* ignore the query string from the end of the URL */
--- src/mod/applications/mod_http_cache/common.h
+++ src/mod/applications/mod_http_cache/common.h
@@ -1,10 +1,40 @@
+/*
+ * aws.h for FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ * Copyright (C) 2013-2014, Grasshopper
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the
+ * License.
+ *
+ * The Original Code is aws.h for FreeSWITCH Modular Media Switching Software Library / Soft-Switch Application
+ *
+ * The Initial Developer of the Original Code is Grasshopper
+ * Portions created by the Initial Developer are Copyright (C)
+ * the Initial Developer. All Rights Reserved.
+ *
+ * Contributor(s):
+ * Chris Rienzo <chris.rienzo@grasshopper.com>
+ * Quoc-Bao Nguyen <baonq5@vng.com.vn>
+ * 
+ * common.h - Functions common to the store provider
+ *
+ */
+
 #ifndef COMMON_H
 #define COMMON_H
 
 #include <switch.h>
 
 /**
- * An http profile.  Defines optional credentials
+ * An http profile. Defines optional credentials
  * for access to Amazon S3 and Azure Blob Service
  */
 struct http_profile {
@@ -12,7 +42,10 @@ struct http_profile {
 	char *aws_s3_access_key_id;
 	char *secret_access_key;
 	char *base_domain;
+	char *region;					// AWS region. Used by AWS S3
+	switch_time_t expires;			// Expiration time in seconds for URL signature. Default is 604800 seconds. Used by AWS S3
 	switch_size_t bytes_per_block;
+	char* backup_folder;			// backup folder used when failed to upload file by http_put
 
 	// function to be called to add the profile specific headers to the GET/PUT requests
 	switch_curl_slist_t *(*append_headers_ptr)(struct http_profile *profile, switch_curl_slist_t *headers,
@@ -24,6 +57,64 @@ typedef struct http_profile http_profile_t;
 
 
 SWITCH_MOD_DECLARE(void) parse_url(char *url, const char *base_domain, const char *default_base_domain, char **bucket, char **object);
+
+
+/**
+ * Get current time_stamp. Example: 20190724T110316Z
+ * @param format format of the time in strftime format
+ * @param buffer buffer to store the result
+ * @param buffer_length length of buffer
+ * @return current time stamp
+ */
+SWITCH_MOD_DECLARE(char*) get_time(char* format, char* buffer, unsigned int buffer_length);
+
+
+/**
+ * Calculate HMAC-SHA256 hash of a message
+ * @param buffer buffer to store the HMAC-SHA256 version of message as byte array
+ * @param buffer_length length of buffer
+ * @param key buffer that store the key to run HMAC-SHA256
+ * @param key_length length of the key
+ * @param message message that will be hashed
+ * @return byte array, equals to buffer
+ */
+SWITCH_MOD_DECLARE(char*) hmac256(char* buffer, unsigned int buffer_length, const char* key, unsigned int key_length, const char* message);
+
+/**
+ * Calculate HMAC-SHA256 hash of a message
+ * @param buffer buffer to store the HMAC-SHA256 version of the message as hex string
+ * @param key buffer that store the key to run HMAC-SHA256
+ * @param key_length length of the key
+ * @param message message that will be hashed
+ * @return hex string that store the HMAC-SHA256 version of the message
+ */
+SWITCH_MOD_DECLARE(char*) hmac256_hex(char* buffer, const char* key, unsigned int key_length, const char* message);
+
+
+/**
+ * Calculate SHA256 hash of a message
+ * @param buffer buffer to store the SHA256 version of the message as hex string
+ * @param string string to be hashed
+ * @return hex string that store the SHA256 version of the message
+ */
+SWITCH_MOD_DECLARE(char*) sha256_hex(char* buffer, const char* string);
+
+
+
+/**
+ * Backup file to local storage. Used with http_put in case of failed
+ * @param source
+ * @param dest
+ * @return
+ */
+SWITCH_MOD_DECLARE(int) backup_file(const char* source, const char* dest);
+
+
+/**
+ * Create a folder if it is not exists. Equivalent to 'mkdir -p'
+ * @param path
+ */
+SWITCH_MOD_DECLARE(void) recursive_mkdir(char *path);
 
 #endif
 
