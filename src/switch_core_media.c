b7c5742b6b0ec2d512855754ed55b7e87f6e95a8
Roman Sevko <apple@rom.by>
2019-11-23 17:36:00 +0300

    Update switch_core_media.c
    When there is a generated SRTP call and re-invite, the channel is broken because the encrypted channel is lost. In other words, the server gave the user the parameter:
    a = crypto: 5 AES_CM_128_HMAC_SHA1_80 inline: olNX ...
    ... where 5 - the serial number of this type in the key server list, and when re-invite, the subscriber sends:
    a = crypto: 1 AES_CM_128_HMAC_SHA1_80 inline: olNX ...
    ... where 1 - is the serial number in the caller list. The key is the same, but a different sequence number, ie it is not a mistake. Therefore, we compare is completed, no numbers.
    
    Russian:
    Когда есть сформированный SRTP звонок и выполняем re-invite, то канал разрывается, потому что теряется шифрованный канал. Другими словами сервер отдал абоненту параметр:
    a=crypto:5 AES_CM_128_HMAC_SHA1_80 inline:olNX...
    ...где 5 - это порядковый номер в списке сервера этого типа ключа, а при re-invite, этот абонент присылает:
    a=crypto:1 AES_CM_128_HMAC_SHA1_80 inline:olNX...
    ...где 1 - это порядковый номер в списке абонента. Ключ тот же, но другой порядковый номер, то есть это не ошибка. Поэтому мы сравниваем именно ключ, без номера.

--- src/switch_core_media.c
+++ src/switch_core_media.c
@@ -1826,7 +1826,10 @@ SWITCH_DECLARE(int) switch_core_session_check_incoming_crypto(switch_core_sessio
 
 	if (engine->ssec[engine->crypto_type].remote_crypto_key && switch_rtp_ready(engine->rtp_session)) {
 		/* Compare all the key. The tag may remain the same even if key changed */
-		if (crypto && engine->crypto_type != CRYPTO_INVALID && !strcmp(crypto, engine->ssec[engine->crypto_type].remote_crypto_key)) {
+		const char *a_crkey = switch_stristr("AE", engine->ssec[engine->crypto_type].remote_crypto_key);
+		const char *b_crkey = switch_stristr("AE", crypto);
+
+		if (crypto && engine->crypto_type != CRYPTO_INVALID && !strcmp(a_crkey, b_crkey)) {
 			switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_DEBUG, "Existing key is still valid.\n");
 			got_crypto = 1;
 		} else {
