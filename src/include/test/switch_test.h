4c434aef1fef98f0c48cfee711d0ebfbf22e99f2
Andrey Volk <andywolk@gmail.com>
2019-11-26 22:54:17 +0400

    [Unit-tests] Fix test framework by telling sqldb to manage handles so they don't leak and are destroyed on shutdown.

--- src/include/test/switch_test.h
+++ src/include/test/switch_test.h
@@ -113,7 +113,7 @@ static switch_status_t fst_init_core_and_modload(const char *confdir, const char
 	switch_core_set_globals();
 
 	if (!minimal) {
-		status = switch_core_init_and_modload(0, SWITCH_TRUE, &err);
+		status = switch_core_init_and_modload(SCF_USE_SQL, SWITCH_TRUE, &err);
 		switch_sleep(1 * 1000000);
 		switch_core_set_variable("sound_prefix", "." SWITCH_PATH_SEPARATOR);
 		if (status != SWITCH_STATUS_SUCCESS && err) {
