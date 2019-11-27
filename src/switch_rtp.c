47c5c8f3e8957c37fad5feeeb791375d05992b93
Anthony Minessale <anthm@freeswitch.org>
2019-11-27 21:10:15 +0000

    [Core] Fix short-circuit in write timestamp calc

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -9039,7 +9039,6 @@ SWITCH_DECLARE(int) switch_rtp_write_frame(switch_rtp_t *rtp_session, switch_fra
 		data = frame->data;
 		len = frame->datalen;
 		ts = rtp_session->flags[SWITCH_RTP_FLAG_RAW_WRITE] ? (uint32_t) frame->timestamp : 0;
-		if (!ts) ts = rtp_session->last_write_ts + rtp_session->samples_per_interval;
 	}
 
 	/*
