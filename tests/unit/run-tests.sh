9762569bfad853c9fdce0d94924b44fd933aebb5
Andrey Volk <andywolk@gmail.com>
2019-12-27 19:25:38 +0400

    [Unit-tests] Collect crash data when a unit-test core-dumps

--- tests/unit/run-tests.sh
+++ tests/unit/run-tests.sh
@@ -21,6 +21,12 @@ do
 	rm $logfilename ;
     else
 	echo "*** ./$i exit status is $exitstatus" ;
+	if ls /cores/core.*.!drone!src!tests!unit!.libs!$i.* 1> /dev/null 2>&1; then
+	    echo "Coredump found";
+	    COREDUMP=$(ls /cores/core.*.!drone!src!tests!unit!.libs!$i.*) ;
+	    echo $COREDUMP;
+	    gdb -ex "set logging file backtrace_$i.txt" -ex "set logging on" -ex "set pagination off" -ex "bt" -ex "bt full" -ex "info threads" -ex "thread apply all bt" -ex "thread apply all bt full" -ex "quit" /drone/src/tests/unit/.libs/$i $COREDUMP ;
+	fi ;
 	echo "*** $logfilename was saved" ;
     fi ;
     echo "----------------" ;
