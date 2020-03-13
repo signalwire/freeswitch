3a0ee19f5b336a0e19f7a04660b4126808d40c53
lazedo <luis.azedo@factorlusitano.com>
2020-03-13 16:19:21 +0000

    [mod_kazoo] maintenance
    * [mod_kazoo] add configured sleep time between each io fault
    
    * [mod_kazoo] add configured delay  before fetching initial configuration
    
    * [mod_kazoo] add explicit handling of http_cache for kz_moh

--- src/mod/event_handlers/mod_kazoo/kazoo_ei.h
+++ src/mod/event_handlers/mod_kazoo/kazoo_ei.h
@@ -186,6 +186,7 @@ struct kz_globals_s {
 	switch_port_t port;
 	int config_fetched;
 	int io_fault_tolerance;
+	switch_interval_time_t io_fault_tolerance_sleep;
 	kazoo_event_profile_ptr events;
 	kazoo_config_ptr definitions;
 	kazoo_config_ptr event_handlers;
@@ -198,6 +199,8 @@ struct kz_globals_s {
 	int legacy_events;
 	uint8_t tweaks[KZ_TWEAK_MAX];
 	switch_bool_t expand_headers_on_fetch;
+
+	switch_interval_time_t delay_before_initial_fetch;
 
 
 };
