63b64382e9777e56f47407c4e91dcc20073ddb31
Nickolay Shmyrev <nshmyrev@gmail.com>
2020-04-23 01:51:02 +0200

    Something works with libks

--- src/mod/asr_tts/mod_vosk/mod_vosk.c
+++ src/mod/asr_tts/mod_vosk/mod_vosk.c
@@ -32,7 +32,10 @@
  *
  */
 
+#define __PRETTY_FUNCTION__ __func__
 #include <switch.h>
+#include <netinet/tcp.h>
+#include <libks/ks.h>
 
 SWITCH_MODULE_LOAD_FUNCTION(mod_vosk_load);
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_vosk_shutdown);
@@ -42,30 +45,40 @@ static switch_mutex_t *MUTEX = NULL;
 static switch_event_node_t *NODE = NULL;
 
 static struct {
-	char *remote_url;
+	char *server_url;
+
 	int auto_reload;
 	switch_memory_pool_t *pool;
+	ks_pool_t *ks_pool;
 } globals;
 
 
 typedef struct {
-	switch_mutex_t *flag_mutex;
+	kws_t *ws;
 } vosk_t;
 
 /*! function to open the asr interface */
 static switch_status_t vosk_asr_open(switch_asr_handle_t *ah, const char *codec, int rate, const char *dest, switch_asr_flag_t *flags)
 {
 	vosk_t *vosk;
+	ks_json_t *req = ks_json_create_object();
+	ks_json_add_string_to_object(req, "url", globals.server_url);
 
 	if (!(vosk = (vosk_t *) switch_core_alloc(ah->memory_pool, sizeof(*vosk)))) {
 		return SWITCH_STATUS_MEMERR;
 	}
-
-	switch_mutex_init(&vosk->flag_mutex, SWITCH_MUTEX_NESTED, ah->memory_pool);
 	ah->private_info = vosk;
 
 	codec = "L16";
 	ah->codec = switch_core_strdup(ah->memory_pool, codec);
+	
+	if (kws_connect_ex(&vosk->ws, req, KWS_BLOCK | KWS_CLOSE_SOCK, globals.ks_pool, NULL, 3000) != KS_STATUS_SUCCESS) {
+		ks_json_delete(&req);
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Websocket connect to %s failed.\n", globals.server_url);
+		return SWITCH_STATUS_GENERR;
+	}
+	ks_json_delete(&req);
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "ASR open.\n");
 
 	return SWITCH_STATUS_SUCCESS;
 }
@@ -73,38 +86,37 @@ static switch_status_t vosk_asr_open(switch_asr_handle_t *ah, const char *codec,
 /*! function to close the asr interface */
 static switch_status_t vosk_asr_close(switch_asr_handle_t *ah, switch_asr_flag_t *flags)
 {
-	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Port Closed.\n");
+	vosk_t *vosk = (vosk_t *) ah->private_info;
+
+	kws_destroy(&vosk->ws);
+
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "ASR closed.\n");
 	switch_set_flag(ah, SWITCH_ASR_FLAG_CLOSED);
+
 	return SWITCH_STATUS_SUCCESS;
 }
 
 /*! function to feed audio to the ASR */
 static switch_status_t vosk_asr_feed(switch_asr_handle_t *ah, void *data, unsigned int len, switch_asr_flag_t *flags)
 {
-	//vosk_t *vosk = (vosk_t *) ah->private_info;
-
-	if (switch_test_flag(ah, SWITCH_ASR_FLAG_CLOSED))
-		return SWITCH_STATUS_BREAK;
-
+	vosk_t *vosk = (vosk_t *) ah->private_info;
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Processing data size %d.\n", len);
+	
+	kws_write_frame(vosk->ws, WSOC_BINARY, data, len);
+	
 	return SWITCH_STATUS_SUCCESS;
 }
 
 /*! function to pause recognizer */
 static switch_status_t vosk_asr_pause(switch_asr_handle_t *ah)
 {
-	//vosk_t *vosk = (vosk_t *) ah->private_info;
-	switch_status_t status = SWITCH_STATUS_FALSE;
-
-	return status;
+	return SWITCH_STATUS_SUCCESS;
 }
 
 /*! function to resume recognizer */
 static switch_status_t vosk_asr_resume(switch_asr_handle_t *ah)
 {
-	//vosk_t *vosk = (vosk_t *) ah->private_info;
-	switch_status_t status = SWITCH_STATUS_FALSE;
-
-	return status;
+	return SWITCH_STATUS_SUCCESS;
 }
 
 /**
@@ -136,7 +148,14 @@ static switch_status_t vosk_asr_unload_grammar(switch_asr_handle_t *ah, const ch
 /*! function to read results from the ASR*/
 static switch_status_t vosk_asr_check_results(switch_asr_handle_t *ah, switch_asr_flag_t *flags)
 {
-	//vosk_t *vosk = (vosk_t *) ah->private_info;
+	vosk_t *vosk = (vosk_t *) ah->private_info;
+	kws_opcode_t oc;
+	uint8_t *rdata;
+	int rlen;
+	
+	rlen = kws_read_frame(vosk->ws, &oc, &rdata);
+	switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Recieved %d %s.\n", rlen, rdata);
+
 
 	return SWITCH_STATUS_SUCCESS;
 }
@@ -145,9 +164,8 @@ static switch_status_t vosk_asr_check_results(switch_asr_handle_t *ah, switch_as
 static switch_status_t vosk_asr_get_results(switch_asr_handle_t *ah, char **xmlstr, switch_asr_flag_t *flags)
 {
 	//vosk_t *vosk = (vosk_t *) ah->private_info;
-	switch_status_t status = SWITCH_STATUS_SUCCESS;
 
-	return status;
+	return SWITCH_STATUS_SUCCESS;;
 }
 
 /*! function to start input timeouts */
@@ -169,18 +187,22 @@ static switch_status_t load_config(void)
 		status = SWITCH_STATUS_FALSE;
 		goto done;
 	}
-
+	
+	
 	if ((settings = switch_xml_child(cfg, "settings"))) {
 		for (param = switch_xml_child(settings, "param"); param; param = param->next) {
 			char *var = (char *) switch_xml_attr_soft(param, "name");
 			char *val = (char *) switch_xml_attr_soft(param, "value");
-			if (!strcasecmp(var, "remote_url")) {
-				globals.remote_url = switch_core_strdup(globals.pool, val);
+			if (!strcasecmp(var, "server-url")) {
+				globals.server_url = switch_core_strdup(globals.pool, val);
 			}
 		}
 	}
 
   done:
+	if (!globals.server_url) {
+		globals.server_url = switch_core_strdup(globals.pool, "ws://127.0.0.1:2700");
+	}
 	if (xml) {
 		switch_xml_free(xml);
 	}
@@ -199,7 +221,7 @@ static void event_handler(switch_event_t *event)
 {
 	if (globals.auto_reload) {
 		do_load();
-		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "vosk Reloaded\n");
+		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_INFO, "Vosk Reloaded\n");
 	}
 }
 
@@ -210,6 +232,10 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_vosk_load)
 	switch_mutex_init(&MUTEX, SWITCH_MUTEX_NESTED, pool);
 
 	globals.pool = pool;
+
+	ks_init();
+
+	ks_pool_open(&globals.ks_pool);
 
 	if ((switch_event_bind_removable(modname, SWITCH_EVENT_RELOADXML, NULL, event_handler, NULL, &NODE) != SWITCH_STATUS_SUCCESS)) {
 		switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Couldn't bind!\n");
@@ -239,6 +265,8 @@ SWITCH_MODULE_LOAD_FUNCTION(mod_vosk_load)
 
 SWITCH_MODULE_SHUTDOWN_FUNCTION(mod_vosk_shutdown)
 {
+	ks_pool_close(&globals.ks_pool);
+
 	switch_event_unbind(&NODE);
 	return SWITCH_STATUS_UNLOAD;
 }
