6265389ab831ef6a97e430f141842edf1ddd60d9
Chris Rienzo <chris@signalwire.com>
2020-05-13 17:36:11 -0400

    [mod_conference] Members are searchable by var in more APIs

--- src/mod/applications/mod_conference/mod_conference.h
+++ src/mod/applications/mod_conference/mod_conference.h
@@ -1099,6 +1099,7 @@ void conference_member_set_score_iir(conference_member_t *member, uint32_t score
 
 conference_relationship_t *conference_member_add_relationship(conference_member_t *member, uint32_t id);
 conference_member_t *conference_member_get(conference_obj_t *conference, uint32_t id);
+conference_member_t *conference_member_get_by_str(conference_obj_t *conference, const char *id_str);
 conference_member_t *conference_member_get_by_var(conference_obj_t *conference, const char *var, const char *val);
 conference_member_t *conference_member_get_by_role(conference_obj_t *conference, const char *role_id);
 switch_status_t conference_member_del_relationship(conference_member_t *member, uint32_t id);
