83325712f36b78e016af49495c6bb49653e94836
Anthony Minessale <anthm@signalwire.com>
2020-08-05 01:39:11 +0000

    [core, mod_verto] double udp buffer for audio, add verto command for video refresh

--- src/switch_rtp.c
+++ src/switch_rtp.c
@@ -2921,10 +2921,13 @@ SWITCH_DECLARE(switch_status_t) switch_rtp_set_local_address(switch_rtp_t *rtp_s
 		goto done;
 	}
 
-	//if (rtp_session->flags[SWITCH_RTP_FLAG_VIDEO]) {
-	//	switch_socket_opt_set(new_sock, SWITCH_SO_RCVBUF, 1572864);
-	//	switch_socket_opt_set(new_sock, SWITCH_SO_SNDBUF, 1572864);
-	//}
+	if (rtp_session->flags[SWITCH_RTP_FLAG_VIDEO]) {
+		switch_socket_opt_set(new_sock, SWITCH_SO_RCVBUF, 1572864);
+		switch_socket_opt_set(new_sock, SWITCH_SO_SNDBUF, 1572864);
+	} else {
+		switch_socket_opt_set(new_sock, SWITCH_SO_RCVBUF, 851968);
+		switch_socket_opt_set(new_sock, SWITCH_SO_SNDBUF, 851968);
+	}
 
 	if (switch_socket_bind(new_sock, rtp_session->local_addr) != SWITCH_STATUS_SUCCESS) {
 		char *em = switch_core_sprintf(rtp_session->pool, "Bind Error! %s:%d", host, port);
