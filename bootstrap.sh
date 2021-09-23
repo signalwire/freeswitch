9ba0f0f99f1ff209295b27f984adef042c116771
Alfonso <alfonso@mongotel.com>
2021-09-23 16:16:25 -0400

    add again sofia-sip with 183-180 patch

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ BGJOB=false
 VERBOSE=false
 BASEDIR=`pwd`;
 LIBDIR=${BASEDIR}/libs;
-SUBDIRS="apr libzrtp iksemel libdingaling srtp freetdm unimrcp fs";
+SUBDIRS="apr libzrtp iksemel libdingaling sofia-sip srtp freetdm unimrcp fs";
 
 while getopts 'jhd:v' o; do 
   case "$o" in
