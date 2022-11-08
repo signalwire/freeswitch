9314291c5fa90db6fa5bde5c860c4b875c2186a7
dev-ryanc <46878298+dev-ryanc@users.noreply.github.com>
2022-11-09 00:33:33 +0800

    TEL-5494: Add strict fmt flag to all nua request

--- src/mod/endpoints/mod_sofia/sofia_glue.c
+++ src/mod/endpoints/mod_sofia/sofia_glue.c
@@ -1738,6 +1738,7 @@ switch_status_t sofia_glue_do_invite(switch_core_session_t *session)
 				   NUTAG_SESSION_REFRESHER(tech_pvt->session_refresher),
 				   NUTAG_UPDATE_REFRESH(tech_pvt->update_refresher),
 				   SOATAG_SDP_PRINT_FLAGS(SOA_SDP_PRINT_FLAG_ALWAYS),
+				   SOATAG_SDP_MEDIA_STRICT_FMT(sofia_test_pflag(tech_pvt->profile, PFLAG_SDP_MEDIA_STRICT_FMT)),
 				   TAG_IF(!zstr(session_id_header), SIPTAG_HEADER_STR(session_id_header)),
 				   TAG_IF(sofia_test_flag(tech_pvt, TFLAG_RECOVERED), NUTAG_INVITE_TIMER(UINT_MAX)),
 				   TAG_IF(!zstr(recover_direction), SIPTAG_HEADER_STR(recover_direction)),
