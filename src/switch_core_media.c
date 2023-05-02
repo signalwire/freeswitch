5a4992e2323641294cc376fd9c404d536fb5039f
Yan Brenman <yb91423@gmail.com>
2023-05-02 09:45:11 -0700

    Removed misleading comment

--- src/switch_core_media.c
+++ src/switch_core_media.c
@@ -13126,10 +13126,7 @@ SWITCH_DECLARE(switch_status_t) switch_core_media_receive_message(switch_core_se
 													   "codec_string", "=%s", switch_channel_get_variable(session->channel, "ep_codec_string"));
 				}
 				
-				/* Required fix to allow audio to be added to video
-				 * After making video call a_engine->rmode gets set to SWITCH_MEDIA_FLOW_DISABLED,
-				 * wich prevents audio component to be added to the SDP
-                 */
+				/* Set rmode back to INACTIVE */
 				a_engine->rmode = SWITCH_MEDIA_FLOW_INACTIVE;
 
 				a_engine->codec_negotiated = 0;
