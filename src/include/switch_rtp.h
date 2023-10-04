e73d980a5cd1479f434abfd5393b84fae44b0fe5
Alfonso Pinto <alfonso@telnyx.com>
2023-10-04 17:34:25 +0200

    Revert "TEL-5552: fix merge issue with switch_rtp_new"
    This reverts commit 5dcc885c78a5ed3858268ef0403b62eefb3c8570.

--- src/include/switch_rtp.h
+++ src/include/switch_rtp.h
@@ -307,7 +307,7 @@ SWITCH_DECLARE(switch_rtp_t *) switch_rtp_new(const char *rx_host,
 											  switch_payload_t payload,
 											  uint32_t samples_per_interval,
 											  uint32_t ms_per_packet,
-											  switch_rtp_flag_t flags[], char *timer_name, const char **err, switch_memory_pool_t *pool, switch_port_t bundle_internal_port, switch_port_t bundle_external_port);
+											  switch_rtp_flag_t flags[], char *timer_name, const char **err, switch_memory_pool_t *pool);
 
 /*!
   \brief Assign a remote address to the RTP session
