9abb9bb3a0cc4e27b09a4855a241bc694df69e59
Howell Yan <1726474652@qq.com>
2023-10-15 21:59:26 +0800

    feat: specify rtp ip by variable

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -5026,6 +5026,12 @@ static switch_call_cause_t sofia_outgoing_channel(switch_core_session_t *session
 
 	sofia_glue_attach_private(nsession, profile, tech_pvt, dest);
 
+	// specify rtp ip by variable 
+	if (!zstr(switch_event_get_header(var_event, "rtp_ip_v4"))) {
+        	tech_pvt->mparams.rtpip4 = switch_core_strdup(profile->pool, switch_event_get_header(var_event, "rtp_ip_v4"));
+        	tech_pvt->mparams.rtpip = tech_pvt->mparams.rtpip4;
+    	}
+	
 	if (tech_pvt->local_url) {
 		switch_channel_set_variable(nchannel, "sip_local_url", tech_pvt->local_url);
 		if (profile->pres_type) {
