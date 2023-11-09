186fbd418509500df30433aa8f21acd9f4d3d24c
Nadin Zajimovic <Nadin.Zajimovic@infobip.com>
2023-11-09 16:14:05 +0100

    remove comment

--- src/mod/endpoints/mod_sofia/sofia.c
+++ src/mod/endpoints/mod_sofia/sofia.c
@@ -1669,7 +1669,7 @@ static void our_sofia_event_callback(nua_event_t event,
 		{
 			if (channel && sip) {
 				const char *r_sdp = NULL;
-				//switch_event_t *event;
+				switch_event_t *event;
 				sofia_glue_store_session_id(session, profile, sip, 0);
 
 				if (sip->sip_payload && sip->sip_payload->pl_data) {
@@ -1750,10 +1750,10 @@ static void our_sofia_event_callback(nua_event_t event,
 
 				}
 
-				/*if (switch_event_create(&event, SWITCH_EVENT_RECV_ACK) == SWITCH_STATUS_SUCCESS) {
+				if (switch_event_create(&event, SWITCH_EVENT_RECV_ACK) == SWITCH_STATUS_SUCCESS) {
 					switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "ack_received", "true");
 					switch_event_fire(&event);
-				}*/
+				}
 
 			}
 		}
