f49f73933358dd0bd8eae68b2cf416b5069a871d
aks <aks@cforge.org>
2024-06-14 07:06:23 +0400

    hashtable memset issue fix

--- src/switch_hashtable.c
+++ src/switch_hashtable.c
@@ -126,7 +126,7 @@ hashtable_expand(switch_hashtable_t *h)
 				realloc(h->table, newsize * sizeof(struct entry *));
 			if (NULL == newtable) { (h->primeindex)--; return 0; }
 			h->table = newtable;
-			memset(newtable[h->tablelength], 0, newsize - h->tablelength);
+			memset(&newtable[h->tablelength], 0, ((newsize - h->tablelength) * sizeof(struct entry*)));
 			for (i = 0; i < h->tablelength; i++) {
 				for (pE = &(newtable[i]), e = *pE; e != NULL; e = *pE) {
 					index = indexFor(newsize,e->h);
