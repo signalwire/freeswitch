77959d23892d686c3308670111b165531e586d05
qiwei <qiweibo1984@126.com>
2024-09-04 13:25:58 +0800

    [core]:fix gen local sdp nat test error

--- src/switch_core_media.c
+++ src/switch_core_media.c
@@ -8176,7 +8176,8 @@ SWITCH_DECLARE(switch_status_t) switch_core_media_choose_port(switch_core_sessio
 	sdp_port = engine->local_sdp_port;
 
 	/* Check if NAT is detected  */
-	if (!zstr(smh->mparams->remote_ip) && switch_core_media_check_nat(smh, smh->mparams->remote_ip)) {
+	if ((!zstr(smh->mparams->remote_ip) && switch_core_media_check_nat(smh, smh->mparams->remote_ip))
+	||(engine->cur_payload_map && !zstr(engine->cur_payload_map->remote_sdp_ip) && switch_core_media_check_nat(smh, engine->cur_payload_map->remote_sdp_ip))) {
 		/* Yes, map the port through switch_nat */
 		switch_nat_add_mapping(engine->local_sdp_port, SWITCH_NAT_UDP, &sdp_port, SWITCH_FALSE);
 
