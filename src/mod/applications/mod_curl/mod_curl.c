1b666f1acd43c95a217199adf5c9ff0f924e5fc9
Brian McHugh <brianomchugh@gmail.com>
2025-05-20 18:11:22 -0400

    [mod_curl] Address review feedback: keep error on argc == 2 and HTTP/1.1

--- src/mod/applications/mod_curl/mod_curl.c
+++ src/mod/applications/mod_curl/mod_curl.c
@@ -378,16 +378,15 @@ static char *print_json(switch_memory_pool_t *pool, http_data_t *http_data)
 				char *argv[3] = { 0 };
 				int argc;
 				if ((argc = switch_separate_string(header->data, ' ', argv, (sizeof(argv) / sizeof(argv[0]))))) {
-					if (argc >= 2) {
-  						cJSON_AddItemToObject(top, "version", cJSON_CreateString(argv[0]));
-  						if (argc > 2) {
-    							cJSON_AddItemToObject(top, "phrase", cJSON_CreateString(argv[2]));
-  						} else {
-    							/* HTTP/2 responses may not include a phrase */
-    							cJSON_AddItemToObject(top, "phrase", cJSON_CreateString(""));
-  						}
+					if (argc > 2) {
+						cJSON_AddItemToObject(top, "version", cJSON_CreateString(argv[0]));
+						cJSON_AddItemToObject(top, "phrase", cJSON_CreateString(argv[2]));
+					} else if (argc == 2 && strcmp(argv[0], "HTTP/2") == 0) {
+						/* HTTP/2 responses may not include a phrase */
+						cJSON_AddItemToObject(top, "version", cJSON_CreateString(argv[0]));
+						cJSON_AddItemToObject(top, "phrase", cJSON_CreateString(""));
 					} else {
-						switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unparsable header: argc: %d\n", argc);
+						switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Unparsable header: argc: %d\n", argc);	
 					}
 				} else {
 					switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "Starts with HTTP but not parsable: %s\n", header->data);
