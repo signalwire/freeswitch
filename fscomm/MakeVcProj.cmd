5f3950b49a4eb7438a1669ce4201eb39d3f46d7f
Shreyas-B-Aradhya <shreyasb0115@gmail.com>
2025-08-12 16:00:02 +0530

    trial

--- fscomm/MakeVcProj.cmd
+++
@@ -1,5 +0,0 @@
-\Qt\4.6.0\bin\qmake.exe -spec win32-msvc2008 -tp vc
-copy fscomm.vcproj FSComm.2008.vcproj /y
-echo "Manually copy/replace $QTDIR environment variable into project file"
-echo "Push top GUID block back current"
-pause
--- fscomm/channel.cpp
+++
@@ -1,9 +0,0 @@
-#include "channel.h"
-
-Channel::Channel(QString uuid):
-        _uuid(uuid)
-{
-    _progressEpoch = 0;
-    _progressMediaEpoch = 0;
-    _createdEpoch = 0;
-}
