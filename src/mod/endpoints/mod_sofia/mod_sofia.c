a73c2697afdbe9fd0e331f5955501e1b0fd2ec40
dev-ryanc <46878298+dev-ryanc@users.noreply.github.com>
2025-09-19 20:20:45 +0800

    TEL-6574: Fix deadlock in sofia_receive_message during RESAMPLE_EVENT (#438)

--- src/mod/endpoints/mod_sofia/mod_sofia.c
+++ src/mod/endpoints/mod_sofia/mod_sofia.c
@@ -1750,6 +1750,7 @@ static switch_status_t sofia_receive_message(switch_core_session_t *session, swi
 		}
 		break;
 	case SWITCH_MESSAGE_INDICATE_TRANSCODING_NECESSARY:
+	case SWITCH_MESSAGE_RESAMPLE_EVENT:
 		// Do nothing and exit to prevent unnecessary locking of sofia_mutex
 		goto end;
 	default:
