fb5b98db0a001aff12b7d872327e584e1979691a
Andrey Volk <andywolk@gmail.com>
2026-08-04 01:29:36 +0300

    Bump sofia-sip library requirement to version 1.13.18

--- debian/bootstrap.sh
+++ debian/bootstrap.sh
@@ -318,7 +318,7 @@ Build-Depends:
  uuid-dev, libexpat1-dev, libgdbm-dev, libdb-dev,
 # used by many modules
  libcurl4-openssl-dev | libcurl4-gnutls-dev | libcurl-dev,
- bison, zlib1g-dev, libsofia-sip-ua-dev (>= 1.13.17),
+ bison, zlib1g-dev, libsofia-sip-ua-dev (>= 1.13.18),
  libspandsp4-dev,
 # used to format the private freeswitch apt-repo key properly
  gnupg,
@@ -357,7 +357,7 @@ Description: Cross-Platform Scalable Multi-Protocol Soft Switch
 
 Package: libfreeswitch1
 Architecture: amd64 armhf arm64
-Depends: \${shlibs:Depends}, \${misc:Depends}, libsofia-sip-ua0 (>= 1.13.17)
+Depends: \${shlibs:Depends}, \${misc:Depends}, libsofia-sip-ua0 (>= 1.13.18)
 Recommends:
 Suggests: libfreeswitch1-dbg
 Conflicts: freeswitch-all (<= 1.6.7)
